55811bc0cef2872dae064d52dcc9259c43a829d4
shibbo <xarf98@yahoo.com>
2019-07-21 23:17:46 -0400

    Move PPCArch.s into a C source file

---
+++ include/arch/PPCArch.h
@@ -0,0 +1,41 @@
+#ifndef PPCARCH_H
+#define PPCARCH_H
+
+#include "types.h"
+
+const char* hid4ErrStr = "H4A should not be cleared because of Broadway errata.\n";
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+u32 PPCMfmsr();
+void PPCMtmsr(u32);
+u32 PPCMfhid0();
+void PPCMthid0(u32);
+u32 PPCMfl2cr();
+void PPCMtl2cr(u32);
+void PPCMtdec(u32);
+void PPCSync();
+void PPCHalt();
+void PPCMtmmcr0(u32);
+void PPCMtmmcr1(u32);
+void PPCMtpmc1(u32);
+void PPCMtpmc2(u32);
+void PPCMtpmc3(u32);
+void PPCMtpmc4(u32);
+u32 PPCMffpscr();
+void PPCMtfpscr(u32);
+u32 PPCMfhid2();
+void PPCMthid2(u32);
+u32 PPCMfwpar();
+void PPCMtwpar(u32);
+void PPCDisableSpeculation();
+void PPCSetFpNonIEEEMode();
+void PPCMthid4(register u32);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PPCARCH_H
