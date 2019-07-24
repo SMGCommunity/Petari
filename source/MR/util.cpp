4c7e2b52c7b1ac4a5b83eeea94f777735dcd135d
Simon Aarons <40786398+simontime@users.noreply.github.com>
2019-07-25 04:42:35 +1000

    Clean MR::getHashCode

--- source/MR/util.cpp
+++ source/MR/util.cpp
@@ -2,26 +2,15 @@
 
 namespace MR
 {
-    // todo -- make me better
-    s32 getHashCode(const char *r3)
+    s32 getHashCode(const char *str)
     {
-        s32 r0 = 0;
-        s32 r5 = 0;
-        char r4;
-        goto loadChar;
+        s32 hash = 0;
 
-        genHash:
-            r0 = r5 * 0x1F;
-            r5 = (s32)r4 + r0;
-            r3 = r3 + 1;
-            goto loadChar;
+        while (*str)
+        {
+            hash = (hash * 0x1f) + *str++;
+        }
 
-        loadChar:
-            r4 = *r3;
-            
-            if (r4 != '\0')
-                goto genHash;
-
-            return r5;
+        return hash;
     }
 };
