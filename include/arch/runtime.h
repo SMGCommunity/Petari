0992b0def1a8636357d9522edb42b4b5033657ad
shibbo <xarf98@yahoo.com>
2019-08-13 19:36:44 -0400

    Remove runtime.s as it is useless

---
+++ include/arch/runtime.h
@@ -0,0 +1,16 @@
+#ifndef RUNTIME_H
+#define RUNTIME_H
+
+#include "types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+u32 __cvt_fp2unsigned(f32 in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // RUNIME_H
