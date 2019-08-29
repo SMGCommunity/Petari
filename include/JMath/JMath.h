621d7df1a3a7241d1bb58a176cb46e28a38345f1
shibbo <xarf98@yahoo.com>
2019-08-29 17:49:22 -0400

    JMAVEC does not use C-type symbols

--- include/JMath/JMath.h
+++ include/JMath/JMath.h
@@ -3,14 +3,6 @@
 
 #include "OS/vec.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void JMAVECScaleAdd(const Vec *, const Vec *, Vec *, f32);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // JMATH_H
