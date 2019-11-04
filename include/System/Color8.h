9f1e234012793b95b6deabd780f48f7358893868
shibbo <xarf98@yahoo.com>
2019-11-03 23:31:42 -0500

    Some early progress on Color8

---
+++ include/System/Color8.h
@@ -0,0 +1,25 @@
+#ifndef COLOR8_H
+#define COLOR8_H
+
+#include "OS/GX/GXStruct.h"
+
+class Color8
+{
+public:
+    Color8();
+    Color8(u32);
+    Color8(u8, u8, u8, u8);
+    Color8(_GXColor);
+
+    void set(_GXColor);
+    void set(u8, u8, u8, u8);
+    void operator=(const Color8 &);
+
+    union
+    {
+        u32 fullColor;
+        struct { u8 r, g, b, a; };
+    };
+};
+
+#endif // COLOR8_H
