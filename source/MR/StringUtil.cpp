8c22c015c3a530cda48f0ed2bcf3b344a6b7b131
shibbo <jtandrew@umes.edu>
2021-04-06 15:24:21 -0400

    remove stringutil nonmatching

--- source/MR/StringUtil.cpp
+++ source/MR/StringUtil.cpp
@@ -1,25 +1,11 @@
 #include "MR/StringUtil.h"
 #include "smg.h"
 #include <cstring>
-#include <wchar.h>
-#include <stdio.h>
+#include "extras.h"
+#include "wstring.h"
 
 namespace MR
 {
-    /*void removeExtensionString(char *pOut, u32 len, const char *pSrc)
-    {
-        snprintf(pOut, len, "%s", pSrc);
-        char* fuck = std::strrchr(pOut, '.');
-        char* shit = std::strrchr(pOut, '/');
-
-        if (fuck >= shit)
-        {
-            if (shit++ != fuck)
-            {
-                *fuck = 0;
-            }
-        }
-    }*/
 
     void copyString(char *dest, const char *src, u32 len)
     {
@@ -28,31 +14,13 @@ namespace MR
 
     void copyString(wchar_t *dest, const wchar_t *src, u32 len)
     {
-        wcsncpy(dest, src, len);
+        std::wcsncpy(dest, src, len);
     }
-
-    /*const char* getBasename(const char *pName)
-    {
-        const char* cur = pName;
-        const char* res = std::strrchr(pName, '/');
-
-        if (res != nullptr)
-        {
-            cur = res++;
-        }
-        
-        return cur;
-    }*/
 
     bool isEqualString(const char *str1, const char *str2)
     {
         return !(std::strcmp(str1, str2));
     }
-
-    /*bool isEqualStringCase(const char *str1, const char *str2)
-    {
-        return !(strcasecmp(str1, str2));
-    }*/
 
     bool isEqualSubString(const char *str1, const char *str2)
     {
