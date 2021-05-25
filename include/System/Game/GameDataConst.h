66e0d09a37f0df767eb023d58b70c250237e8c27
Edward Tye <edward.tye@hotmail.com>
2021-05-25 12:28:20 +0100

    ScenarioDataParser.h and some GameDataConst.h

---
+++ include/System/Game/GameDataConst.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <revolution.h>
+
+namespace GameDataConst
+{
+    bool isGrandStar(const char *, s32);
+
+    bool isPowerStarSpecial( const char *, s32, const char *);
+};
