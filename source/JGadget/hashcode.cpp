0936a340bb2694509b10588789314a393eca91e0
shibbo <jtandrew@umes.edu>
2021-07-02 20:47:20 -0400

    manually write hashcode.cpp, and add in progress calculation script

--- source/JGadget/hashcode.cpp
+++ source/JGadget/hashcode.cpp
@@ -2,5 +2,28 @@
 
 namespace JGadget
 {
-    // todo
+    u32 getHashCode(const register char *pStr)
+    {
+        __asm
+        {
+            li r5, 0
+            li r0, 0
+
+            b loadChar
+
+            addToHash:
+                mulli r4, r5, 0x1F
+                extsb r5, r6
+                addi pStr, pStr, 1
+                add r5, r5, r4
+
+            loadChar:
+                lbz r6, 0(pStr)
+                extsb r4, r6
+                cmpw r4, r0
+                bne addToHash
+                mr r3, r5
+                blr
+        }
+    }
 };
