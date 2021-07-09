7418fb5df882b6b8211b2faac346789cd6cb765e
Someone <edward.tye@hotmail.com>
2021-07-09 15:26:00 +0100

    Actually push FlashingCtrl

---
+++ include/LiveActor/FlashingCtrl/FlashingCtrl.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "LiveActor/LiveActor.h"
+
+class FlashingCtrl : public NameObj
+{
+public:
+    FlashingCtrl(LiveActor *, bool);
+
+    virtual void movement();
+
+    void start(int);
+    void end();
+    s32 getCurrentInterval() const;
+    bool isNowFlashing() const;
+    bool isNowOn() const;
+    void updateFlashing();
+
+    LiveActor *mActor; // _C
+    bool mToggleDraw; // _10
+    bool mIsEnded; // _11
+    u8 mOverrideInterval; // _12
+    u8 _13; // padding
+    s32 mTimer; // _14
+    s32 mFlashStartTime; // _18
+};
