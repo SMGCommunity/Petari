1a41a819974c9b9f0c6c07058769bd1bd1ed0ecd
shibbo <jtandrew@umes.edu>
2021-10-25 04:16:52 -0400

    add support for multiple compiler versions in building script

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This repo contains a WIP decompilation of Super Mario Galaxy 1 (USA).
 ## Build Instructions
 
 1. Obtain a copy of mwcceppc and mwldeppc from CodeWarrior 1.2.5 for Gamecube, as well as any version of RVL_SDK, and NW4R. (I can supply this if you DM me shibboleet#1010). You can run the installer and grab the two files out of the `PowerPC_EABI_Tools/Command_Line_Tools` folder where you installed it. Also obtain `powerpc-eabi-as` and `powerpc-eabi-objcopy`, which can be installed with [devkitPro](https://devkitpro.org/wiki/Getting_Started). These will go into the `tools` folder
-2. Add `CWFOLDER`, the path to the CodeWarrior 1.2.5 installation, to the system enviornment variables. Do the same for NW4R with the name `NW4RFOLDER`, and once more for RVL_SDK with the name `RVLFOLDER`.
+2. Add `MWFOLDER`, the path to the CodeWarrior 1.2.5 installation, to the system enviornment variables. Do the same for NW4R with the name `NW4RFOLDER`, and once more for RVL_SDK with the name `RVLFOLDER`. Add `CWFOLDER` and have it point to a folder that contains all of the GC/Wii compilers (I can supply this if you need it.)
 3. Run `build.py`.
 
 ## Contributions
--- build.py
+++ build.py
@@ -17,8 +17,14 @@ toolsPath = os.path.dirname(os.path.realpath(__file__)) + "\\tools\\"
 flags = "-c -Cpp_exceptions off -stdinc -nodefaults -proc gekko -fp hard -lang=c++ -inline off -ipa file -O4,s -rtti off -sdata 4 -sdata2 4 -align powerpc -enum int -DRVL_SDK -DEPPC -DHOLLYWOOD_REV -DTRK_INTEGRATION -DGEKKO -DMTX_USE_PS -D_MSL_USING_MW_C_HEADERS -msgstyle gcc "
 includes = "-i . -I- -i include "
 
+default_compiler_path = "GC/3.0a3/"
+
+compiler_execptions = {
+    #"source\JSystem\JKernel\JKRThread.cpp": "GC/2.5/"
+}
+
 # a list of files that need the flags to be turned on for inlining
-inlined_files = [ "source\Game\System\FileHolder.cpp", "source\JSystem\JKernel\JKRDisposer.cpp" ]
+inlined_files = [ "source\Game\System\FileHolder.cpp", "source\JSystem\JKernel\JKRDisposer.cpp", "source\JSystem\JKernel\JKRThread.cpp" ]
 
 if "-nonmatching" in sys.argv:
     print("Using nonmatching functions")
@@ -36,11 +42,16 @@ if "NW4RFOLDER" not in os.environ:
     print("NW4RFOLDER not set in PATH.")
     sys.exit(1)
 
+if "MWFOLDER" not in os.environ:
+    print("MWFOLDER not set in PATH.")
+    sys.exit(1)
+
 rvl_path = os.getenv("RVLFOLDER")
 cw_path = os.getenv("CWFOLDER")
 nw_path = os.getenv("NW4RFOLDER")
+mw_path = os.getenv("MWFOLDER")
 
-includes += f"-i {rvl_path}\\include -I- -i {nw_path}\\include -I- -i  {cw_path}\\PowerPC_EABI_Support\\MetroTRK -I- -i  {cw_path}\\PowerPC_EABI_Support\\Runtime\\Inc -I- -i {cw_path}\\PowerPC_EABI_Support\\MSL\\MSL_C\\PPC_EABI\\Include -I- -i {cw_path}\\PowerPC_EABI_Support\\MSL\\MSL_C\\MSL_Common\\Include "
+includes += f"-i {rvl_path}\\include -I- -i {nw_path}\\include -I- -i  {mw_path}\\PowerPC_EABI_Support\\MetroTRK -I- -i  {mw_path}\\PowerPC_EABI_Support\\Runtime\\Inc -I- -i {mw_path}\\PowerPC_EABI_Support\\MSL\\MSL_C\\PPC_EABI\\Include -I- -i {mw_path}\\PowerPC_EABI_Support\\MSL\\MSL_C\\MSL_Common\\Include "
 flags += includes
 
 if os.path.exists("build"):
@@ -62,11 +73,19 @@ for root, dirs, files in os.walk("source"):
 for task in tasks:
     source_path, build_path = task
 
+    compiler_path = f"{cw_path}/{default_compiler_path}"
+
+    try:
+        if compiler_execptions[source_path]:
+            compiler_path = f"{cw_path}/{compiler_execptions[source_path]}"
+    except:
+        pass
+
     if source_path in inlined_files:
         new_flags = flags.replace("-inline off", "-inline auto")
         print(f"Compiling {source_path} [with inlined options]...")
 
-        if subprocess.call(f"{toolsPath}/mwcceppc.exe {new_flags} {source_path} -o {build_path}", shell=True) == 1:
+        if subprocess.call(f"{compiler_path}/mwcceppc.exe {new_flags} {source_path} -o {build_path}", shell=True) == 1:
                 deleteDFiles()
                 sys.exit(1)
 
@@ -74,7 +93,7 @@ for task in tasks:
 
     print(f"Compiling {source_path}...")
 
-    if subprocess.call(f"{toolsPath}/mwcceppc.exe {flags} {source_path} -o {build_path}", shell=True) == 1:
+    if subprocess.call(f"{compiler_path}/mwcceppc.exe {flags} {source_path} -o {build_path}", shell=True) == 1:
             deleteDFiles()
             sys.exit(1)
 
