9e1a23cc9d23bf93e378bd9464e2c96088884ad5
shibbo <jtandrew@umes.edu>
2021-11-07 12:02:13 -0500

    NameObjGroup decompiled

---
+++ source/Game/NameObj/NameObjGroup.cpp
@@ -0,0 +1,33 @@
+#include "Game/NameObj/NameObjGroup.h"
+#include "Game/Util.h"
+
+NameObjGroup::NameObjGroup(const char *pGroupName, int maxCount) : NameObj(pGroupName) {
+    _C = 0;
+    mObjectCount = 0;
+    mObjects = 0;
+    initObjArray(maxCount);
+}
+
+NameObjGroup::~NameObjGroup() {
+    delete[] mObjects;
+}
+
+void NameObjGroup::registerObj(NameObj *pObj) {
+    mObjects[mObjectCount] = pObj;
+    mObjectCount++;
+}
+
+void NameObjGroup::pauseOffAll() const {
+    for (s32 i = 0; i < mObjectCount; i++) {
+        MR::requestMovementOn(mObjects[i]);
+    }
+}
+
+void NameObjGroup::initObjArray(int maxCount) {
+    _C = maxCount;
+    mObjects = new NameObj*[maxCount];
+
+    for (s32 i = 0; i < _C; i++) {
+        mObjects[i] = 0;
+    }
+}
