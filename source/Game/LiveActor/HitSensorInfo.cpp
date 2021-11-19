1d0debbba24161b08f81edcd08a14b5ea9693526
shibbo <jtandrew@umes.edu>
2021-11-19 09:41:06 -0500

    match HitSensorInfo ctor

--- source/Game/LiveActor/HitSensorInfo.cpp
+++ source/Game/LiveActor/HitSensorInfo.cpp
@@ -3,19 +3,25 @@
 #include "Game/LiveActor/LiveActor.h"
 #include "Game/Util.h"
 
-#ifdef NON_MATCHING
 HitSensorInfo::HitSensorInfo(const char *pName, HitSensor *pSensor, const TVec3f *a3, MtxPtr mtx, const register TVec3f &a5, bool a6) {
     mName = pName;
     mHashCode = MR::getHashCode(pName);
     mSensor = pSensor;
-    _C.x = a5.x;
-    _C.y = a5.y;
-    _C.z = a5.z;
+
+    register HitSensorInfo* inf = this;
+    __asm {
+        psq_l f0, 0(a5), 0, 0
+        lfs f1, 8(a5)
+        psq_st f0, 0xC(inf), 0, 0
+        stfs f1, 0x14(inf)
+    };
+    //_C.x = a5.x;
+    //_C.y = a5.y;
+    //_C.z = a5.z;
     _18 = a3;
     _1C = mtx;
     _20 = a6;
 }
-#endif
 
 // HitSensorInfo::update()
 
