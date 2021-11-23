89285c685d7d74b9da43acf20c93b17a14603e7c
shibbo <jtandrew@umes.edu>
2021-11-23 11:33:11 -0500

    decompile MovementOnOffGroupHolder

---
+++ include/Game/NameObj/MovementOnOffGroupHolder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Game/NameObj/NameObj.h"
+#include "Game/NameObj/NameObjGroup.h"
+
+class MovementOnOffGroupHolder : public NameObj {
+public:
+    MovementOnOffGroupHolder(const char *);
+    ~MovementOnOffGroupHolder();
+
+    NameObjGroup* joinToGroup(const char *, NameObj *, u32);
+    void onMovementGroup(const char *);
+    NameObjGroup* createGroup(const char *, u32);
+    NameObjGroup* findGroupFromName(const char *) const;
+
+    NameObjGroup* mGroups[0x20];    // _C
+    s32 mCount;                     // _8C
+};
