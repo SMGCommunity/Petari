fb0745c6374c0c7ee3bfcca0b059cc281d76ecbd
Wexos <wexosmk@gmail.com>
2021-12-11 23:00:47 +0100

    CameraHolder

---
+++ include/Game/Camera/CameraHolder.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Game/Camera/Camera.h"
+#include "Game/NameObj/NameObj.h"
+
+class CameraHolder : public NameObj {
+public:
+    CameraHolder(const char *);
+
+    s32 getIndexOf(const char *) const;
+    Camera *getDefaultCamera();
+    s32 getIndexOf(Camera *) const;
+    void createCameras();
+    Camera *getCameraInner(long) const;
+
+    s32 mDefaultCameraIndex;    // _C
+    Camera **mCameras;          // _10
+    void **_14;
+    void *mDefaultFrom14;       // _18
+};
