f132cb5b317bfffab3e5f0b7ba597e65feae4781
Wexos <wexosmk@gmail.com>
2022-01-01 16:47:28 +0100

    JKRDecomp except the actual decompression algorithms

--- include/JSystem/JKernel/JKRDecomp.h
+++ include/JSystem/JKernel/JKRDecomp.h
@@ -2,15 +2,31 @@
 
 #include "JSystem/JKernel/JKRThread.h"
 
+class JKRAMCommand;
+
 enum EJKRCompression {
     JKR_COMPRESSION_NONE = 0,
     JKR_COMPRESSION_SZP = 1,
     JKR_COMPRESSION_SZS = 2,
+    JKR_COMPRESSION_ASR = 3
 };
 
 class JKRDecompCommand {
 public:
     JKRDecompCommand();
+
+    u8 _0[4];
+    u8 *mSrc;                       // _4
+    u8 *mDst;                       // _8
+    u32 mCompressedSize;            // _C
+    u32 mDecompressedSize;          // _10
+    void (*_14)(u32);
+    JKRDecompCommand *mThis;        // _18
+    OSMessageQueue *_1C;
+    s32 _20;
+    JKRAMCommand *mAmCommand;       // _24
+    OSMessageQueue mMessageQueue;   // _28
+    OSMessage mMessage;             // _48
 };
 
 class JKRDecomp : public JKRThread {
@@ -19,17 +35,14 @@ public:
     virtual ~JKRDecomp();
 
     virtual s32 run();
-
+    
     static JKRDecomp *create(long);
-
-    void prepareCommand(unsigned char *, unsigned char *, unsigned long, unsigned long, void(unsigned long));
-    void sendCommand(JKRDecompCommand *);
-    void sync(JKRDecompCommand *, int);
-    void orderSync(unsigned char *, unsigned char *, unsigned long, unsigned long);
-    void decode(unsigned char *, unsigned char *, unsigned long, unsigned long);
-    void decodeSZP(unsigned char *, unsigned char *, unsigned long, unsigned long);
-    void decodeSZS(unsigned char *, unsigned char *, unsigned long, unsigned long);
-    EJKRCompression checkCompressed(unsigned char *);
-
-    static JKRDecomp *sDecompInstance; // _806B71D8
+    static JKRDecompCommand *prepareCommand(unsigned char *, unsigned char *, unsigned long, unsigned long, void (*)(unsigned long));
+    static void sendCommand(JKRDecompCommand *);
+    static bool sync(JKRDecompCommand *, int);
+    static bool orderSync(unsigned char *, unsigned char *, unsigned long, unsigned long);
+    static void decode(unsigned char *, unsigned char *, unsigned long, unsigned long);
+    static void decodeSZP(unsigned char *, unsigned char *, unsigned long, unsigned long);
+    static void decodeSZS(unsigned char *, unsigned char *, unsigned long, unsigned long);
+    static EJKRCompression checkCompressed(unsigned char *);
 };
--- source/JSystem/JKernel/JKRDecomp.cpp
+++ source/JSystem/JKernel/JKRDecomp.cpp
@@ -1,6 +1,23 @@
 #include "JSystem/JKernel/JKRDecomp.h"
 #include "JSystem/JKernel/JKRHeap.h"
+#include "JSystem/JKernel/JKRAramPiece.h"
 #include "revolution.h"
+
+#define NR_MESSAGES 8
+
+namespace {
+    JKRDecomp *gDecompInstance; // _806B71D8
+    OSMessage gMessage[NR_MESSAGES]; // _8060D038
+    OSMessageQueue gMessageQueue; // _8060D058
+}
+
+JKRDecompCommand::JKRDecompCommand() {
+    OSInitMessageQueue(&mMessageQueue, &mMessage, 1);
+    mThis = this; // Probably a pointer to the data stored at 0x00 instead
+    _14 = 0;
+    _1C = NULL;
+    _20 = 0;
+}
 
 JKRDecomp::JKRDecomp(long a1) : JKRThread(0x4000, 0x10, a1) {
     OSResumeThread(mThread);
@@ -10,17 +27,140 @@ JKRDecomp::~JKRDecomp() {
 
 }
 
+s32 JKRDecomp::run() {
+    OSInitMessageQueue(&gMessageQueue, &gMessage[0], NR_MESSAGES);
 
+    JKRDecompCommand *commandPtr;
 
+    while (true) {
+        OSReceiveMessage(&gMessageQueue, reinterpret_cast<OSMessage *>(&commandPtr), OS_MESSAGE_BLOCK);
 
+        JKRDecompCommand &command = *commandPtr;
 
+        decode(
+            command.mSrc,
+            command.mDst,
+            command.mCompressedSize,
+            command.mDecompressedSize
+            );
 
+        if (command._20 != 0) {
+            if (command._20 == 1) {
+                JKRAramPiece::sendCommand(command.mAmCommand);
+            }
 
+            continue;
+        }
+
+        if (command._14 != NULL) {
+            command._14(reinterpret_cast<u32>(&command));
+            continue;
+        }
+
+        if (command._1C != NULL) {
+            OSSendMessage(command._1C, reinterpret_cast<OSMessage>(1), OS_MESSAGE_NOBLOCK);
+        }
+        else {
+            OSSendMessage(&command.mMessageQueue, reinterpret_cast<OSMessage>(1), OS_MESSAGE_NOBLOCK);
+        }
+    }
+}
 
 JKRDecomp *JKRDecomp::create(long a1) {
-    if (sDecompInstance == NULL) {
-        sDecompInstance = new(JKRHeap::sGameHeap, 0) JKRDecomp(a1);
+    if (gDecompInstance == NULL) {
+        gDecompInstance = new(JKRHeap::sGameHeap, 0) JKRDecomp(a1);
     }
 
-    return sDecompInstance;
+    return gDecompInstance;
+}
+
+JKRDecompCommand *JKRDecomp::prepareCommand(unsigned char *pSrc, unsigned char *pDst, unsigned long compressedSize, unsigned long decompressedSize, void (*a5)(unsigned long)) {
+    JKRDecompCommand *command = new(JKRHeap::sGameHeap, -4) JKRDecompCommand();
+
+    command->mSrc = pSrc;
+    command->mDst = pDst;
+    command->mCompressedSize = compressedSize;
+    command->mDecompressedSize = decompressedSize;
+    command->_14 = a5;
+
+    return command;
+}
+
+void JKRDecomp::sendCommand(JKRDecompCommand *pCommand) {
+    OSSendMessage(&gMessageQueue, pCommand, OS_MESSAGE_NOBLOCK);
+}
+
+bool JKRDecomp::sync(JKRDecompCommand *pCommand, int noBlock) {
+    OSMessage message;
+
+    if (noBlock == 0) {
+        OSReceiveMessage(&pCommand->mMessageQueue, &message, OS_MESSAGE_BLOCK);
+        return true;
+    }
+    else {
+        return OSReceiveMessage(&pCommand->mMessageQueue, &message, OS_MESSAGE_NOBLOCK) != 0;
+    }
+}
+
+bool JKRDecomp::orderSync(unsigned char *pSrc, unsigned char *pDst, unsigned long compressedSize, unsigned long decompressedSize) {
+    JKRDecompCommand *command = prepareCommand(pSrc, pDst, compressedSize, decompressedSize,NULL);
+
+    OSSendMessage(&gMessageQueue, command, OS_MESSAGE_NOBLOCK);
+    bool received = sync(command, 0);
+
+    delete command;
+
+    return received;
+}
+
+void JKRDecomp::decode(unsigned char *pSrc, unsigned char *pDst, unsigned long compressedSize, unsigned long decompressedSize) {
+    EJKRCompression compression = checkCompressed(pSrc);
+
+    if (compression == JKR_COMPRESSION_SZP) {
+        decodeSZP(pSrc, pDst, compressedSize, decompressedSize);
+    }
+    else if (compression == JKR_COMPRESSION_SZS) {
+        decodeSZS(pSrc, pDst, compressedSize, decompressedSize);
+    }
+}
+
+/*void JKRDecomp::decodeSZP(unsigned char *pSrc, unsigned char *pDst, unsigned long compressedSize, unsigned long decompressedSize) {
+
+}*/
+
+/*void JKRDecomp::decodeSZS(unsigned char *pSrc, unsigned char *pDst, unsigned long compressedSize, unsigned long a4) {
+    u8 *end = pDst + (*reinterpret_cast<u32 *>(pSrc + 4)) - a4;
+
+    if (compressedSize == 0) {
+        return;
+    }
+
+    if (a4 > *reinterpret_cast<u32 *>(pSrc)) {
+        return;
+    }
+
+    u8 *current = pSrc + 0x10;
+
+    do {
+
+    }
+    while (true);
+}*/
+
+EJKRCompression JKRDecomp::checkCompressed(unsigned char *pSrc) {
+    if (pSrc[0] == 'Y' && pSrc[1] == 'a' && pSrc[3] == '0') {
+        if (pSrc[2] == 'y') {
+            return JKR_COMPRESSION_SZP;
+        }
+        
+        if (pSrc[2] == 'z') {
+            return JKR_COMPRESSION_SZS;
+        }
+    }
+
+    if (pSrc[0] == 'A' && pSrc[1] == 'S' && pSrc[2] == 'R') {
+        return JKR_COMPRESSION_ASR;
+    }
+
+    return JKR_COMPRESSION_NONE;
 }
