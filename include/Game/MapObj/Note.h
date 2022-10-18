0b5169f8cb60121075ce8a2d022aeb97283da4c5
shibbo <jtandrew@umes.edu>
2022-10-18 04:47:27 -0400

    do some Note documentation

--- include/Game/MapObj/Note.h
+++ include/Game/MapObj/Note.h
@@ -17,6 +17,7 @@ public:
     f32 mRotation;  // _18
 };
 
+/// @brief Class that represents a single note in a NoteFairy actor.
 class Note : public LiveActor {
 public:
     Note(const char *, const TVec3f &, NoteFairy *);
@@ -25,20 +26,26 @@ public:
     virtual void init(const JMapInfoIter &);
     virtual void control();
     virtual void calcAndSetBaseMtx();
+
+    /// @brief Checks if the sensor is either a player or a rideable entity and if it is, it will "collect" the note.
     virtual void attackSensor(HitSensor *, HitSensor *);
 
     void startCountDown();
+
+    /// @brief Begins the countdown sequence that plays the "waiting" animation, and is switched to the countdown nerve when mIsCountdown is set to true by the mParentFairy.
     void exeWait();
+    /// @brief Begins the flashing sequence and if the current step reaches mCounter's value, it calls Note::kill.
     void exeCountDown();
+    /// @brief Scales the note's velocity by -30.0 and then invalidates all hit sensors, and after 10 steps, calls Note::kill.
     void exeFlyUp();
 
-    NoteFairy* mParentFairy;    // _8C
+    /* 0x8C */  NoteFairy* mParentFairy;        ///< The parent NoteFairy that this note was spawned from.
     TVec3f _90;
     TVec3f mRailDirection;      // _9C
-    int mCounter;               // _A8
+    /* 0xA8 */  int mCounter;                   ///< The amount of steps it takes for the note to disappear.
     f32 _AC;
     bool mIsCountdown;          // _B0            
-    FlashingCtrl* mFlashCtrl;   // _B4
+    /* 0xB4 */  FlashingCtrl* mFlashCtrl;       // A pointer to the FlashingCtrl instance that handles the "fading away" effects.
 };
 
 namespace NrvNote {
