dfdb464ff77976c412268f788ade17894f255734
shibbo <jtandrew@umes.edu>
2022-11-04 22:18:52 -0400

    document some JSystem / ResourceHolder

--- include/Game/System/ResourceHolder.h
+++ include/Game/System/ResourceHolder.h
@@ -1,8 +1,62 @@
 #pragma once
 
 #include "Game/System/ResourceInfo.h"
+#include "Game/Animation/BckCtrl.h"
+#include <JSystem/JKernel/JKRHeap.h>
+#include <JSystem/JKernel/JKRArchive.h>
+#include <JSystem/JKernel/JKRFileFinder.h>
+
+class MaterialAnmBuffer;
+class J3DModelData;
+
+typedef const char* ArchiveName;
+
+namespace {
+    const char* sModelExt[2] = { ".bdl", ".bmd"};
+    const char* sMotionExt[2] = { ".bck", ".bca" };
+    const char* sBtkExt = ".btk";
+    const char* sBpkExt = ".bpk";
+    const char* sBtpExt = ".btp";
+    const char* sBlkExt = ".blk";
+    const char* sBrkExt = ".brk";
+    const char* sBasExt = ".bas";
+    const char* sBmtExt = ".bmt";
+    const char* sBvaExt = ".bva";
+    const char* sBanmtExt = ".banmt";
+};
 
 class ResourceHolder {
 public:
-    ResTable* mTable;   // _0
+    ResourceHolder(JKRArchive &);
+
+    const char* getMotionName(u32) const;
+    bool isExistMaterialAnm() const;
+    void newMaterialAnmBuffer(J3DModelData *);
+    void newBckCtrl();
+
+    void initializeArc(JKRArchive &);
+    static JKRFileFinder* getFindFinder(JKRArchive *, const char *);
+    u32 initEachResTable(ResTable **, JKRArchive *, const ArchiveName *);
+
+    void mount(JKRArchive *, char *);
+
+    ResTable* mModelResTable;           // _0
+    ResTable* mMotionResTable;          // _4
+    ResTable* mBtkResTable;             // _8
+    ResTable* mBpkResTable;             // _C
+    ResTable* mBtpResTable;             // _10
+    ResTable* mBlkResTable;             // _14
+    ResTable* mBrkResTable;             // _18
+    ResTable* mBasResTable;             // _1C
+    ResTable* mBmtResTable;             // _20
+    ResTable* mBvaResTable;             // _24
+    ResTable* mBanmtResTable;           // _28
+    ResTable* mFileInfoTable;           // _2C
+    ResTable mDefaultTable;             // _30
+    MaterialAnmBuffer* mMaterialBuf;    // _38
+    BckCtrl* mBckCtrl;                  // _3C
+    void* mBackupMaterialData;          // _40
+    JKRArchive* mArchive;               // _44
+    JKRHeap* mHeap;                     // _48
+    u32 _4C;
 };
---
+++ include/Game/Util/GeometryBindUtil.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <JSystem/JGeometry.h>
+#include <revolution.h>
+
+class BindSphere {
+public:
+    TVec3f _0;
+    f32 _C;
+};
+
+class BindCone {
+public:
+    BindCone(const TVec3f &, const TVec3f &, f32, f32);
+
+    bool isInPoint(const TVec3f &) const;
+    void calcVertexPosition();
+    void calcGeneratixLength();
+    void setPosition(const TVec3f &);
+
+    TVec3f _0;
+    TVec3f _C;
+    TVec3f _18;
+    TVec3f _24;
+};
