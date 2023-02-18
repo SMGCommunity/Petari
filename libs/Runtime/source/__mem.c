f4b934ad47afacd372c4a52fe36c9cd200f8e697
shibbo <jtandrew@umes.edu>
2023-02-18 16:29:20 -0500

    fix __flush_cache tabs maybe

--- libs/Runtime/source/__mem.c
+++ libs/Runtime/source/__mem.c
@@ -23,28 +23,28 @@ void* memcpy(void *pDest, const void *pSrc, size_t len) {
 void __fill_mem(void *pDest, int val, unsigned long n) {
 	unsigned long v = (unsigned char) val;
 	unsigned long i;
-	
+
 	((unsigned char*)pDest) = ((unsigned char*)pDest) - 1;
-	
+
 	if (n >= 32) {
 		i = (~(unsigned long)pDest) & 3;
 
 		if (i) {
 			n -= i;
-			
+
 			do {
 				*++(((unsigned char*)pDest)) = v;
-            }
+			}
 			while (--i);
 		}
-	
+
 		if (v != 0) {
 			v |= v << 24 | v << 16 | v <<  8;
-        }
-		
+		}
+
 		((unsigned long*)pDest) = ((unsigned long*) (((unsigned char*)pDest) + 1)) - 1;
 		i = n >> 5;
-		
+
 		if (i != 0) {
 			do {
 				*++(((unsigned long*)pDest)) = v;
@@ -57,27 +57,27 @@ void __fill_mem(void *pDest, int val, unsigned long n) {
 				*++(((unsigned long*)pDest)) = v;
 			}
 			while (--i);
-        }
-		
+		}
+
 		i = (n & 31) >> 2;
-		
+
 		if (i) {
 			do {
-                *++(((unsigned long*)pDest)) = v;
-            }	
+				*++(((unsigned long*)pDest)) = v;
+			}	
 			while (--i);
-        }
-		
+		}
+
 		((unsigned char*)pDest) = ((unsigned char*) (((unsigned long*)pDest) + 1)) - 1;
 		n &= 3;
 	}
-	
+
 	if (n) {
 		do {
 			*++(((unsigned char*)pDest)) = v;
-        }
+		}
 		while (--n);
-    }
+	}
 }
 
 void* memset(void *pDest, int val, size_t num) {
