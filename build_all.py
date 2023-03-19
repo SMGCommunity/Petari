3c18a32a814071908c5141450a64179bc9259a88
shibbo <jtandrew@umes.edu>
2023-03-19 02:21:37 -0400

    change build_all to allow to archive and link a single lib and make it create a DOL

--- build_all.py
+++ build_all.py
@@ -1,9 +1,17 @@
-import subprocess
+import subprocess, sys
 
 libs = [ "JSystem", "MetroTRK", "MSL_C", "nw4r", "Runtime", "RVL_SDK", "RVLFaceLib" ]
 
-for lib in libs:
-    print(f"libs/{lib}")
-    subprocess.call("python build.py -archive", cwd=f"libs/{lib}")
+if len(sys.argv) >= 2:
+    lib_name = sys.argv[1]
+    
+    if lib_name not in libs:
+        print("Error: library not found")
+        sys.exit(1)
+    else:
+        subprocess.call("python build.py -archive", cwd=f"libs/{lib_name}")
+else:
+    for lib in libs:
+        subprocess.call("python build.py -archive", cwd=f"libs/{lib}")
 
 subprocess.call("python build.py -link")
