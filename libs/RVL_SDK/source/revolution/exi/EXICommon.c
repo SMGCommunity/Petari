5fbf3dab7071746e868a591197edb4624db40105
shibbo <jtandrew@umes.edu>
2023-06-27 13:10:57 -0400

    fix wrong value assigned to `__EXIFreq`

--- libs/RVL_SDK/source/revolution/exi/EXICommon.c
+++ libs/RVL_SDK/source/revolution/exi/EXICommon.c
@@ -1,6 +1,6 @@
 #include <revolution/exi.h>
 
-const u32 __EXIFreq = 2;
+const u32 __EXIFreq = 4;
 
 static inline u32	__EXISwap32(u32 val) {
 	return ((val >> 24) & 0x000000FF) | ((val >> 8) & 0x0000FF00) | ((val << 8) & 0x00FF0000) | ((val << 24) & 0xFF000000);
