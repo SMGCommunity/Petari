0d21b9a05a23f761a63df14a1d226c4404f43000
shibbo <supershibbo12@gmail.com>
2024-11-23 19:42:08 -0500

    correct linking

--- configure.py
+++ configure.py
@@ -2594,8 +2594,8 @@ config.libs = [
             Object(NonMatching, "RVL_SDK/os/OSNandbootInfo.c"),
             Object(NonMatching, "RVL_SDK/os/OSPlayTime.c"),
             Object(NonMatching, "RVL_SDK/os/OSLaunch.c"),
-            Object(NonMatching, "RVL_SDK/os/__start.c"),
-            Object(NonMatching, "RVL_SDK/os/__ppc_eabi_init.cpp"),
+            Object(NonMatching, "RVL_SDK/os/init/__start.c"),
+            Object(NonMatching, "RVL_SDK/os/init/__ppc_eabi_init.cpp"),
             Object(NonMatching, "RVL_SDK/os/__ppc_eabi_init.c")
         ]
     ),
