391af1572c43ba0e93eb529f69ab7487dd089269
ThatNintendoNerd <samtheman1026@gmail.com>
2025-04-13 19:47:51 -0700

    Fix missing parameter name in documentation

--- include/Game/NWC24/UTF16Util.hpp
+++ include/Game/NWC24/UTF16Util.hpp
@@ -5,7 +5,7 @@
 
 namespace MR {
     /// @brief Returns the length of the given null-terminated wide string.
-    /// @param The pointer to the null-terminated wide string to be examined.
+    /// @param pStr The pointer to the null-terminated wide string to be examined.
     /// @return The length of the string.
     size_t strlenUTF16(const u16 *pStr);
 };
