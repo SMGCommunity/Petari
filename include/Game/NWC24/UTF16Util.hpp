171c6de6de9903dc7e83b792c4f78d21aacc4907
ThatNintendoNerd <samtheman1026@gmail.com>
2025-04-13 19:35:45 -0700

    Match `UTF16Util`

---
+++ include/Game/NWC24/UTF16Util.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <revolution.h>
+#include <size_t.h>
+
+namespace MR {
+    /// @brief Returns the length of the given null-terminated wide string.
+    /// @param The pointer to the null-terminated wide string to be examined.
+    /// @return The length of the string.
+    size_t strlenUTF16(const u16 *pStr);
+};
