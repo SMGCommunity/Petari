4684db29b2ba8225e4480a96f6e0fc2c96be25a1
shibbo <supershibbo12@gmail.com>
2025-04-25 15:34:49 -0400

    `TripodBossRailMoveParts`

---
+++ include/Game/Boss/TripodBossRailMoveParts.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Game/Boss/TripodBossFixParts.hpp"
+
+class MapPartsRailMover;
+
+class TripodBossRailMoveParts : public TripodBossFixParts {
+public:
+    TripodBossRailMoveParts(const char *);
+
+    virtual ~TripodBossRailMoveParts();
+    virtual void init(const JMapInfoIter &);
+    virtual void makeActorDead();
+    virtual void calcTripodLocalMatrix(TPos3f *);
+    virtual void activateTripodBoss();
+    virtual void exeWaitOwn();
+
+    MapPartsRailMover* mRailMover;          // 0xF0
+};
---
+++ src/Game/Boss/TripodBossRailMoveParts.cpp
@@ -0,0 +1,40 @@
+#include "Game/Boss/TripodBossRailMoveParts.hpp"
+#include "Game/Boss/TripodBossFixParts.hpp"
+#include "Game/MapObj/MapPartsRailMover.hpp"
+
+TripodBossRailMoveParts::TripodBossRailMoveParts(const char *pName) : TripodBossFixParts(pName) {
+    mRailMover = nullptr;
+}
+
+void TripodBossRailMoveParts::init(const JMapInfoIter &rIter) {
+    TripodBossFixParts::init(rIter);
+    initRailRider(rIter);
+    mRailMover = new MapPartsRailMover(this);
+    mRailMover->init(rIter);
+}
+
+void TripodBossRailMoveParts::makeActorDead() {
+    if (mRailMover != nullptr && mRailMover->isWorking()) {
+        mRailMover->end();
+    }
+
+    TripodBossFixPartsBase::makeActorDead();
+}
+
+void TripodBossRailMoveParts::activateTripodBoss() {
+    mRailMover->start();
+    TripodBossFixParts::activateTripodBoss();
+}
+
+void TripodBossRailMoveParts::calcTripodLocalMatrix(TPos3f *pMtx) {
+    _BC.setPS(mRailMover->_28);
+    TripodBossFixPartsBase::calcTripodLocalMatrix(pMtx);
+}
+
+void TripodBossRailMoveParts::exeWaitOwn() {
+    mRailMover->movement();
+}
+
+TripodBossRailMoveParts::~TripodBossRailMoveParts() {
+    
+}
