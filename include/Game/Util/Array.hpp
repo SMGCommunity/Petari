e661e7fff3bca5f15fd929d2e621390df77ddcb0
ThatNintendoNerd <samtheman1026@gmail.com>
2025-04-26 03:00:21 -0700

    Reorder and remove unused `Array` members

--- include/Game/Util/Array.hpp
+++ include/Game/Util/Array.hpp
@@ -10,9 +10,17 @@ namespace MR {
     template<class T>
     class AssignableArray {
     public:
+        typedef T Item;
+
         inline AssignableArray() {
             mArr = 0;
             mMaxSize = 0;
+        }
+
+        ~AssignableArray() NO_INLINE {
+            if (mArr) {
+                delete[] mArr;
+            }
         }
 
         inline void init(s32 cnt) {
@@ -31,18 +39,6 @@ namespace MR {
         inline T* end() {
             return &mArr[mMaxSize];
         }
-
-        ~AssignableArray() NO_INLINE {
-            if (mArr) {
-                delete[] mArr;
-            }
-        }
-
-        inline T* getAtIdx(u32 idx) {
-            return mArr[idx];
-        }
-
-        typedef T Item;
     
         T* mArr;        // 0x0
         s32 mMaxSize;   // 0x4
@@ -71,13 +67,12 @@ namespace MR {
             mCount = 0;
         }
 
-
         ~Vector() NO_INLINE {
             
         }
 
-        inline s32 getCount() {
-            return mCount;
+        inline T::Item operator[](int idx) const {
+            return mArray.mArr[idx];
         }
 
         inline void init(s32 cnt) {
@@ -85,12 +80,18 @@ namespace MR {
             mArray.mMaxSize = cnt;
         }
 
+        inline s32 getCount() {
+            return mCount;
+        }
+
         inline void assign(T::Item &pItem, int where) {
             mArray.mArr[where] = pItem;
         }
 
-        inline void assignToMax(T::Item &pItem) {
-            assign(pItem, mCount);
+        void push_back(const T::Item &rItem) {
+            u32 count = mCount;
+            mCount++;
+            mArray.mArr[count] = rItem;
         }
 
         T::Item* erase(T::Item* pItem) NO_INLINE {
@@ -105,22 +106,12 @@ namespace MR {
             return pItem;
         }
 
-        void push_back(const T::Item &rItem) {
-            u32 count = mCount;
-            mCount++;
-            mArray.mArr[count] = rItem;
-        }
-
         T::Item* begin() {
             return &mArray.mArr[0];
         }
 
         T::Item* end() {
             return &mArray.mArr[mCount];
-        }
-
-        inline T::Item operator[](int idx) const {
-            return mArray.mArr[idx];
         }
 
         T mArray;       // 0x0
