317b024762b0544b2da8d611cba23359c5596e30
ThatNintendoNerd <samtheman1026@gmail.com>
2025-04-26 23:55:25 -0700

    Add documentation to `Array` members

--- include/Game/Util/Array.hpp
+++ include/Game/Util/Array.hpp
@@ -4,124 +4,180 @@
 #include <revolution.h>
 
 namespace MR {
+    /// @brief A contiguous reallocatable array.
     template<class T>
     class AssignableArray {
     public:
         typedef T Item;
 
+        /// @brief Creates a new `AssignableArray`.
         AssignableArray() {
             mArr = 0;
             mMaxSize = 0;
         }
 
+        /// @brief Destroys the `AssignableArray`.
         ~AssignableArray() {
             if (mArr != NULL) {
                 delete[] mArr;
             }
         }
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         T& operator[](int idx) {
             return mArr[idx];
         }
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         const T& operator[](int idx) const {
             return mArr[idx];
         }
 
-        void init(s32 cnt) {
-            mArr = new T[cnt];
-            mMaxSize = cnt;
+        /// @brief Allocates memory for the array.
+        /// @param num The number of elements.
+        void init(s32 num) {
+            mArr = new T[num];
+            mMaxSize = num;
         }
 
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
         int size() const {
             return mMaxSize;
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         T* begin() {
             return &mArr[0];
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         const T* begin() const {
             return &mArr[0];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         T* end() {
             return &mArr[mMaxSize];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         const T* end() const {
             return &mArr[mMaxSize];
         }
 
+        /// @brief The pointer to the array of elements.
         /* 0x0 */ T* mArr;
+
+        /// @brief The number of elements.
         /* 0x4 */ s32 mMaxSize;
     };
 
+    /// @brief A contiguous fixed-size array.
     template<class T, int N>
     class FixedArray {
     public:
         typedef T Item;
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         T& operator[](int idx) {
             return mArr[idx];
         }
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         const T& operator[](int idx) const {
             return mArr[idx];
         }
 
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
         int size() const {
             return N;
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         T* begin() {
             return &mArr[0];
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         const T* begin() const {
             return &mArr[0];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         T* end() {
             return &mArr[N];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         const T* end() const {
             return &mArr[N];
         }
 
     private:
+        /// @brief The array of elements.
         /* 0x0 */ T mArr[N];
     };
 
+    /// @brief A contiguous growable array.
     template<class T>
     class Vector {
     public:
+        /// @brief Creates a new `Vector`.
         Vector() {
             mCount = 0;
         }
 
+        /// @brief Destroys the `Vector`.
         ~Vector() {
             
         }
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         T::Item& operator[](int idx) {
             return mArray[idx];
         }
 
+        /// @brief Returns the element at the given position.
+        /// @param idx The position of the element.
+        /// @return The reference to the element.
         const T::Item& operator[](int idx) const {
             return mArray[idx];
         }
 
-        void init(s32 cnt) {
-            mArray.mArr = new T::Item[cnt];
-            mArray.mMaxSize = cnt;
+        /// @brief Allocates memory for the array.
+        /// @param num The number of elements.
+        void init(s32 num) {
+            mArray.init(num);
         }
 
+        /// @brief Returns the number of elements.
+        /// @return The number of elements.
         int size() const {
             return mCount;
         }
 
+        /// @brief Appends a copy of `rItem` to the end of the container.
+        /// @param rItem The reference to the value to append.
         void push_back(const T::Item &rItem) {
             u32 index = mCount;
 
@@ -129,6 +185,9 @@ namespace MR {
             mArray[index] = rItem;
         }
 
+        /// @brief Removes the value at the given position from the container.
+        /// @param pIter The pointer to the position where the value should be removed.
+        /// @return The pointer to the position of the removed value.
         T::Item* erase(T::Item* pIter) NO_INLINE {
             if (end() - pIter - 1 > 0) {
                 for (T::Item* p = pIter; p + 1 != end(); p++) {
@@ -141,23 +200,34 @@ namespace MR {
             return pIter;
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         T::Item* begin() {
             return &mArray[0];
         }
 
+        /// @brief Returns an iterator to the first element.
+        /// @return The pointer to the first element.
         const T::Item* begin() const {
             return &mArray[0];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         T::Item* end() {
             return &mArray[mCount];
         }
 
+        /// @brief Returns an iterator past the last element.
+        /// @return The pointer past the last element.
         const T::Item* end() const {
             return &mArray[mCount];
         }
 
+        /// @brief The array of elements.
         /* 0x0 */ T mArray;
+
+        /// @brief The number of valid elements.
         /* 0x? */ s32 mCount;
     };
 
