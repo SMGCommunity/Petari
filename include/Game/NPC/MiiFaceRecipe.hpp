de722f96e28b9272421399260c954dc2749356fe
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-02 22:48:05 -0700

    Cleanup `MiiFaceRecipe`

--- include/Game/NPC/MiiFaceRecipe.hpp
+++ include/Game/NPC/MiiFaceRecipe.hpp
@@ -3,7 +3,7 @@
 #include <revolution.h>
 #include <RVLFaceLib.h>
 
-class MiiFaceRecipe { 
+class MiiFaceRecipe {
 public:
     MiiFaceRecipe(RFLDataSource, int, RFLResolution, u32);
 
@@ -11,13 +11,13 @@ public:
     bool hasExpression(RFLExpression) const;
     u32 getModelBufferSize() const;
     void initFaceModel(RFLCharModel *, void *) const;
-    RFLErrcode makeIcon(void *, int, int, _GXColor) const;
+    RFLErrcode makeIcon(void *, int, int, GXColor) const;
     RFLErrcode makeIconWithFavoriteColor(void *, int, int) const;
 
+private:
     RFLDataSource mDataSource;  // 0x0
     RFLMiddleDB* mMiddleDB;     // 0x4
     u16 mIndex;                 // 0x8
-    u16 _A;
     RFLResolution mResolution;  // 0xC
     u32 mExpressionFlag;        // 0x10
 };
--- src/Game/NPC/MiiFaceRecipe.cpp
+++ src/Game/NPC/MiiFaceRecipe.cpp
@@ -1,48 +1,48 @@
 #include "Game/NPC/MiiFaceRecipe.hpp"
-#include <RVLFaceLib.h>
 
-MiiFaceRecipe::MiiFaceRecipe(RFLDataSource dataSource, int index, RFLResolution resolution, u32 expressionFlags) {
-    mDataSource = dataSource;
-    mMiddleDB = 0;
-    mIndex = index;
-    mResolution = resolution;
-    mExpressionFlag = expressionFlags;
+MiiFaceRecipe::MiiFaceRecipe(RFLDataSource dataSource, int index, RFLResolution resolution, u32 expressionFlag) :
+    mDataSource(dataSource),
+    mMiddleDB(NULL),
+    mIndex(index),
+    mResolution(resolution),
+    mExpressionFlag(expressionFlag)
+{}
+
+void MiiFaceRecipe::setDatabase(RFLMiddleDB* pMiddleDB) {
+    mMiddleDB = pMiddleDB;
 }
 
-void MiiFaceRecipe::setDatabase(RFLMiddleDB *pDb) {
-    mMiddleDB = pDb;
-}
-
-bool MiiFaceRecipe::hasExpression(RFLExpression expr) const {
-    return (mExpressionFlag & (1 << expr)) != 0;
+bool MiiFaceRecipe::hasExpression(RFLExpression expression) const {
+    return (mExpressionFlag & (1 << expression)) != 0;
 }
 
 u32 MiiFaceRecipe::getModelBufferSize() const {
     return RFLGetModelBufferSize(mResolution, mExpressionFlag);
 }
 
-void MiiFaceRecipe::initFaceModel(RFLCharModel *pModel, void *pData) const {
-    RFLInitCharModel(pModel, mDataSource, mMiddleDB, mIndex, pData, mResolution, mExpressionFlag);
+void MiiFaceRecipe::initFaceModel(RFLCharModel* pCharModel, void* pBuffer) const {
+    RFLInitCharModel(pCharModel, mDataSource, mMiddleDB, mIndex, pBuffer, mResolution, mExpressionFlag);
 }
 
-RFLErrcode MiiFaceRecipe::makeIcon(void *pData, int width, int height, _GXColor color) const {
-    RFLIconSetting settings;
-    settings.width = width;
-    settings.height = height;
-    settings.bgType = RFLIconBG_Direct;
-    settings.bgColor.r = color.r;
-    settings.bgColor.g = color.g;
-    settings.bgColor.b = color.b;
-    settings.bgColor.a = color.a;
-    settings.drawXluOnly = false;
-    return RFLMakeIcon(pData, mDataSource, mMiddleDB, mIndex, RFLExp_Normal, &settings);
+RFLErrcode MiiFaceRecipe::makeIcon(void* pBuffer, int width, int height, GXColor bgColor) const {
+    RFLIconSetting setting;
+
+    setting.width = width;
+    setting.height = height;
+    setting.bgType = RFLIconBG_Direct;
+    setting.bgColor = bgColor;
+    setting.drawXluOnly = false;
+
+    return RFLMakeIcon(pBuffer, mDataSource, mMiddleDB, mIndex, RFLExp_Normal, &setting);
 }
 
-RFLErrcode MiiFaceRecipe::makeIconWithFavoriteColor(void *pData, int width, int height) const {
-    RFLIconSetting settings;
-    settings.width = width;
-    settings.height = height;
-    settings.bgType = RFLIconBG_Favorite;
-    settings.drawXluOnly = false;
-    return RFLMakeIcon(pData, mDataSource, mMiddleDB, mIndex, RFLExp_Normal, &settings);
+RFLErrcode MiiFaceRecipe::makeIconWithFavoriteColor(void* pBuffer, int width, int height) const {
+    RFLIconSetting setting;
+
+    setting.width = width;
+    setting.height = height;
+    setting.bgType = RFLIconBG_Favorite;
+    setting.drawXluOnly = false;
+
+    return RFLMakeIcon(pBuffer, mDataSource, mMiddleDB, mIndex, RFLExp_Normal, &setting);
 }
