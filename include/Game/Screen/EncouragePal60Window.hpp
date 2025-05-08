79bf169c48930b04470b20b09f233cfd5c706e58
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-07 19:28:04 -0700

    Add partial documentation to `EncouragePal60Window`

--- include/Game/Screen/EncouragePal60Window.hpp
+++ include/Game/Screen/EncouragePal60Window.hpp
@@ -4,18 +4,28 @@
 
 class IconAButton;
 
+/// @brief The actor for the window encouraging the player to change their TV Type from 50 Hz to 60 Hz.
 class EncouragePal60Window : LayoutActor {
 public:
+    /// @brief Creates a new `EncouragePal60Window`.
     EncouragePal60Window();
 
+    /// @brief Destroys the `EncouragePal60Window`.
     virtual ~EncouragePal60Window();
+
     virtual void init(const JMapInfoIter& rIter);
     virtual void appear();
 
+    /// @brief Updates the object when transitioning on screen.
     void exeAppear();
+
+    /// @brief Updates the object when idle or waiting for player input.
     void exeDisplay();
+
+    /// @brief Updates the object when transitioning off screen.
     void exeDisappear();
 
 private:
+    /// @brief The pointer to the actor instance for the A Button.
     /* 0x20 */ IconAButton* mAButtonIcon;
 };
