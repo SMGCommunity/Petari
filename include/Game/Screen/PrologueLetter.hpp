d4ddc5e47f93f745c7e57cc16c903e82fc65582d
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-07 19:36:33 -0700

    Add partial documentation to `PrologueLetter`

--- include/Game/Screen/PrologueLetter.hpp
+++ include/Game/Screen/PrologueLetter.hpp
@@ -4,20 +4,32 @@
 
 class IconAButton;
 
+/// @brief The actor for the letter from Princess Peach during the beginning of the game.
 class PrologueLetter : LayoutActor {
 public:
+    /// @brief Creates a new `PrologueLetter`.
+    /// @param pName The pointer to the null-terminated name of the object.
     PrologueLetter(const char* pName);
 
+    /// @brief Destroys the `PrologueLetter`.
     virtual ~PrologueLetter();
+
     virtual void init(const JMapInfoIter& rIter);
     virtual void appear();
     virtual void kill();
 
     void pauseOff();
+
+    /// @brief Updates the object when transitioning on screen.
     void exeAppear();
+
+    /// @brief Updates the object when waiting for player input.
     void exeWait();
+
+    /// @brief Updates the object when transitioning off screen.
     void exeDisappear();
 
 private:
+    /// @brief The pointer to the actor instance for the A Button.
     /* 0x20 */ IconAButton* mAButtonIcon;
 };
