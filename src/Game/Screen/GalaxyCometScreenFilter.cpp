fc68fab06229e253fd446abcfe244502ac532c75
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-11 03:37:21 -0700

    Match `GalaxyCometScreenFilter`

---
+++ src/Game/Screen/GalaxyCometScreenFilter.cpp
@@ -0,0 +1,21 @@
+#include "Game/Screen/GalaxyCometScreenFilter.hpp"
+#include "Game/Util/EventUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
+#include "Game/Util/LayoutUtil.hpp"
+
+GalaxyCometScreenFilter::GalaxyCometScreenFilter() :
+    LayoutActor("コメット用画面塗り潰し", true),
+    _20(true)
+{
+    MR::connectToScene(this, 14, 13, -1, 65);
+    initLayoutManager("CometScreenFilter", 1);
+    appear();
+}
+
+void GalaxyCometScreenFilter::setCometType(const char* pCometName) {
+    MR::setCometAnimFromId(this, MR::getCometNameIdFromString(pCometName), 0);
+}
+
+GalaxyCometScreenFilter::~GalaxyCometScreenFilter() {
+    
+}
