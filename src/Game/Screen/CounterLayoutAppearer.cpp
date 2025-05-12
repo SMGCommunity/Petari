3568cd06c9fe32d0190a17f4e5af8a3aaf14f104
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-12 02:20:46 -0700

    Cleanup `CounterLayoutAppearer`

--- src/Game/Screen/CounterLayoutAppearer.cpp
+++ src/Game/Screen/CounterLayoutAppearer.cpp
@@ -1,19 +1,24 @@
+#include "Game/LiveActor/Nerve.hpp"
 #include "Game/Screen/CounterLayoutAppearer.hpp"
 #include "Game/Util/LayoutUtil.hpp"
 #include "Game/Util/NerveUtil.hpp"
 
-static f32 zero = 0.0f;
+namespace NrvCounterLayoutAppearer {
+    NEW_NERVE(CounterLayoutAppearerNrvHide, CounterLayoutAppearer, Hide);
+    NEW_NERVE(CounterLayoutAppearerNrvAppear, CounterLayoutAppearer, Appear);
+    NEW_NERVE(CounterLayoutAppearerNrvShow, CounterLayoutAppearer, Show);
+    NEW_NERVE(CounterLayoutAppearerNrvDisappear, CounterLayoutAppearer, Disappear);
+};
 
-CounterLayoutAppearer::CounterLayoutAppearer(LayoutActor *pActor, const TVec2f &a2) : NerveExecutor("カウンタ出現制御") {
-    mLayout = pActor;
-    _C = a2.x;
-    _10 = a2.y;
-    mFollowPos.x = 0.0f;
-    mFollowPos.y = 0.0f;
-    _1C = 0.0f;
-    _20 = 0.0f;
-    MR::setFollowPos(&mFollowPos, pActor, nullptr);
-    MR::setFollowTypeAdd(mLayout, nullptr);
+CounterLayoutAppearer::CounterLayoutAppearer(LayoutActor* pActor, const TVec2f& param2) :
+    NerveExecutor("カウンタ出現制御"),
+    mLayout(pActor),
+    _C(param2.x, param2.y),
+    mFollowPos(0.0f, 0.0f),
+    _1C(0.0f, 0.0f)
+{
+    MR::setFollowPos(&mFollowPos, pActor, NULL);
+    MR::setFollowTypeAdd(mLayout, NULL);
     initNerve(&NrvCounterLayoutAppearer::CounterLayoutAppearerNrvHide::sInstance);
     MR::startAnimAndSetFrameAndStop(mLayout, "Appear", 0.0f, 0);
     setNerve(&NrvCounterLayoutAppearer::CounterLayoutAppearerNrvHide::sInstance);
@@ -23,9 +28,10 @@ void CounterLayoutAppearer::update() {
     updateNerve();
 }
 
-void CounterLayoutAppearer::appear(const TVec2f &a1) {
-    _1C = a1.x;
-    _20 = a1.y;
+void CounterLayoutAppearer::appear(const TVec2f& param1) {
+    _1C.x = param1.x;
+    _1C.y = param1.y;
+
     MR::startAnim(mLayout, "Appear", 0);
     updateLayoutOffset(0.0f);
     setNerve(&NrvCounterLayoutAppearer::CounterLayoutAppearerNrvAppear::sInstance);
@@ -50,20 +56,37 @@ void CounterLayoutAppearer::reset() {
 
 #ifdef NON_MATCHING
 void CounterLayoutAppearer::updateLayoutOffset(f32 offset) {
+    static f32 sZero = 0.0f;
+
     TVec2f vec;
-    f32 x = (_C + (offset * (zero - _C))) + _1C;
-    vec.x = zero;
-    vec.y = zero;
-    f32 y = ((_10 + (offset * (zero - _10))) + _20);
-    mFollowPos.x = x;
-    mFollowPos.y = y;
+    vec.x = sZero - _C.x;
+    vec.y = sZero - _C.y;
+
+    mFollowPos.x = _C.x + offset * vec.x + _1C.x;
+    mFollowPos.y = _C.y + offset * vec.y + _1C.y;
 }
 #endif
 
+void CounterLayoutAppearer::setAppearOffset(const TVec2f& param1) {
+    _C.x = param1.x;
+    _C.y = param1.y;
+}
+
+void CounterLayoutAppearer::exeHide() {
+    
+}
+
 void CounterLayoutAppearer::exeAppear() {
     updateLayoutOffset(MR::calcNerveEaseOutRate(this, MR::getAnimFrameMax(mLayout, (u32)0)));
+
     if (MR::isAnimStopped(mLayout, 0)) {
         setNerve(&NrvCounterLayoutAppearer::CounterLayoutAppearerNrvShow::sInstance);
+    }
+}
+
+void CounterLayoutAppearer::exeShow() {
+    if (MR::isFirstStep(this)) {
+        updateLayoutOffset(1.0f);
     }
 }
 
@@ -72,7 +95,7 @@ void CounterLayoutAppearer::exeDisappear() {
         MR::startAnim(mLayout, "End", 0);
     }
 
-    updateLayoutOffset((1.0f - MR::calcNerveEaseInRate(this, MR::getAnimFrameMax(mLayout, (u32)0))));
+    updateLayoutOffset(1.0f - MR::calcNerveEaseInRate(this, MR::getAnimFrameMax(mLayout, (u32)0)));
 
     if (MR::isAnimStopped(mLayout, 0)) {
         setNerve(&NrvCounterLayoutAppearer::CounterLayoutAppearerNrvHide::sInstance);
@@ -80,34 +103,5 @@ void CounterLayoutAppearer::exeDisappear() {
 }
 
 CounterLayoutAppearer::~CounterLayoutAppearer() {
-
+    
 }
-
-namespace NrvCounterLayoutAppearer {
-    INIT_NERVE(CounterLayoutAppearerNrvHide);
-    INIT_NERVE(CounterLayoutAppearerNrvAppear);
-    INIT_NERVE(CounterLayoutAppearerNrvShow);
-    INIT_NERVE(CounterLayoutAppearerNrvDisappear);
-
-    void CounterLayoutAppearerNrvDisappear::execute(Spine *pSpine) const {
-        CounterLayoutAppearer* layout = reinterpret_cast<CounterLayoutAppearer*>(pSpine->mExecutor);
-        layout->exeDisappear();
-    }
-
-    void CounterLayoutAppearerNrvShow::execute(Spine *pSpine) const {
-        CounterLayoutAppearer* layout = reinterpret_cast<CounterLayoutAppearer*>(pSpine->mExecutor);
-        
-        if (MR::isFirstStep(layout)) {
-            layout->updateLayoutOffset(1.0f);
-        }
-    }
-
-    void CounterLayoutAppearerNrvAppear::execute(Spine *pSpine) const {
-        CounterLayoutAppearer* layout = reinterpret_cast<CounterLayoutAppearer*>(pSpine->mExecutor);
-        layout->exeAppear();
-    }
-
-    void CounterLayoutAppearerNrvHide::execute(Spine *pSpine) const {
-        
-    }
-};
