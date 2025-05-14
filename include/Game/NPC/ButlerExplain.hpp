c5ff389dfc61a1d2efd72023363c2fad0236da52
Gabbo <74122269+Gabbo089@users.noreply.github.com>
2025-05-14 02:34:42 +0200

    ``ButlerExplain``

---
+++ include/Game/NPC/ButlerExplain.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Game/NPC/NPCActor.hpp"
+#include "Game/Util/NPCUtil.hpp"
+
+class ButlerExplain : public NPCActor {
+public:
+    ButlerExplain(const char *);
+
+    virtual ~ButlerExplain();
+    virtual void init(const JMapInfoIter &);
+    virtual void control();
+
+    void startDemo();
+    inline void exeDemo();
+
+    bool _D8;
+    TalkMessageCtrl *talk;
+};
---
+++ src/Game/NPC/ButlerExplain.cpp
@@ -0,0 +1,48 @@
+#include "Game/NPC/ButlerExplain.hpp"
+#include "Game/NPC/NPCActor.hpp"
+#include "Game/Util/DemoUtil.hpp"
+#include "Game/Util/Functor.hpp"
+#include "Game/Util/SoundUtil.hpp"
+#include "Game/Demo/DemoFunction.hpp"
+#include "revolution/types.h"
+
+namespace NrvButlerExplain {
+    NEW_NERVE(ButlerExplainNrvDemo, ButlerExplain, Demo);
+};
+
+ButlerExplain::ButlerExplain(const char *pName) : NPCActor(pName){
+
+}
+
+ButlerExplain::~ButlerExplain() {
+    
+}
+
+void ButlerExplain::init(const JMapInfoIter &rIter) {
+    NPCActorCaps caps("ButlerExplain");
+    caps.setDefault();
+    caps.mObjectName = "Butler";
+    NPCActor::initialize(rIter, caps);
+    if (MR::tryRegisterDemoCast(this, rIter)) {
+        DemoFunction::tryCreateDemoTalkAnimCtrlForActor(this, "DemoWithButler", nullptr);
+        MR::registerDemoActionFunctor(this, MR::Functor_Inline(this, &ButlerExplain::startDemo), "DemoWithButler");
+        DemoFunction::registerDemoTalkMessageCtrl(this, talk);
+    }
+    MR::tryRegisterDemoCast(this, "グランドスター１帰還", rIter);
+}
+
+void ButlerExplain::control() {
+    if (_D8) {
+        MR::startSound(this, "SE_SM_NPC_TRAMPLED", -1, -1);
+        MR::startSound(this, "SE_SV_BUTLER_TRAMPLED", -1, -1);
+    }
+    NPCActor::control();
+}
+
+void ButlerExplain::startDemo() {
+    setNerve(&NrvButlerExplain::ButlerExplainNrvDemo::sInstance);
+}
+
+inline void ButlerExplain::exeDemo() {
+
+}
