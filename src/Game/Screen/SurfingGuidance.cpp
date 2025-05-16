bef1666ae1b76b7f7cce2f39f038bd48961684a3
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-16 01:10:42 -0700

    Cleanup `SurfingGuidance`

--- src/Game/Screen/SurfingGuidance.cpp
+++ src/Game/Screen/SurfingGuidance.cpp
@@ -1,17 +1,39 @@
+#include "Game/LiveActor/Nerve.hpp"
 #include "Game/Screen/SurfingGuidance.hpp"
-#include "Game/Util.hpp"
+#include "Game/Util/LayoutUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
+#include "Game/Util/SoundUtil.hpp"
 
-SurfingGuidance::SurfingGuidance() : LayoutActor("サーフィンガイダンス", true) {
-    _20 = 0;
-    _24 = 0.0f;
-    _2C = 0;
+namespace {
+    NEW_NERVE(SurfingGuidanceFadeIn, SurfingGuidance, FadeIn);
+    NEW_NERVE(SurfingGuidanceLevelOffReady, SurfingGuidance, LevelOffReady);
+    NEW_NERVE(SurfingGuidanceLevelOffHold, SurfingGuidance, LevelOffHold);
+    NEW_NERVE(SurfingGuidanceLevelOffSuccess, SurfingGuidance, LevelOffSuccess);
+    NEW_NERVE(SurfingGuidanceTurnLeftReady, SurfingGuidance, TurnLeftReady);
+    NEW_NERVE(SurfingGuidanceTurnLeftHold, SurfingGuidance, TurnLeftHold);
+    NEW_NERVE(SurfingGuidanceTurnLeftSuccess, SurfingGuidance, TurnLeftSuccess);
+    NEW_NERVE(SurfingGuidanceTurnRightReady, SurfingGuidance, TurnRightReady);
+    NEW_NERVE(SurfingGuidanceTurnRightHold, SurfingGuidance, TurnRightHold);
+    NEW_NERVE(SurfingGuidanceTurnRightSuccess, SurfingGuidance, TurnRightSuccess);
+    NEW_NERVE(SurfingGuidanceFadeOut, SurfingGuidance, FadeOut);
+};
+
+SurfingGuidance::SurfingGuidance() :
+    LayoutActor("サーフィンガイダンス", true),
+    _20(0),
+    _24(0.0f),
+    _2C(0)
+{
+    
 }
 
 void SurfingGuidance::init(const JMapInfoIter &rIter) {
     MR::connectToSceneLayout(this);
     initLayoutManager("TiltGuidance", 3);
     initNerve(&SurfingGuidanceFadeIn::sInstance);
+
     _20 = 0;
+
     kill();
 }
 
@@ -94,12 +116,24 @@ void SurfingGuidance::exeLevelOffReady() {
 
     if (MR::isFirstStep(this)) {
         if (MR::getAnimFrame(this, 1) == 0.0f) {
-            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 0x1F4, -1);
+            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 500, -1);
         }
     }
 
     if (MR::getAnimFrame(this, 1) == 85.0f) {
-        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 0x3E8, -1);
+        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 1000, -1);
+    }
+}
+
+void SurfingGuidance::exeLevelOffHold() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "SlopeHold", 1);
+    }
+}
+
+void SurfingGuidance::exeLevelOffSuccess() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "SlopeOK", 1);
     }
 }
 
@@ -110,12 +144,24 @@ void SurfingGuidance::exeTurnLeftReady() {
 
     if (MR::isFirstStep(this)) {
         if (MR::getAnimFrame(this, 1) == 0.0f) {
-            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 0x1F4, -1);
+            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 500, -1);
         }
     }
 
     if (MR::getAnimFrame(this, 1) == 35.0f) {
-        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 0x3E8, -1);
+        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 1000, -1);
+    }
+}
+
+void SurfingGuidance::exeTurnLeftHold() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "TurnLeftHold", 1);
+    }
+}
+
+void SurfingGuidance::exeTurnLeftSuccess() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "TurnLeftOK", 1);
     }
 }
 
@@ -126,12 +172,24 @@ void SurfingGuidance::exeTurnRightReady() {
 
     if (MR::isFirstStep(this)) {
         if (MR::getAnimFrame(this, 1) == 0.0f) {
-            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 0x1F4, -1);
+            MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE", 500, -1);
         }
     }
 
     if (MR::getAnimFrame(this, 1) == 35.0f) {
-        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 0x3E8, -1);
+        MR::startSystemSE("SE_SY_CTRL_GUIDE_CHANGE2", 1000, -1);
+    }
+}
+
+void SurfingGuidance::exeTurnRightHold() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "TurnRightHold", 1);
+    }
+}
+
+void SurfingGuidance::exeTurnRightSuccess() {
+    if (MR::isFirstStep(this)) {
+        MR::startAnim(this, "TurnRightOK", 1);
     }
 }
 
@@ -147,93 +205,5 @@ void SurfingGuidance::exeFadeOut() {
 }
 
 SurfingGuidance::~SurfingGuidance() {
-
+    
 }
-
-namespace {
-    INIT_NERVE(SurfingGuidanceFadeIn);
-    INIT_NERVE(SurfingGuidanceLevelOffReady);
-    INIT_NERVE(SurfingGuidanceLevelOffHold);
-    INIT_NERVE(SurfingGuidanceLevelOffSuccess);
-    INIT_NERVE(SurfingGuidanceTurnLeftReady);
-    INIT_NERVE(SurfingGuidanceTurnLeftHold);
-    INIT_NERVE(SurfingGuidanceTurnLeftSuccess);
-    INIT_NERVE(SurfingGuidanceTurnRightReady);
-    INIT_NERVE(SurfingGuidanceTurnRightHold);
-    INIT_NERVE(SurfingGuidanceTurnRightSuccess);
-    INIT_NERVE(SurfingGuidanceFadeOut);
-
-    void SurfingGuidanceFadeOut::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-        guide->exeFadeOut();
-    }
-
-    void SurfingGuidanceTurnRightSuccess::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "TurnRightOK", 1);
-        }
-    }
-
-    void SurfingGuidanceTurnRightHold::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "TurnRightHold", 1);
-        }
-    }
-
-    void SurfingGuidanceTurnRightReady::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-        guide->exeTurnRightReady();
-    }
-
-    void SurfingGuidanceTurnLeftSuccess::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "TurnLeftOK", 1);
-        }
-    }
-
-    void SurfingGuidanceTurnLeftHold::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "TurnLeftHold", 1);
-        }
-    }
-
-    void SurfingGuidanceTurnLeftReady::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-        guide->exeTurnLeftReady();
-    }
-
-    void SurfingGuidanceLevelOffSuccess::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "SlopeOK", 1);
-        }
-    }
-
-    void SurfingGuidanceLevelOffHold::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-
-        if (MR::isFirstStep(guide)) {
-            MR::startAnim(guide, "SlopeHold", 1);
-        }
-    }
-
-    void SurfingGuidanceLevelOffReady::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-        guide->exeLevelOffReady();
-    }
-
-    void SurfingGuidanceFadeIn::execute(Spine *pSpine) const {
-        SurfingGuidance* guide = reinterpret_cast<SurfingGuidance*>(pSpine->mExecutor);
-        guide->exeFadeIn();
-    }
-}
-
