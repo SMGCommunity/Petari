37aa9b77ea79a987fd872a45b2e11e1e87798ac6
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-17 22:25:04 -0700

    Match 97% of `WipeKoopa`

---
+++ include/Game/Screen/WipeKoopa.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Game/Screen/WipeLayoutBase.hpp"
+
+class WipeKoopa : public WipeLayoutBase {
+public:
+    WipeKoopa();
+
+    virtual ~WipeKoopa() {}
+    virtual void init(const JMapInfoIter&);
+    virtual void wipe(s32);
+    virtual void forceClose();
+    virtual void forceOpen();
+    virtual bool isOpen() const;
+    virtual bool isClose() const;
+    virtual bool isWipeIn() const;
+    virtual bool isWipeOut() const;
+
+    virtual s32 getWipeType() const {
+        return 2;
+    }
+
+    void exeWait();
+    void exeWipeOut();
+    void exeClose();
+
+private:
+    /* 0x20 */ s32 mStepNum;
+};
