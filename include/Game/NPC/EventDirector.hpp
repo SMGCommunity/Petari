8bccd3e108233f7f3e470e64f1fa7685540de00e
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-18 20:27:56 -0700

    Match `EventDirector`

--- include/Game/NPC/EventDirector.hpp
+++ include/Game/NPC/EventDirector.hpp
@@ -1,12 +1,49 @@
 #pragma once
 
-class PowerStarHolder;
+#include "Game/NameObj/NameObj.hpp"
 
-class EventFunction {
+class CometEventKeeper;
+class PowerStarEventKeeper;
+class PowerStarHolder;
+class StageStateKeeper;
+class TimeAttackEventKeeper;
+
+class EventDirector : public NameObj {
 public:
-    static PowerStarHolder* getPowerStarHolder();
+    /// @brief Creates a new `EventDirector`.
+    EventDirector();
+
+    /// @brief Destroys the `EventDirector`.
+    virtual ~EventDirector() {}
+
+    virtual void init(const JMapInfoIter& rIter);
+
+    /* 0x0C */ PowerStarEventKeeper* mPowerStarEventKeeper;
+    /* 0x10 */ StageStateKeeper* mStageStateKeeper;
+    /* 0x14 */ PowerStarHolder* mPowerStarHolder;
+    /* 0x18 */ CometEventKeeper* mCometEventKeeper;
+    /* 0x1C */ TimeAttackEventKeeper* mTimeAttackEventKeeper;
+};
+
+namespace EventFunction {
+    PowerStarEventKeeper* getPowerStarEventKeeper();
+    StageStateKeeper* getStageStateKeeper();
+    PowerStarHolder* getPowerStarHolder();
+    CometEventKeeper* getCometEventKeeper();
+    TimeAttackEventKeeper* getTimeAttackEventKeeper();
+    bool isStartCometEvent(const char*);
+    void startCometEvent();
+    void endCometEvent();
 };
 
 namespace MR {
+    void declareEventPowerStar(const char*, s32, bool);
     void initEventSystemAfterPlacement();
+    void declareEventPowerStar(const NameObj*, s32, bool);
+    bool isSuccessEventPowerStar(const char*, s32);
+    bool isGreenEventPowerStar(const char*, s32);
+    bool isRedEventPowerStar(const char*, s32);
+    bool isGrandEventPowerStar(const char*, s32);
+    void appearEventPowerStar(const char*, s32, const TVec3f*, bool, bool);
+    bool isEndEventPowerStarAppearDemo(const char*);
 };
