1f3820305e882368a57e1dcbd9804a54e0a12463
ThatNintendoNerd <samtheman1026@gmail.com>
2025-05-19 22:09:35 -0700

    Match 30% of `GameSystem`

--- src/Game/System/GameSystem.cpp
+++ src/Game/System/GameSystem.cpp
@@ -1,6 +1,10 @@
+#include "Game/LiveActor/Nerve.hpp"
+#include "Game/NameObj/NameObjRegister.hpp"
 #include "Game/System/FileRipper.hpp"
 #include "Game/System/GameSystem.hpp"
 #include "Game/System/GameSystemException.hpp"
+#include "Game/System/GameSystemObjHolder.hpp"
+#include "Game/System/GameSystemStationedArchiveLoader.hpp"
 #include "Game/System/HeapMemoryWatcher.hpp"
 #include "Game/Util/MathUtil.hpp"
 #include "Game/Util/MemoryUtil.hpp"
@@ -47,3 +51,82 @@ void main(void) {
         pGameSystem->frameLoop();
     }
 }
+
+namespace NrvGameSystem {
+    NEW_NERVE(GameSystemInitializeAudio, GameSystem, InitializeAudio);
+    NEW_NERVE(GameSystemInitializeLogoScene, GameSystem, InitializeLogoScene);
+    NEW_NERVE(GameSystemLoadStationedArchive, GameSystem, LoadStationedArchive);
+    NEW_NERVE(GameSystemWaitForReboot, GameSystem, WaitForReboot);
+    NEW_NERVE(GameSystemNormal, GameSystem, Normal);
+};
+
+GameSystem::GameSystem() :
+    NerveExecutor("GameSystem"),
+    mFifoBase(NULL),
+    mSequenceDirector(NULL),
+    mErrorWatcher(NULL),
+    mFontHolder(NULL),
+    mFrameControl(NULL),
+    mObjHolder(NULL),
+    mSceneController(NULL),
+    mStationedArchiveLoader(NULL),
+    mHomeButtonLayout(NULL),
+    mSystemWipeHolder(NULL),
+    mHomeButtonStateNotifier(NULL),
+    mIsExecuteLoadSystemArchive(false)
+{
+    
+}
+
+// GameSystem::init()
+
+bool GameSystem::isExecuteLoadSystemArchive() const {
+    return mIsExecuteLoadSystemArchive;
+}
+
+bool GameSystem::isDoneLoadSystemArchive() const {
+    return isNerve(&NrvGameSystem::GameSystemNormal::sInstance);
+}
+
+void GameSystem::startToLoadSystemArchive() {
+    mIsExecuteLoadSystemArchive = true;
+
+    SingletonHolder<HeapMemoryWatcher>::get()->setCurrentHeapToStationedHeap();
+    SingletonHolder<NameObjRegister>::get()->setCurrentHolder(mObjHolder->mObjHolder);
+    setNerve(&NrvGameSystem::GameSystemLoadStationedArchive::sInstance);
+}
+
+// GameSystem::exeInitializeAudio()
+// GameSystem::exeInitializeLogoScene()
+
+void GameSystem::exeLoadStationedArchive() {
+    mStationedArchiveLoader->update();
+    updateSceneController();
+
+    if (mStationedArchiveLoader->isDone()) {
+        setNerve(&NrvGameSystem::GameSystemNormal::sInstance);
+    }
+}
+
+void GameSystem::exeWaitForReboot() {
+    
+}
+
+void GameSystem::exeNormal() {
+    updateSceneController();
+    mStationedArchiveLoader->update();
+}
+
+// GameSystem::initGX()
+// GameSystem::initAfterStationedResourceLoaded()
+
+void GameSystem::prepareReset() {
+    mStationedArchiveLoader->prepareReset();
+}
+
+// GameSystem::isPreparedReset() const
+// GameSystem::frameLoop()
+// GameSystem::draw()
+// GameSystem::update()
+// GameSystem::updateSceneController()
+// GameSystem::calcAnim()
