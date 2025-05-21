118aa8ce887de603f65061c33a98a1230a169087
shibbo <supershibbo12@gmail.com>
2025-05-21 13:07:40 -0400

    map out `WalkerState` classes

---
+++ include/Game/Enemy/WalkerStateFindPlayer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Game/System/NerveExecutor.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+
+class WalkerStateParam;
+
+class WalkerStateFindPlayerParam {
+public:
+    WalkerStateFindPlayerParam();
+
+    u32 _0;
+    f32 _4;
+    f32 _8;
+};
+
+class WalkerStateFindPlayer : public ActorStateBase<LiveActor> {
+public:
+    WalkerStateFindPlayer(LiveActor *, TVec3f , WalkerStateParam *, WalkerStateFindPlayerParam *);
+
+    virtual ~WalkerStateFindPlayer();
+    virtual void appear();
+
+    void exeFind();
+    void exeFindJumpStart();
+    void exeFindJump();
+    void exeFindJumpEnd();
+    bool isInSightPlayer() const;
+    bool isFindJumpBegin() const;
+    bool isLandStart() const;
+
+    LiveActor* mParent;                                 // 0x0C
+    TVec3f* _10;
+    WalkerStateParam* mStateParam;                      // 0x14
+    WalkerStateFindPlayerParam* mFindPlayerParam;       // 0x18
+};
---
+++ src/Game/Enemy/WalkerStateChase.cpp
@@ -0,0 +1,8 @@
+#include "Game/Enemy/WalkerStateChase.hpp"
+
+WalkerStateChaseParam::WalkerStateChaseParam() {
+    _4 = 130;
+    _8 = 300;
+    _C = 2.0f;
+    _10 = 30;
+}
