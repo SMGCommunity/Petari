b45ec0534e73f421cf380d1840e084b3fab84712
ThePlayerRolo <thesolis78243@gmail.com>
2025-05-21 19:18:30 -0400

    NeedlePlant

---
+++ include/Game/MapObj/NeedlePlant.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Game/LiveActor/HitSensor.hpp"
+#include "Game/MapObj/MapObjActor.hpp"
+#include "Game/MapObj/MapObjActorInitInfo.hpp"
+
+class NeedlePlant : public MapObjActor {
+    public: 
+        NeedlePlant(const char *);
+        virtual ~NeedlePlant();
+        virtual void init(const JMapInfoIter &);
+        virtual void kill();
+        virtual void attackSensor(HitSensor *, HitSensor *);
+        virtual bool receiveMsgPush(HitSensor *, HitSensor *);
+        virtual bool receiveMsgPlayerAttack(u32, HitSensor *, HitSensor *);
+        virtual bool receiveMsgEnemyAttack(u32, HitSensor *, HitSensor *);
+        virtual void connectToScene(const MapObjActorInitInfo &);
+
+        void exeShake();
+        void exeWait();
+        s32 _C4; //0xC4
+};
