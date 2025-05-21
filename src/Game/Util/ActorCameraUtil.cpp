e9db27182b58b16772280c43616c1b504aa7cc1e
Purpal7 <haveyouanywool514@gmail.com>
2025-05-21 15:32:18 +1000

    `ActorCameraUtil` cleanup

--- src/Game/Util/ActorCameraUtil.cpp
+++ src/Game/Util/ActorCameraUtil.cpp
@@ -6,28 +6,6 @@
 #include "Game/Util/PlayerUtil.hpp"
 #include <cstdio>
 
-namespace {
-    /* char* createActorCameraName(char *pName, u32 a2, const LiveActor *pActor, const ActorCameraInfo *pInfo) {
-        if ((pInfo->mCameraSetID & 0x8000) != 0) {
-            snprintf(pName, a2, "", pActor->mName, pInfo->mCameraSetID - 0x8000);
-        }
-        else {
-            snprintf(pName, a2, "", pActor->mName);
-        }
-        return pName;
-    } */
-
-    /* char* createMultiActorCameraName(char *pNewName, u32 a2, const LiveActor *pActor, const ActorCameraInfo *pInfo, char *pName) {
-        if ((pInfo->mCameraSetID & 0x8000) != 0) {
-            snprintf(pNewName, a2, "", pActor->mName, pName, pInfo->mCameraSetID - 0x8000);
-        }
-        else {
-            snprintf(pNewName, a2, "", pActor->mName);
-        }
-        return pNewName;
-    } */
-};
-
 namespace MR {
     ActorCameraInfo* createActorCameraInfo(const JMapInfoIter &rIter) {
         ActorCameraInfo* pInfo = new ActorCameraInfo(rIter);
@@ -54,24 +32,6 @@ namespace MR {
         }
         return initMultiActorCameraNoInit(pActor, (*pInfo), pName);
     }
-
-    /* bool initMultiActorCameraNoInit(const LiveActor *pActor, ActorCameraInfo *pInfo, const char *pName) {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            declareEventCamera(pInfo, pNewName);
-            return true;
-        }
-    } */
-
 
     void initActorCameraProgrammable(const LiveActor *pActor) {
         declareEventCameraProgrammable(pActor->mName);
@@ -115,52 +75,7 @@ namespace MR {
     bool startMultiActorCameraTargetSelf(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, s32 a4) {
         return startMultiActorCameraTargetOther(pActor, pInfo, pName, CameraTargetArg(pActor), a4);
     }
-
-    /* bool startMultiActorCameraTargetOther(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, const CameraTargetArg &target, s32 a5) {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            startEventCamera(pInfo, pNewName, target, a5);
-            return true;
-        }
-    } */
-
-    /* void startAnimCameraTargetPlayer(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, s32 a4, f32 a5) {
-        CameraTargetArg target = CameraTargetArg();
-        setCameraTargetToPlayer(&target);
-        char* pNewName;
-        snprintf(pNewName, 128, "", pActor->mName, pName);
-        startEventCameraAnim(pInfo, pNewName, target, a4, a5);
-        return;
-    }
-    90.28% match */ 
-    
-    /* void startAnimCameraTargetSelf(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, s32 a4, f32 a5) {
-        CameraTargetArg target = CameraTargetArg(pActor);
-        char* pNewName;
-        snprintf(pNewName, 128, "", pActor->mName, pName);
-        startEventCameraAnim(pInfo, pNewName, target, a4, a5);
-        return;
-    }
-    90.00% match */
-
-    /* void startAnimCameraTargetOther(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, const CameraTargetArg &target, s32 a4, f32 a5) {
-        char* pNewName;
-        snprintf(pNewName, 128, "", pActor->mName, pName);
-        startEventCameraAnim(pInfo, pNewName, target, a4, a5);
-        return;
-    }
-    80.81% match */
      
-
     void startActorCameraProgrammable(const LiveActor *pActor, s32 a2) {
         startGlobalEventCameraNoTarget(pActor->mName, a2);
     }
@@ -170,44 +85,10 @@ namespace MR {
         
     }
 
-    /* bool endMultiActorCamera(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, bool a4, s32 a5) {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            endEventCamera(pInfo, pNewName, a4, a5);
-            return true;
-        }
-    } */
-
     bool endActorCameraAtLanding(const LiveActor *pActor, const ActorCameraInfo *pInfo, s32 a3) {
         return endMultiActorCameraAtLanding(pActor, pInfo, nullptr, a3);
         
     }
-
-    /* bool endMultiActorCameraAtLanding(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, s32 a4) {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            endEventCameraAtLanding(pInfo, pNewName, a4);
-            return true;
-        }
-    } */
 
     void endActorCameraProgrammable(const LiveActor *pActor, s32 a2, bool a3) {
         endGlobalEventCamera(pActor->mName, a2, a3);
@@ -218,23 +99,6 @@ namespace MR {
         return isActiveMultiActorCamera(pActor, pInfo, nullptr);
     }
 
-    /* bool isActiveMultiActorCamera(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            isEventCameraActive(pInfo, pNewName);
-            return true;
-        }
-    } */
-
     void setProgrammableCameraParam(const LiveActor *pActor, const TVec3f &v1, const TVec3f &v2, const TVec3f &v3) {
         setProgrammableCameraParam(pActor->mName, v1, v2, v3, true);
         return;
@@ -244,69 +108,6 @@ namespace MR {
         setProgrammableCameraParamFovy(pActor->mName, fov);
         return;
     }
-
-    /* void initAnimCamera(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName1, const char *pName2) {
-        char* pNewName1;
-        snprintf(pNewName1, 128, "", pActor->mName, pName2);
-        char* pNewName2;
-        snprintf(pNewName2, 64, "", pName2);
-        loadResourceFromArc(pName1, pNewName2);
-        declareEventCameraAnim(pInfo, pNewName1, (void*) pName1);
-        return;
-
-    } */
-
-    /* void endAnimCamera(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName, s32 a4, bool a5) {
-        char* pNewName;
-        snprintf(pNewName, 128, "", pActor->mName, pName);
-        endEventCamera(pInfo, pNewName, a5, a4);
-        return;
-    } */
-
-    /* s32 getAnimCameraFrame(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName) {
-        char* pNewName;
-        snprintf(pNewName, 256, "", pActor->mName, pName);
-        return getAnimCameraFrame(pInfo, pNewName);
-    } */
-
-    /* bool isAnimCameraEnd(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName) {
-        char* pNewName;
-        snprintf(pNewName, 256, "", pActor->mName, pName);
-        return isAnimCameraEnd(pInfo, pNewName);
-    } */
-
-    /* s32 getActorCameraFrames(const LiveActor *pActor, const ActorCameraInfo *pInfo) {
-        getMultiActorCameraFrames(pActor, pInfo, nullptr);
-    } */
-
-    /* bool getMultiActorCameraFrames(const LiveActor *pActor, const ActorCameraInfo *pInfo, const char *pName {
-        char *pNewName;
-        if (pInfo->mCameraSetID < 0) {
-            return false;
-        }
-        else {
-            if (pName == nullptr) {
-                pNewName = ::createActorCameraName(pNewName, 256, pActor, pInfo);
-            }
-            else {
-                pNewName = ::createMultiActorCameraName(pNewName, 256, pActor, pInfo, pName);
-            }
-            getEventCameraFrames(pInfo, pNewName);
-            return true;
-        }
-    } */
-
-    /* bool isExistActorCamera(const ActorCameraInfo *pInfo) {
-        if (pInfo == nullptr) {
-            return false;
-        }
-        else if (pInfo->mCameraSetID == -1) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    } */
      
     void startRumbleWithShakeCameraWeak(const LiveActor *pActor, const char *pName1, const char *pName2, f32 range1, f32 range2) {
         if (calcDistanceToPlayer(pActor) <= range1) {
