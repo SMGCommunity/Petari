b18b944cb4f32d3fde66f5bed459fdb82ab85a40
VTXG <VTXG@users.noreply.github.com>
2025-05-24 21:32:51 +0100

    Decompile CenterScreenBlur

---
+++ include/Game/Screen/CenterScreenBlur.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/LiveActor/Nerve.hpp"
+#include <revolution/types.h>
+
+class CenterScreenBlur : public LiveActor {
+public:
+    CenterScreenBlur();
+    ~CenterScreenBlur();
+
+    virtual void init(JMapInfoIter const &);
+    virtual void appear();
+    virtual void draw() const;
+
+    void start(s32, f32, bool, s32, s32);
+    void exeFadeIn();
+    void exeKeep();
+    void exeFadeOut();
+
+    s32 _8C;
+    s32 _90;
+    s32 _94;
+    f32 _98;
+    bool _9C;
+    f32 _A0;
+};
+
+namespace NrvCenterScreenBlur {
+    NEW_NERVE(CenterScreenBlurNrvFadeIn, CenterScreenBlur, FadeIn);
+    NEW_NERVE(CenterScreenBlurNrvKeep, CenterScreenBlur, Keep);
+    NEW_NERVE(CenterScreenBlurNrvFadeOut, CenterScreenBlur, FadeOut);
+};
