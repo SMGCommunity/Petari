91205c39e9478ed463f9f92e7bba45ead9c391e6
Swift <swiftshineexists@gmail.com>
2025-05-24 21:58:25 -0500

    move nerve to source file

--- include/Game/MapObj/ChipCounter.hpp
+++ include/Game/MapObj/ChipCounter.hpp
@@ -18,13 +18,11 @@ public:
     void requestActive();
     void requestDeactive();
     
+    void exeHide();
+
     CollectCounter* mCollectCounter;    // 0x20
     s32 mCount;                         // 0x24
     s32 mType;                          // 0x28
     s32 _2C;
     f32 _30;
-};
-
-namespace NrvChipCounter {
-    NERVE(ChipCounterNrvHide);
 };
