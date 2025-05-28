0d489f6ddc9ee284ed3a4e614df692ad0dfa2b56
ThePlayerRolo <thesolis78243@gmail.com>
2025-05-27 20:04:00 -0400

    SHOULD BE DONE EUGH

--- include/Game/MapObj/PeachCastleGardenPlanet.hpp
+++ include/Game/MapObj/PeachCastleGardenPlanet.hpp
@@ -2,17 +2,17 @@
 
 #include "Game/MapObj/MapObjActor.hpp"
 class PeachCastleGardenPlanet : public MapObjActor {
-public:
-    PeachCastleGardenPlanet(const char *);
+    public:
+        PeachCastleGardenPlanet(const char *);
 
-    virtual ~PeachCastleGardenPlanet();
-    virtual void init(const JMapInfoIter &);
-    virtual void connectToScene(const MapObjActorInitInfo &);
-    virtual void initCaseUseSwitchA(const MapObjActorInitInfo &);
+        virtual ~PeachCastleGardenPlanet();
+        virtual void init(const JMapInfoIter &);
+        virtual void connectToScene(const MapObjActorInitInfo &);
+        virtual void initCaseUseSwitchA(const MapObjActorInitInfo &);
     
-    void startDamage();
-    void exeWait();
-    void exeDamage();
+        void startDamage();
+        void exeWait();
+        void exeDamage();
 
-    u32 _C4;
+        u32 _C4;
 };
--- include/Game/MapObj/TransparentWall.hpp
+++ include/Game/MapObj/TransparentWall.hpp
@@ -3,12 +3,12 @@
 #include "Game/MapObj/InvisiblePolygonObj.hpp"
 class TransparentWall : public InvisiblePolygonObj {
     public:
-    TransparentWall(const char *);
+        TransparentWall(const char *);
 
-    virtual ~TransparentWall();
-    virtual void init(const JMapInfoIter &);
-    virtual void control();
+        virtual ~TransparentWall();
+        virtual void init(const JMapInfoIter &);
+        virtual void control();
 
-    s32 _BC;
-    bool _C0;
+        s32 _BC;
+        bool _C0;
 };
