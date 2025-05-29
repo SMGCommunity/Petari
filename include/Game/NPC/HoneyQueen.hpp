75888d9403abdb615464e382a472a06ee0cb8ebc
Gabbo <74122269+Gabbo089@users.noreply.github.com>
2025-05-29 04:19:40 +0200

    ``HoneyQueen``

---
+++ include/Game/NPC/HoneyQueen.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Game/Map/CollisionParts.hpp"
+#include "Game/NPC/NPCActor.hpp"
+#include "Game/Util/JMapInfo.hpp"
+#include "Game/Util/NPCUtil.hpp"
+#include "revolution/types.h"
+
+class HoneyQueen : public NPCActor {
+public:
+    HoneyQueen(const char *);
+
+    virtual ~HoneyQueen();
+    virtual void init(const JMapInfoIter &);
+    virtual void calcAnim();
+    virtual void control();
+    virtual void attackSensor(HitSensor *, HitSensor *);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor *, HitSensor *);
+
+    void fadeOut();
+    void fadeIn();
+    void talkEntry();
+    void switchFunc();
+    void exeWait();
+    inline void exeReady();
+    inline void exeDemo();
+    inline void exeFade();
+    void exeTalk();
+    void exeItch();
+    void exeEvent();
+    inline void exeAfter();
+
+    ModelObj* mNpcModel;
+    CollisionParts* mCenterPart;
+    CollisionParts* mCenterFurPart;
+    CollisionParts* mFacePart;
+    CollisionParts* mLArm01Part;
+    CollisionParts* mLArm02Part;
+    CollisionParts* mLFoot01Part;
+    CollisionParts* mRArm01Part;
+    CollisionParts* mRArm02Part;
+    CollisionParts* mRFoot01Part;
+    CollisionParts* mTactilePart;
+    s32 _188;
+};
