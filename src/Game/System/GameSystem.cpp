e5bf42b1cb01580de730b94fbcf09f5467c2a8ba
Sean Miller <seanmiller999@gmail.com>
2025-06-04 13:23:54 -0700

    Replace asm in main() with call to OSInitFastCast

--- src/Game/System/GameSystem.cpp
+++ src/Game/System/GameSystem.cpp
@@ -41,21 +41,7 @@ namespace NrvGameSystem {
 };
 
 void main(void) {
-    __asm {
-        li r3, 4
-        oris r3, r3, 4
-        mtspr 0x392, r3
-        li r3, 5
-        oris r3, r3, 5
-        mtspr 0x393, r3
-        li r3, 6
-        oris r3, r3, 6
-        mtspr 0x394, r3
-        li r3, 7
-        oris r3, r3, 7
-        mtspr 0x395, r3
-    };
-
+    OSInitFastCast();
     DVDInit();
     VIInit();
     HeapMemoryWatcher::createRootHeap();
