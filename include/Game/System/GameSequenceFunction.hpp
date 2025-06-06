5950c843e828972802c385fc00f7991d7935283c
Purpal7 <haveyouanywool514@gmail.com>
2025-06-06 19:27:59 +1000

    Update headers needed for `EventUtil`

--- include/Game/System/GameSequenceFunction.hpp
+++ include/Game/System/GameSequenceFunction.hpp
@@ -11,4 +11,6 @@ public:
 
     static bool isActiveSaveDataHandleSequence();
     static void startGameDataSaveSequence(bool, bool);
+
+    static void sendStageResultSequenceParam(const char *, s32, int, int);
 };
