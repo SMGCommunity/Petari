e49777b663738f0bf31d51b7ceec9f1535cdfe32
Purpal7 <haveyouanywool514@gmail.com>
2025-06-24 09:05:21 +1000

    Match `RushEndInfo`

--- include/Game/Player/RushEndInfo.hpp
+++ include/Game/Player/RushEndInfo.hpp
@@ -5,5 +5,11 @@
 class RushEndInfo {
 public:
     RushEndInfo(LiveActor *, u32, const TVec3f &, bool, u32);
-    u8 _0[0x24];
+    u32 _0;
+    u32 _4;
+    TVec3f _8;
+    bool _14;
+    u32 _18;
+    LiveActor* _1C;
+    u32 _20;
 };
