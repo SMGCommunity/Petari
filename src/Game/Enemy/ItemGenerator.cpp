fbc2e647b1fdc754f403850ffddf795b5ae37a73
ThePlayerRolo <thesolis78243@gmail.com>
2025-06-26 17:49:56 -0400

    ItemGenerator Mostly Matching Besides Inline :(

--- src/Game/Enemy/ItemGenerator.cpp
+++ src/Game/Enemy/ItemGenerator.cpp
@@ -1,26 +1,53 @@
 #include "Game/Enemy/ItemGenerator.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/Util/ObjUtil.hpp"
 #include "Game/Util/SceneUtil.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
 
 ItemGenerator::ItemGenerator() {
     mNumType = 1;
-    mType = Coin;
+    mType = TYPE_COIN;
 }
 
 void ItemGenerator::setTypeNone() {
+    mType = TYPE_NONE;
     mNumType = 0;
-    mType = None;
 }
 
 void ItemGenerator::setTypeCoin(s32 arg0) {
     mNumType = arg0;
-    mType = Coin; 
+    mType = TYPE_COIN; 
 }
 
 void ItemGenerator::setTypeStarPeace(s32 arg0) {
     mNumType = arg0;
-    mType = StarPiece; 
+    mType = TYPE_STARPIECE; 
 }
 
+
+
+//Adding the top function messes stuff up without the hack
+
+void ItemGenerator::generate(const LiveActor* pActor) {
+    if (mNumType > 0) {
+        switch (mType) {
+            case 1:
+            MR::appearCoinPopToDirection(pActor, pActor->mPosition, -pActor->mGravity, mNumType);
+            break;
+            case 2:
+            MR::appearStarPieceToDirection(pActor, pActor->mPosition,  -pActor->mGravity, mNumType, 10.0f, 40.0f, false);
+            if (ItemGenerator::isUseFarSE()) {
+                MR::startSound(pActor, "SE_OJ_STAR_PIECE_BURST_F", -1, -1);
+            }
+            else {
+                MR::startSound(pActor, "SE_OJ_STAR_PIECE_BURST", -1, -1);
+            }
+            break;
+        }
+    }
+}
+
+//Gets Inlined :(
 bool ItemGenerator::isUseFarSE() const {
     return MR::isEqualStageName("KoopaBattleVs2Galaxy");
 }
