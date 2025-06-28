b29a7b374ba237da32e3e2ed12b935dce6155bfd
ThePlayerRolo <thesolis78243@gmail.com>
2025-06-28 19:08:00 -0400

    Some work on bomb teresa

---
+++ include/Game/Enemy/BombTeresa.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "Game/Enemy/AnimScaleController.hpp"
+#include "Game/Enemy/WalkerStateBindStarPointer.hpp"
+#include "Game/LiveActor/ActorStateBase.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/Util/JMapInfo.hpp"
+#include "Game/Util/JointController.hpp"
+#include "JSystem/JGeometry/TMatrix.hpp"
+#include "JSystem/JGeometry/TQuat.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+
+
+class BombTeresa : public LiveActor {
+public:
+    BombTeresa(const char *);
+
+    ~BombTeresa();
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void makeActorAppeared();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor *, HitSensor *);
+    virtual bool receiveMsgPush(HitSensor *, HitSensor *);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor *, HitSensor *);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor *, HitSensor *);
+    virtual bool receiveOtherMsg(u32, HitSensor *, HitSensor *);
+ 
+    void initTongue();
+    void initSensor();
+    void initFromJMapParam(const JMapInfoIter&);
+    void exeAppear();
+    void exeShadowAppear();
+    void exeBallAppear();
+    void exeWait();
+    void exeWander();
+    void exeChase();
+    void exeAttackTongueFailed();
+    void exeDirectTackleSign();
+    void exeDirectTackle();
+    void exeDrift();
+    void exeDriftRelease();
+    void exeExplosion();
+    void exeShock();
+    void exeDisperse();
+    void exeReadyRestart();
+    void exeBindStarPointer();
+    void tryCheseEnd();
+    void tryDirectTackle();
+    void tryAbortDrift();
+    void tryDriftRelease();
+    void tryExplosion();
+    void tryRevival();
+    void requestDrift();
+    void requestDisperse();
+    void appearNormal(const TVec3f &, const TVec3f &);
+    void appearShadow(const TVec3f &, const TVec3f &);
+    bool rootTongueMtxCallBack(TPos3f*, const JointControllerInfo&);
+    bool endTongueMtxCallBack(TPos3f*, const JointControllerInfo&);
+    void updateNormalVelocity();
+    void updateDriftReleaseVelocity();
+    bool isTouchTongue() const;
+    bool isEnableDrift() const;
+    bool isEnablePointBind() const;
+    bool isEnableDisperse() const;
+    bool isEnableHitExplosionToPlayer() const;
+    bool isEnableHitExplosionToEnemy() const;
+    bool isEnableHitExplosion() const;
+    bool isEnableStarPieceAttack() const;
+    bool isEnableShockWave() const;
+    void addTeresaSpinPullVelocity(f32);
+
+    inline void applyScale(f32 scale) {
+        mScale.x *= scale;
+        mScale.y *= scale;
+        mScale.z *= scale;
+    }
+
+    s32 _8C;
+    s32 _90;
+    AnimScaleController* _94;
+    WalkerStateBindStarPointer* _98;
+    TQuat4f _9C;
+    TVec3f _AC;
+    TVec3f _B8;
+    TVec3f _C4;
+    TVec3f _D0;
+    f32 _DC;
+    f32 _E0;
+    f32 _E4;
+    s32 _E8;
+    bool  _EC;
+    bool mDisableRespawning;
+    bool  _EE;
+    bool  _EF;
+
+    JointControlDelegator<BombTeresa>* mJointDelegator; //Probably wrong offset 
+};
---
+++ src/Game/Enemy/BombTeresa.cpp
@@ -0,0 +1,108 @@
+#include "Game/Enemy/BombTeresa.hpp"
+#include "Game/Enemy/AnimScaleController.hpp"
+#include "Game/Enemy/WalkerStateBindStarPointer.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/LiveActor/Nerve.hpp"
+#include "Game/Util/ActorMovementUtil.hpp"
+#include "Game/Util/ActorSwitchUtil.hpp"
+#include "Game/Util/JMapInfo.hpp"
+#include "Game/Util/JMapUtil.hpp"
+#include "Game/Util/JointController.hpp"
+#include "Game/Util/LiveActorUtil.hpp"
+#include "Game/Util/MapUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+#include "revolution/types.h"
+
+namespace NrvBombTeresa {
+    NEW_NERVE(BombTeresaNrvAppear, BombTeresa, Appear);
+    NEW_NERVE(BombTeresaNrvShadowAppear, BombTeresa, ShadowAppear);
+    NEW_NERVE(BombTeresaNrvBallAppear, BombTeresa, BallAppear);
+    NEW_NERVE(BombTeresaNrvWait, BombTeresa, Wait);
+    NEW_NERVE(BombTeresaNrvWander, BombTeresa, Wander);
+    NEW_NERVE(BombTeresaNrvChase, BombTeresa, Chase);
+    NEW_NERVE(BombTeresaNrvAttackTongueFailed, BombTeresa, AttackTongueFailed);
+    NEW_NERVE(BombTeresaNrvDirectTackleSign, BombTeresa, DirectTackleSign);
+    NEW_NERVE(BombTeresaNrvDirectTackle, BombTeresa, DirectTackle);
+    NEW_NERVE(BombTeresaNrvDrift, BombTeresa, Drift);
+    NEW_NERVE(BombTeresaNrvDriftRelease, BombTeresa, DriftRelease);
+    NEW_NERVE(BombTeresaNrvBindStarPointer, BombTeresa, BindStarPointer);
+}
+
+BombTeresa::BombTeresa(const char* pName) 
+    : LiveActor(pName),
+      _8C(0),
+      _90(0),
+      _94(nullptr),
+      _98(nullptr),
+      _9C(0.0f, 0.0f, 0.0f, 1.0f),
+      _AC(0 ,0 ,1),
+      _B8(0, 0, 0),
+      _C4(0, 0, 0),
+      _D0(0, 0, 0),
+      _DC(1.0f),
+      _E0(1.0f),
+      _E4(1.0f),
+      _E8(0),
+      _EC(false),
+      mDisableRespawning(false),
+      _EE(true),
+      _EF(false)
+{
+}
+
+void BombTeresa::init(const JMapInfoIter & rIter) {
+    initFromJMapParam(rIter);
+    initModelManagerWithAnm("BombTeresa", 0, 0);
+    MR::connectToSceneEnemy(this);
+    MR::initLightCtrl(this);
+    initSensor();
+    f32 scale = mScale.x;
+    scale = scale *  70.0f;
+    initBinder(scale, 0.0f, 0);
+    initEffectKeeper(1, nullptr, false);
+    initSound(8, false);
+    MR::initShadowVolumeSphere(this, 80.0f);
+    MR::onShadowFollowHostScale(this, nullptr);
+    initTongue();
+    initNerve(&NrvBombTeresa::BombTeresaNrvAppear::sInstance);
+    _94 = new AnimScaleController(nullptr);
+    _98 = new WalkerStateBindStarPointer(this, _94);
+    if (MR::isValidInfo(rIter)) {
+        if (MR::useStageSwitchReadAppear(this, rIter)) {
+            MR::syncStageSwitchAppear(this);
+            makeActorDead();
+            return;
+        }
+        makeActorAppeared();
+        return;
+    }
+    makeActorDead();
+}
+
+void BombTeresa::initAfterPlacement() {
+    MR::trySetMoveLimitCollision(this);
+}
+
+//The 2.0 needs to be 1.0
+void BombTeresa::initFromJMapParam(const JMapInfoIter & rIter) {
+    if (MR::isValidInfo(rIter)) {
+        MR::initDefaultPos(this, rIter);
+        applyScale(1.0f);
+        MR::makeQuatAndFrontFromRotate(&_9C, &_AC, this);
+        _D0 = mPosition;
+        bool Arg1Bool;
+        MR::getJMapInfoArg1WithInit(rIter, &Arg1Bool);
+        mDisableRespawning = Arg1Bool == false;
+        MR::useStageSwitchWriteDead(this, rIter);
+    }
+}
+
+
+void BombTeresa::initTongue() {
+    mJointDelegator = new JointControlDelegator<BombTeresa>(this, &BombTeresa::rootTongueMtxCallBack, &BombTeresa::rootTongueMtxCallBack);
+}
+
+void BombTeresa::initSensor() {
+    
+}
