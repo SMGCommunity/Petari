5eb6ff8b8eafbc51f8e755c4e7d585572f377d5d
VTXG <VTXG@users.noreply.github.com>
2025-07-10 01:10:04 +0100

    Create WPad related headers

---
+++ include/Game/System/WPadAcceleration.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "JSystem/JGeometry/TVec.hpp"
+#include <revolution.h>
+
+class WPad;
+
+class WPadAcceleration {
+public:
+    WPadAcceleration(const WPad*, u32);
+
+    void getAcceleration(TVec3f *) const;
+    void getPastAcceleration(TVec3f *, s32) const;
+    bool isStationary() const;
+    bool isBalanced() const;
+    void update();
+    void updateRotate();
+    void updateAccAverage();
+    void updateIsStable();
+
+    // TODO document members
+    u8 _0[0x650];
+};
