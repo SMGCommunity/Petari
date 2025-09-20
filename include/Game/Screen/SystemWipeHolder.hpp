c3d861523319c93c46782c878260e9c80cfc92e0
ThatNintendoNerd <samtheman1026@gmail.com>
2025-09-20 00:04:42 -0700

    Fully define `SystemWipeHolder`

--- include/Game/Screen/SystemWipeHolder.hpp
+++ include/Game/Screen/SystemWipeHolder.hpp
@@ -1,13 +1,34 @@
 #pragma once
 
 #include "Game/Screen/WipeHolderBase.hpp"
+#include <JSystem/JGeometry/TVec.hpp>
 
 class SystemWipeHolder : public WipeHolderBase {
 public:
+    /// @brief Creates a new `SystemWipeHolder`.
     SystemWipeHolder();
-    ~SystemWipeHolder();
 
-    u8 _1C;
+    /// @brief Destroys the `SystemWipeHolder`.
+    virtual ~SystemWipeHolder() {}
+
+    /// @brief Intializes the `SystemWipeHolder` while being placed into a scene.
+    /// @param rIter The reference to an iterator over a `JMapInfo`.
+    virtual void init(const JMapInfoIter& rIter);
+
+    virtual void movement();
+
+    /// @brief Draws the `SystemWipeHolder` to the screen.
+    virtual void draw() const;
+
+    virtual void calcAnim();
+    virtual void updateWipe(const char*);
+
+    void setWipeRingCenter(const TVec3f&);
+    void startGameScreenCapture();
+    void drawGameScreenCapture() const;
+
+private:
+    /* 0x1C */ bool _1C;
 };
 
 namespace MR {
