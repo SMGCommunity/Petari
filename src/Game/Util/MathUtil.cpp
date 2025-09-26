d43880272985b61de262e5332b3b9c850220a72f
ThatNintendoNerd <samtheman1026@gmail.com>
2025-09-26 14:48:14 -0700

    Improve `MathUtil` matches

--- src/Game/Util/MathUtil.cpp
+++ src/Game/Util/MathUtil.cpp
@@ -1,37 +1,43 @@
+#include "Game/System/GameSystem.hpp"
+#include "Game/System/GameSystemObjHolder.hpp"
 #include "Game/Util/MathUtil.hpp"
-#include "JSystem/JGeometry/TUtil.hpp"
-#include "JSystem/JMath/JMath.hpp"
-#include "JSystem/JMath/JMATrigonometric.hpp"
+#include "Game/Util/MtxUtil.hpp"
+#include "Game/SingletonHolder.hpp"
+#include <JSystem/JGeometry/TUtil.hpp>
+#include <JSystem/JMath/JMATrigonometric.hpp>
+#include <JSystem/JMath/JMath.hpp>
 
-#include "math_types.hpp"
-
-#include <cmath>
-
-static f32 minDegree = 0.0f;
-static f32 maxDegree = 360.0f;
+namespace {
+    static f32 sMinDegree = 0.0f;
+    static f32 sMaxDegree = 360.0f;
+};
 
 namespace MR {
-    f32 getRandom(f32 min, f32 max) {
-        return min + ((max - min) * getRandom());
+    f32 getRandom() {
+        u32 rand = SingletonHolder<GameSystem>::get()->mObjHolder->mRandom.rand();
+        u32 value = (rand >> 9) | 0x3F800000;
+
+        return reinterpret_cast<f32&>(value) - 1.0f;
     }
 
-    // getRandom(s32, s32)
+    f32 getRandom(f32 min, f32 max) {
+        return min + (max - min) * getRandom();
+    }
+
+    s32 getRandom(s32 min, s32 max) {
+        return static_cast<f32>(min) + (static_cast<f32>(max) - static_cast<f32>(min)) * getRandom();
+    }
 
     f32 getRandomDegree() {
-        return minDegree + (maxDegree * getRandom());
+        return sMinDegree + sMaxDegree * getRandom();
     }
 
-    void calcRandomVec(TVec3f *pOut, f32 a2, f32 a3) {
-        f32 v10 = (a2 + ((a3 - a2) * getRandom()));
-        f32 v11 = (a2 + ((a3 - a2) * getRandom()));
-        f32 v12 = getRandom();
-
-        f32 dist = a3 - a2;
-        pOut->set((a2 + (dist * v12)), v11, v10);
+    void calcRandomVec(TVec3f *pDst, f32 min, f32 max) {
+        pDst->set<f32>(getRandom(min, max), getRandom(min, max), getRandom(min, max));
     }
 
     bool isHalfProbability() {
-        return MR::getRandom() < 0.5f;
+        return getRandom() < 0.5f;
     }
 
     f32 getSignHalfProbability() {
@@ -43,19 +49,16 @@ namespace MR {
         }
     }
 
-    void getRandomVector(TVec3f *pOut, f32 a2) {
-        f32 z = (-a2 + ((a2 - -a2) * getRandom()));
-        f32 y = (-a2 + ((a2 - -a2) * getRandom()));
-        pOut->set((-a2 + ((a2 - -a2) * getRandom())), y, z);
+    void getRandomVector(TVec3f* pDst, f32 range) {
+        pDst->set<f32>(getRandom(-range, range), getRandom(-range, range), getRandom(-range, range));
     }
 
     #ifdef NON_MATCHING
     // stack places randVec and otherVec wrongly
     void addRandomVector(TVec3f *pOut, const TVec3f &rOtherVec, f32 a3) {
-        
-        f32 x = -a3 + ((a3 - -a3) * getRandom());
-        f32 y = -a3 + ((a3 - -a3) * getRandom());
-        f32 z = -a3 + ((a3 - -a3) * getRandom());
+        f32 x = getRandom(-range, range);
+        f32 y = getRandom(-range, range);
+        f32 z = getRandom(-range, range);
 
         TVec3f randVec;
         randVec.x = x;
@@ -68,50 +71,49 @@ namespace MR {
     }
     #endif
 
-    void turnRandomVector(TVec3f *pOut, const TVec3f &rOtherVec, f32 a3) {
-        f32 mag = PSVECMag((Vec*)(&rOtherVec));
-        addRandomVector(pOut, rOtherVec, a3);
+    void turnRandomVector(TVec3f* pDst, const TVec3f& rSrc, f32 range) {
+        f32 srcLength = PSVECMag(&rSrc);
 
-        if (isNearZero(*pOut, 0.001f)) {
-            pOut->set<f32>(rOtherVec);
+        addRandomVector(pDst, rSrc, range);
+
+        if (isNearZero(rSrc, 0.001f)) {
+            pDst->set(rSrc);
         }
         else {
-            pOut->setLength(mag);
+            pDst->setLength(srcLength);
         }
     }
 
-
-    f32 getInterpolateValue(f32 a1, f32 a2, f32 a3) {
-        return (a2 + (a1 * (a3 - a2)));
+    f32 getInterpolateValue(f32 t, f32 start, f32 end) {
+        return start + (end - start) * t;
     }
 
-    f32 getLinerValue(f32 a1, f32 a2, f32 a3, f32 a4) {
-        return (a2 + (a1 / a4) * (a3 - a2));
+    f32 getLinerValue(f32 x, f32 start, f32 end, f32 w) {
+        return getInterpolateValue(x / w, start, end);
     }
 
-    f32 getLinerValueFromMinMax(f32 a1, f32 a2, f32 a3, f32 a4, f32 a5) {
-        f32 val = (JGeometry::TUtil<f32>::clamp(a1, a2, a3) - a2) / (a3 - a2);
-        return a4 + ((a5 - a4) * val);
+    f32 getLinerValueFromMinMax(f32 x, f32 min, f32 max, f32 start, f32 end) {
+        return getInterpolateValue((JGeometry::TUtil<f32>::clamp(x, min, max) - min) / (max - min), start, end);
     }
 
     // FIXME: Source registers swapped in multiplication by pi.
-    f32 getEaseInValue(f32 a1, f32 a2, f32 a3, f32 a4) {
-        f32 val = (1.0f - JMACosRadian((((a1 / a4) * PI) / 2.0f)));
-        return a2 + ((a3 - a2) * val);
+    f32 getEaseInValue(f32 x, f32 start, f32 end, f32 w) {
+        return getInterpolateValue(1.0f - JMACosRadian(((x / w) * PI) / 2.0f), start, end);
     }
 
-    #ifdef NON_MATCHING
-    // wrong register orders
-    f32 getEaseOutValue(f32 a1, f32 a2, f32 a3, f32 a4) {
-        f32 val = (JMASinRadian((((a1 / a4) * PI) * 0.5f)) * (a3 - a2));
-        return a2 + val;
+    // FIXME: Source registers swapped in multiplication by pi.
+    f32 getEaseOutValue(f32 x, f32 start, f32 end, f32 w) {
+        return getInterpolateValue(JMASinRadian(((x / w) * PI) / 2.0f), start, end);
     }
-    #endif
 
-    // MR::getEaseInOutValue
-    // MR::getScaleWithReactionValueZeroToOne
-    // MR::getConvergeVibrationValue
-    // MR::getReduceVibrationValue
+    // FIXME: Source registers swapped in multiplication by pi, and the instruction is swapped.
+    f32 getEaseInOutValue(f32 x, f32 start, f32 end, f32 w) {
+        return getInterpolateValue((1.0f - JMACosRadian((x / w) * PI)) / 2.0f, start, end);
+    }
+
+    // getScaleWithReactionValueZeroToOne
+    // getConvergeVibrationValue
+    // getReduceVibrationValue
 
     void makeAxisFrontUp(TVec3f *a1, TVec3f *a2, const TVec3f &a3, const TVec3f &a4) {
         PSVECCrossProduct((Vec*)&a4, (Vec*)&a3, (Vec*)a1);
@@ -119,36 +121,71 @@ namespace MR {
         PSVECCrossProduct((Vec*)&a3, (Vec*)a1, (Vec*)a2);
     }
 
-    // MR::makeAxisFrontSide
-    // MR::makeAxisUpFront
-    // MR::makeAxisUpSide
-    // MR::makeAxisVerticalZX
-    // MR::makeAxisCrossPlane
-    // MR::makeAxisAndCosignVecToVec
-    // MR::calcPerpendicFootToLine
-    // MR::calcPerpendicFootToLineInside
+    // makeAxisFrontSide
+    // makeAxisUpFront
+    // makeAxisUpSide
+    // makeAxisVerticalZX
+    // makeAxisCrossPlane
+    // makeAxisAndCosignVecToVec
+    // calcPerpendicFootToLine
+    // calcPerpendicFootToLineInside
+    // checkHitSegmentSphere
+    // checkHitSemilinePlane
+    // calcReboundVelocity
+    // calcReboundVelocity
+    // calcParabolicFunctionParam
+    // makeQuatRotateRadian
+    // makeQuatRotateDegree
+    // makeQuatFromVec
+    // makeQuatSideUp
+    // makeQuatUpFront
+    // makeQuatUpNoSupport
+    // blendQuatUpFront
+    // blendQuatUpFront
+    // blendQuatFrontUp
+    // blendQuatFrontUp
+    // rotateQuatMoment
+    // rotateQuatRollBall
+    // turnQuat
+    // turnQuatXDirRad
+    // turnQuatXDirRate
+    // turnQuatYDirRad
+    // turnQuatYDirRate
+    // turnQuatZDirRad
 
-    // etc
+    f32 sign(f32 x) {
+        if (0.0f < x) {
+            return 1.0f;
+        }
 
-    void clampLength(TVec3f *a1, const TVec3f &a2, f32 a3) {
-        if (a2.squared() > (a3 * a3)) {
-            f32 sqr = a2.squared();
+        if (x < 0.0f) {
+            return -1.0f;
+        }
 
-            if (sqr <= 0.0000038146973f) {
-                a1->zero();
+        return 0.0f;
+    }
+
+    // clampVecAngleDeg
+
+    void clampLength(TVec3f* pDst, const TVec3f& rSrc, f32 length) {
+        if (rSrc.squared() > length * length) {
+            f32 sqr = rSrc.squared();
+
+            if (length <= 0.0000038146973f) {
+                pDst->zero();
             }
             else {
-                f32 inv_sqr = JGeometry::TUtil<f32>::inv_sqrt(sqr);
-                a1->scale((inv_sqr * a3), a2);
+                f32 invSqrt = JGeometry::TUtil<f32>::inv_sqrt(sqr);
+
+                pDst->scale(invSqrt * length, rSrc);
             }
-           
         }
         else {
-            a1->set<f32>(a2);
+            pDst->set<f32>(rSrc);
         }
     }
 
-    // MR::convergeRadian(f32, f32, f32)
+    // convergeRadian
 
     bool isInRange(f32 a1, f32 a2, f32 a3) {
         if (a2 > a3) {
@@ -169,41 +206,139 @@ namespace MR {
         }
     }
 
-    /*f32 calcRotateY(f32 a1, f32 a2) {
+    /*
+    f32 calcRotateY(f32 a1, f32 a2) {
         f32 val = JMath::sAtanTable.atan2_(-a2, a1);
-        return (minDegree + fmod((maxDegree + ((90.0f + (val * 57.295776f)) - minDegree)), maxDegree));
-    }*/
+        return (sMinDegree + fmod((sMaxDegree + ((90.0f + (val * 57.295776f)) - sMinDegree)), sMaxDegree));
+    }
+    */
 
-    
-    f32 calcDistanceXY(const TVec3f &a1, const TVec3f &a2) {
-        f32 x_diff = a1.x - a2.x;
-        f32 y_diff = a1.y - a2.y;
-        return JGeometry::TUtil<f32>::sqrt((x_diff * x_diff) + (y_diff * y_diff));
+    // calcRotateZ
+
+    f32 calcDistanceXY(const TVec3f &rPos1, const TVec3f &rPos2) {
+        f32 xDelta = rPos1.x - rPos2.x;
+        f32 yDelta = rPos1.y - rPos2.y;
+
+        return JGeometry::TUtil<f32>::sqrt(xDelta * xDelta + yDelta * yDelta);
     }
 
-    bool isNearZero(f32 a1, f32 a2) {
-        if (a1 < 0.0f) {
-            a1 = -a1;
+    // rotateVecDegree
+    // rotateVecDegree
+    // rotateVecDegree
+    // rotateVecDegree
+    // rotateVecRadian
+    // rotateVecRadian
+    // calcLocalVec
+    // normalize
+
+    void normalize(TVec3f* pVec) {
+        PSVECNormalize(pVec, pVec);
+    }
+
+    // normalize
+    // normalize
+
+    void normalize(const TVec3f& rSrc, TVec3f* pDst) {
+        pDst->set(rSrc);
+        normalize(pDst);
+    }
+
+    // normalize
+    // normalizeOrZero
+
+    bool normalizeOrZero(TVec3f* pVec) {
+        if (isNearZero(*pVec, 0.001f)) {
+            pVec->zero();
+
+            return true;
         }
 
-        if (a1 < a2) {
+        normalize(pVec);
+
+        return false;
+    }
+
+    bool normalizeOrZero(const TVec3f& rSrc, TVec3f* pDst) {
+        pDst->set(rSrc);
+
+        return normalizeOrZero(pDst);
+    }
+
+    void separateScalarAndDirection(f32* pScalar, TVec3f* pDir, const TVec3f& rVec) {
+        *pScalar = PSVECMag(&rVec);
+
+        if (isNearZero(rVec, 0.001f)) {
+            pDir->zero();
+        }
+        else {
+            normalize(rVec, pDir);
+        }
+    }
+
+    // FIXME: Instruction groups from branches are ordered incorrectly.
+    f32 normalize(f32 x, f32 min, f32 max) {
+        f32 range = max - min;
+
+        if (!isNearZero(range, 0.001f)) {
+            return (JGeometry::TUtil<f32>::clamp(x, min, max) - min) / range;
+        }
+        else if (x < min) {
+            return 0.0f;
+        }
+        else {
+            return 1.0f;
+        }
+    }
+
+    f32 normalizeAbs(f32 x, f32 min, f32 max) {
+        if (x >= 0.0f) {
+            return normalize(x, min, max);
+        }
+        else {
+            return -normalize(-x, min, max);
+        }
+    }
+
+    // turnVecToVecCos
+    // turnVecToVecCosOnPlane
+    // turnVecToVecCosOnPlane
+    // turnVecToVecRadian
+    // turnVecToVecDegree
+    // calcMomentRollBall
+    // calcReflectionVector
+    // isSameDirection
+
+    bool isOppositeDirection(const TVec3f& rParam1, const TVec3f& rParam2, f32 param3) {
+        if (rParam1.dot(rParam2) >= 0.0f) {
+            return false;
+        }
+
+        return isSameDirection(rParam1, rParam2, param3);
+    }
+
+    bool isNearZero(f32 x, f32 epsilon) {
+        if (x < 0.0f) {
+            x = -x;
+        }
+
+        if (x < epsilon) {
             return true;
         }
 
         return false;
     }
 
-    bool isNearZero(const TVec3f &rVec, f32 a2) {
+    bool isNearZero(const TVec3f &rVec, f32 epsilon) {
         f32 v2 = rVec.x;
-        if ( v2 > a2 )
+        if ( v2 > epsilon )
             return 0;
 
-        f32 v4 = -a2;
-        if ( v2 < -a2 )
+        f32 v4 = -epsilon;
+        if ( v2 < -epsilon )
             return 0;
 
         f32 v5 = rVec.y;
-        if ( v5 > a2 )
+        if ( v5 > epsilon )
             return 0;
 
         if ( v5 < v4 )
@@ -211,13 +346,14 @@ namespace MR {
 
         f32 v6 = rVec.z;
 
-        if ( v6 > a2 )
+        if ( v6 > epsilon )
             return 0;
 
-        return !(rVec.z < -a2);
+        return !(rVec.z < -epsilon);
     }
 
-    
+    // isNearZero
+
     f32 diffAngleAbs(f32 a1, f32 a2) {
         f32 normalize = normalizeAngleAbs(a1 - a2);
 
@@ -255,11 +391,7 @@ namespace MR {
             a2_n = val;
         }
 
-        bool res = false;
-
-        if (a1_n >= a3_n && a1_n <= a2_n) {
-            res = true;
-        }
+        bool res = a1_n >= a3_n && a1_n <= a2_n;
 
         if ((a2_n - a3_n) > PI) {
             res = !res;
@@ -273,7 +405,7 @@ namespace MR {
     f32 blendAngle(f32 a1, f32 a2, f32 a3) {
         f32 a1_n = normalizeAngleAbs(a1);
         f32 a2_n = normalizeAngleAbs(a2);
-        
+
         if (!isAngleBetween(0.5f * (a1_n + a2_n), a1_n, a2_n)) {
             if (a1_n < a2_n) {
                 a2_n += TWO_PI;
@@ -283,24 +415,24 @@ namespace MR {
             }
         }
 
-        return normalizeAngleAbs((((1.0f - a3) * a1_n) + (a3 * a2_n)));
+        return normalizeAngleAbs(((1.0f - a3) * a1_n) + (a3 * a2_n));
     }
     #endif
 
-    u8 lerp(u8 a1, u8 a2, f32 a3) {
-        return JGeometry::TUtil<f32>::clamp(a1 + (a3 * (a2 - a1)), 0.0f, 255.0f);
+    u8 lerp(u8 start, u8 end, f32 t) {
+        return JGeometry::TUtil<f32>::clamp(start + (end - start) * t, 0.0f, 255.0f);
     }
 
-    /*#ifdef NON_MATCHING
-    _GXColor lerp(_GXColor a1, _GXColor a2, f32 a3) {
-        u8 v6 = lerp(a1.a, a2.a, a3);
-        u8 v7 = lerp(a1.b, a2.b, a3);
-        u8 v8 = lerp(a1.g, a2.b, a3);
-        u8 thing = lerp(a1.r, a2.r, a3);
+    GXColor lerp(GXColor start, GXColor end, f32 t) {
+        u8 a = lerp(start.a, end.a, t);
+        u8 b = lerp(start.b, end.b, t);
+        u8 g = lerp(start.g, end.b, t);
+        u8 r = lerp(start.r, end.r, t);
 
-        return (v6 | ((v7 << 8) & 0xFF00 | ((v8 << 16) & 0xFF000 | thing << 24)) & 0xFFFF00FF) & 0xFFFFFF00;
+        GXColor color = { r, g, b, a };
+
+        return color;
     }
-    #endif*/
 
     //This function implements the selection sort sorting algorithm
     // on an array of f32 where sortArray is the array to be sorted
@@ -355,10 +487,11 @@ namespace MR {
     f32 vecKillElement(const TVec3f &a1, const TVec3f &a2, TVec3f *a3) {
         if (isNearZero(a2, 0.001f)) {
             *a3 = a1;
+
             return 0.0f;
         }
 
-        return PSVECKillElement((const Vec*)&a1, (const Vec*)&a2, (const Vec*)a3);
+        return PSVECKillElement(&a1, &a2, a3);
     }
 
     void vecScaleAdd(const register TVec3f *a1, const register TVec3f *a2, register f32 a3) {
@@ -397,24 +530,24 @@ namespace MR {
         PSvecBlend(&a1, &a2, a3, 1.0f - a4, a4);
     }
 
-    // MR::vecBlendNormal
-    // MR::vecBlendSphere
-    // MR::vecRotAxis
+    // vecBlendNormal
+    // vecBlendSphere
+    // vecRotAxis
 
-    void blendColor(_GXColor *a1, const _GXColor &a2, const _GXColor &a3, f32 a4) {
-        a1->r = getInterpolateValue(a4, a2.r, a3.r);
-        a1->g = getInterpolateValue(a4, a2.g, a3.g);
-        a1->b = getInterpolateValue(a4, a2.b, a3.b);
-        a1->a = getInterpolateValue(a4, a2.a, a3.a);
+    void blendColor(GXColor* pDst, const GXColor& rStart, const GXColor& rEnd, f32 t) {
+        pDst->r = getInterpolateValue(t, rStart.r, rEnd.r);
+        pDst->g = getInterpolateValue(t, rStart.g, rEnd.g);
+        pDst->b = getInterpolateValue(t, rStart.b, rEnd.b);
+        pDst->a = getInterpolateValue(t, rStart.a, rEnd.a);
     }
 
-    void blendVec(Vec *a1, const Vec &a2, const Vec &a3, f32 a4) {
-        a1->x = getInterpolateValue(a4, a2.x, a3.x);
-        a1->y = getInterpolateValue(a4, a2.y, a3.y);
-        a1->z = getInterpolateValue(a4, a2.z, a3.z);
+    void blendVec(Vec* pDst, const Vec& rStart, const Vec& rEnd, f32 t) {
+        pDst->x = getInterpolateValue(t, rStart.x, rEnd.x);
+        pDst->y = getInterpolateValue(t, rStart.y, rEnd.y);
+        pDst->z = getInterpolateValue(t, rStart.z, rEnd.z);
     }
 
-    // MR::turnVecToPlane
+    // turnVecToPlane
 
     int getMinAbsElementIndex(const TVec3f &rVec) {
         f64 abs_x = __fabs(rVec.x);
@@ -469,4 +602,43 @@ namespace MR {
 
         return 2;
     }
+
+    // diffAngleAbsFast
+    // diffAngleAbs
+    // diffAngleAbs
+    // diffAngleSigned
+    // diffAngleAbsHorizontal
+    // diffAngleSignedHorizontal
+    // isNearAngleRadian
+    // isNearAngleDegree
+    // isNearAngleRadianHV
+    // isNearAngleDegreeHV
+    // createBoundingBox
+    // isNormalize
+    // setNan
+    // isNan
+    // getFootPoint
+
+    f32 mod(f32 x, f32 y) {
+        return fmod(x, y);
+    }
+
+    void floatToFixed16(TVec3s* pDst, const TVec3f& pSrc, u8 q) {
+        f32 scale = 1 << q;
+
+        pDst->x = pSrc.x * scale;
+        pDst->y = pSrc.y * scale;
+        pDst->z = pSrc.z * scale;
+    }
+
+    void fixed16ToFloat(TVec3f* pDst, const TVec3s& pSrc, u8 q) {
+        f32 scale = 1.0f / (1 << q);
+
+        pDst->x = pSrc.x * scale;
+        pDst->y = pSrc.y * scale;
+        pDst->z = pSrc.z * scale;
+    }
+
+    // getRotatedAxisY
+    // getRotatedAxisZ
 };
