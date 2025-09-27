74fd4b6ded44ac435e50079e922923d0bf3dbc46
ThePlayerRolo <thesolis78243@gmail.com>
2025-09-27 17:24:09 -0400

    LavaHomeVolcanoFlow and LavaJamboSunPlanet (#1774)
    * LavaHomeVolcanoFlow and LavaJamboSunPlanet
    
    * Fix LavaProminence split
    
    * Fix unforwards

---
+++ include/Game/MapObj/LavaHomeVolcanoFlow.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Game/LiveActor/LiveActor.hpp"
+
+class LavaHomeVolcanoFlow : public LiveActor {
+public:
+    LavaHomeVolcanoFlow(const char*);
+    
+    virtual ~LavaHomeVolcanoFlow();
+    virtual void init(const JMapInfoIter &rIter);
+    virtual void startClipped();
+    virtual void endClipped();
+
+    void exeMove();
+    void exeWait();
+};
---
+++ include/Game/MapObj/LavaJamboSunPlanet.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Game/LiveActor/LiveActor.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+
+class PartsModel;
+
+class LavaJamboSunPlanet : public LiveActor {
+public:
+    LavaJamboSunPlanet(const char*);
+
+    virtual ~LavaJamboSunPlanet();
+    virtual void init(const JMapInfoIter &);
+    virtual void initAfterPlacement();
+    virtual void control();
+
+    void exeWait();
+    void exeMove();
+
+    PartsModel* mBloomModel;
+    LodCtrl* mLodCtrl;
+    f32 _94;
+    TVec3f _98;
+};
