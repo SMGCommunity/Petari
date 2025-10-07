9a4794b945e5f161b7c1ced20a17086798daa279
ThatNintendoNerd <samtheman1026@gmail.com>
2025-10-06 19:13:40 -0700

    Fix regression with `LayoutActorFlag`

--- src/Game/Screen/LayoutActorFlag.cpp
+++ src/Game/Screen/LayoutActorFlag.cpp
@@ -1,10 +1,8 @@
 #include "Game/Screen/LayoutActorFlag.hpp"
 
-LayoutActorFlag::LayoutActorFlag() :
-    mIsDead(true),
-    mIsHidden(false),
-    mIsStopAnimFrame(false),
-    mIsOffCalcAnim(false)
-{
-    
+LayoutActorFlag::LayoutActorFlag() {
+    mIsDead = true;
+    mIsHidden = false;
+    mIsStopAnimFrame = false;
+    mIsOffCalcAnim = false;
 }
