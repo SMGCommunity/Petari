f70f7ca3502a90e1dc1cda2d63c8f3378d5d93ac
ThatNintendoNerd <samtheman1026@gmail.com>
2025-10-10 15:38:18 -0700

    Match 47% of `EffectSystem`

--- include/Game/Effect/EffectSystem.hpp
+++ include/Game/Effect/EffectSystem.hpp
@@ -1,33 +1,36 @@
 #pragma once
 
-#include <JSystem/JParticle/JPAEmitterManager.hpp>
-#include <revolution.h>
 #include "Game/NameObj/NameObj.hpp"
 
 class AutoEffectGroupHolder;
-class ParticleEmitter;
-class SingleEmitter;
+class JPAEmitterManager;
+class MultiEmitterCallBackBase;
+class MultiEmitterParticleCallBack;
 class ParticleCalcExecutor;
 class ParticleDrawExecutor;
+class ParticleEmitter;
 class ParticleEmitterHolder;
 class ParticleResourceHolder;
+class SingleEmitter;
 
 class EffectSystem : public NameObj {
 public:
     EffectSystem(const char*, bool);
 
+    virtual void init(const JMapInfoIter& rIter);
+
     ParticleEmitter* createEmitter(u16, u8, u8);
     void forceDeleteEmitter(ParticleEmitter*) const;
     void forceDeleteSingleEmitter(SingleEmitter*) const;
-
+    void createSingleEmitter(SingleEmitter*, MultiEmitterCallBackBase*, MultiEmitterParticleCallBack*);
     void entry(ParticleResourceHolder*, u32, u32);
 
-    u32 _C;
-    ParticleEmitterHolder* mEmitterHolder;  // 0x10
-    ParticleDrawExecutor* mDrawExec;        // 0x14
-    ParticleCalcExecutor* mCalcExec;        // 0x18
-    AutoEffectGroupHolder* mGroupHolder;    // 0x1C
-    u32 _20;
+    /* 0x0C */ JPAEmitterManager* mEmitterManager;
+    /* 0x10 */ ParticleEmitterHolder* mEmitterHolder;
+    /* 0x14 */ ParticleDrawExecutor* mDrawExec;
+    /* 0x18 */ ParticleCalcExecutor* mCalcExec;
+    /* 0x1C */ AutoEffectGroupHolder* mGroupHolder;
+    /* 0x20 */ bool _20;
 };
 
 namespace MR {
