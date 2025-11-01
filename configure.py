6dc949d568d0c48aa478b5e70b7dc383101c437f
ThatNintendoNerd <samtheman1026@gmail.com>
2025-11-01 00:56:44 -0700

    Match 96% of `WPadRumbleData`

--- configure.py
+++ configure.py
@@ -2194,7 +2194,7 @@ config.libs = [
             Object(NonMatching, "Game/System/WPadLeaveWatcher.cpp"),
             Object(NonMatching, "Game/System/WPadPointer.cpp"),
             Object(NonMatching, "Game/System/WPadRumble.cpp"),
-            Object(NonMatching, "Game/System/WPadRumbleData.cpp"),
+            Object(Matching, "Game/System/WPadRumbleData.cpp"),
             Object(Matching, "Game/System/WPadStick.cpp"),
             Object(Matching, "Game/System/HomeButtonMenuWrapper.cpp"),
             Object(Matching, "Game/System/HomeButtonStateNotifier.cpp"),
