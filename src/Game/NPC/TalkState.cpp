80a473c80d9ccb5d9f126d95b35b73b0f3f46ce2
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2025-11-01 19:07:15 +0100

    fix TalkState::init argument names

--- src/Game/NPC/TalkState.cpp
+++ src/Game/NPC/TalkState.cpp
@@ -1,8 +1,8 @@
 #include "Game/NPC/TalkState.hpp"
 #include "Game/NPC/TalkMessageCtrl.hpp"
 
-void TalkState::init(TalkMessageCtrl *t1, TalkBalloon *t2) {
-    this->_04 = t1;
-    this->_08 = t2;
+void TalkState::init(TalkMessageCtrl *pArg1, TalkBalloon *pArg2) {
+    this->_04 = pArg1;
+    this->_08 = pArg2;
     this->mMessageID = this->_04->getMessageID();
 }
