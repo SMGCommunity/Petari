5319f738e35a67d56e157eafface1b02d97eb485
ThatNintendoNerd <samtheman1026@gmail.com>
2025-11-02 15:14:40 -0800

    Fully declare `HomeButtonLayout`

--- include/Game/Screen/HomeButtonLayout.hpp
+++ include/Game/Screen/HomeButtonLayout.hpp
@@ -2,13 +2,40 @@
 
 #include "Game/Screen/LayoutActor.hpp"
 
+struct HBMControllerData;
+struct HBMDataInfo;
+
+class HomeButtonMenuContext {
+public:
+    /// @brief Creates a new `HomeButtonMenuContext`.
+    HomeButtonMenuContext();
+
+    void initControllerData();
+    void initHomeButtonInfo();
+
+private:
+    /* 0x00 */ HBMDataInfo* mHomeButtonInfo;
+    /* 0x04 */ HBMControllerData* mControllerData;
+};
+
 class HomeButtonLayout : public LayoutActor {
 public:
+    /// @brief Creates a new `HomeButtonLayout`.
     HomeButtonLayout();
 
-    bool isActive() const;
+    virtual void init(const JMapInfoIter& rIter);
+    virtual void movement();
+    virtual void draw() const;
 
-    u32 _20;
-    u8 _24;
-    u8 _25;
+    void forceToDeactive();
+    bool isActive() const;
+    void updateController();
+    bool tryCorePadTriggerHome();
+    void exeDeactive();
+    void exeActive();
+
+private:
+    /* 0x20 */ HomeButtonMenuContext* mMenuContext;
+    /* 0x24 */ bool _24;
+    /* 0x25 */ bool _25;
 };
