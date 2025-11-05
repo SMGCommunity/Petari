72547283af15f4764ff726fec741b66a75772a2f
ThatNintendoNerd <samtheman1026@gmail.com>
2025-11-04 21:07:02 -0800

    Cleanup `MoviePlayerSimple`

--- src/Game/Screen/MoviePlayerSimple.cpp
+++ src/Game/Screen/MoviePlayerSimple.cpp
@@ -15,6 +15,12 @@
 #include <revolution/gx/GXEnum.h>
 
 namespace {
+    static const s16 sCinemaFrameHeightTop = 44;
+    static const s16 sCinemaFrameHeightBottom = 412;
+    static const GXColor sCinemaFrameColor = { 0, 0, 0, 255 };
+};
+
+namespace {
     NEW_NERVE(MoviePlayerSimpleOff, MoviePlayerSimple, SimpleOff);
     NEW_NERVE(MoviePlayerSimpleOpen, MoviePlayerSimple, Open);
     NEW_NERVE(MoviePlayerSimplePreload, MoviePlayerSimple, Preload);
@@ -22,17 +28,20 @@ namespace {
     NEW_NERVE(MoviePlayerSimpleSuspend, MoviePlayerSimple, SimpleSuspend);
 };
 
-MoviePlayerSimple::MoviePlayerSimple() : LayoutActor("ムービープレイヤー", true), JKRDisposer() {
-    mMovie = nullptr;
-    mPlayerWrapper = nullptr;
-    _44 = false;
-    _45 = false;
+MoviePlayerSimple::MoviePlayerSimple() :
+    LayoutActor("ムービープレイヤー", true),
+    JKRDisposer(),
+    mMovie(nullptr),
+    mPlayerWrapper(nullptr),
+    _44(false),
+    _45(false)
+{
     mMovie = new Movie();
     mMovie->mMovieName = "";
     mMovie->mBuffer = nullptr;
     mMovie->mCurrentFrame = -1;
-    mMovie->_18 = 0;
-    mMovie->_19 = 0;
+    mMovie->_18 = false;
+    mMovie->_19 = false;
     mMovie->mFrameRateDefault = 1;
     mMovie->_20 = 0;
     mMovie->_24 = 0;
@@ -47,25 +56,31 @@ MoviePlayerSimple::~MoviePlayerSimple() {
 
 void MoviePlayerSimple::init(const JMapInfoIter &rIter) {
     MR::connectToSceneMovie(this);
+
     mPlayerWrapper = new THPSimplePlayerWrapper("THPシンプルプレイヤーのラッパ");
     mPlayerWrapper->init(0);
+
     mHeap = JKRExpHeap::create(calcNeedMemoryForMovieWorks(), MR::getCurrentHeap(), true);
+
     initNerve(&MoviePlayerSimpleOff::sInstance);
     kill();
 }
 
 void MoviePlayerSimple::draw() const {
     if (!isNerve(&MoviePlayerSimplePlaying::sInstance) && !isNerve(&MoviePlayerSimpleSuspend::sInstance)) {
-        GXColor color;
-        color.r = 0;
-        color.g = 0;
-        color.b = 0;
-        color.a = 0xFF;
-        MR::fillScreen(color);
+        GXColor fillColor;
+        fillColor.r = 0;
+        fillColor.g = 0;
+        fillColor.b = 0;
+        fillColor.a = 255;
+
+        MR::fillScreen(fillColor);
     }
     else {
         drawCinemaFrame();
+
         u32 frameBufferWidth;
+
         if (MR::isScreen16Per9()) {
             frameBufferWidth = MR::getFrameBufferWidth();
         }
@@ -74,8 +89,8 @@ void MoviePlayerSimple::draw() const {
         }
 
         u32 ySize = mMovie->mVideoInfo.ySize;
-        u32 v4 = (MR::getFrameBufferWidth() - frameBufferWidth) >> 1u;
-        u32 v5 = (MR::getFrameBufferHeight() - ySize) >> 1u;
+        u32 v4 = (MR::getFrameBufferWidth() - frameBufferWidth) / 2;
+        u32 v5 = (MR::getFrameBufferHeight() - ySize) / 2;
         THPGXSetTexObjFilter(GX_LINEAR);
         mMovie->mCurrentFrame = mPlayerWrapper->drawCurrentFrame(JUTVideo::sManager->mRenderModeObj, v4, v5, frameBufferWidth, ySize);
     }
@@ -89,6 +104,7 @@ void MoviePlayerSimple::startMovie(const char *pMovieName, bool a2) {
     mMovie->mMovieName = pMovieName;
     mMovie->_18 = a2;
     mMovie->_24 = 0;
+
     play(a2);
     appear();
     MR::requestMovementOn(this);
@@ -134,16 +150,17 @@ s32 MoviePlayerSimple::getTotalFrame() const {
 
 void MoviePlayerSimple::setFrameRateDefault() {
     f32 frameRate = mPlayerWrapper->getFrameRate();
-    if (MR::isNearZero(frameRate - 59.939999f, 0.001f)) {
+
+    if (MR::isNearZero(frameRate - 59.94f, 0.001f)) {
         mMovie->mFrameRateDefault = 1;
     }
-    else if (MR::isNearZero(frameRate - 29.969999f, 0.001f)) {
+    else if (MR::isNearZero(frameRate - 29.97f, 0.001f)) {
         mMovie->mFrameRateDefault = 2;
     }
 }
 
 void MoviePlayerSimple::setVolume(f32 volume, s32 time) {
-    mPlayerWrapper->setVolume(127.0f * volume, time);
+    mPlayerWrapper->setVolume(volume * 127.0f, time);
 }
 
 void MoviePlayerSimple::setUnpauseHomeButtonFlag() {
@@ -162,7 +179,7 @@ void MoviePlayerSimple::exeOpen() {
     if (mPlayerWrapper->_9) {
         mPlayerWrapper->getVideoInfo(&mMovie->mVideoInfo);
         mMovie->_20 = 0;
-        mMovie->_19 = 0;
+        mMovie->_19 = false;
         setFrameRateDefault();
         mMovie->mBuffer = new(mHeap, 32) u8[mPlayerWrapper->calcNeedMemory()];
         mPlayerWrapper->setBuffer(mMovie->mBuffer);
@@ -172,10 +189,12 @@ void MoviePlayerSimple::exeOpen() {
 }
 
 void MoviePlayerSimple::exePreload() {
-    if (!mPlayerWrapper->isPreLoading()) {
-        setNerve(&MoviePlayerSimplePlaying::sInstance);
-        appear();
+    if (mPlayerWrapper->isPreLoading()) {
+        return;
     }
+
+    setNerve(&MoviePlayerSimplePlaying::sInstance);
+    appear();
 }
 
 void MoviePlayerSimple::exePlaying() {
@@ -193,22 +212,23 @@ void MoviePlayerSimple::exePlaying() {
         s32 result = mPlayerWrapper->decode(0);
 
         switch (result) {
-            case 0:
-                mMovie->_19 = 1;
-                break;
-            case 2:
-                mMovie->_24++;
-                break;
-            case 3:
-                return;
+        case 0:
+            mMovie->_19 = true;
+            break;
+        case 2:
+            mMovie->_24++;
+            break;
+        case 3:
+            return;
         }
     }
 
     mMovie->_20++;
+
     if (mMovie->_19) {
         if (mMovie->mFrameRateDefault <= mMovie->_20) {
             mMovie->_20 = 0;
-            mMovie->_19 = 0;
+            mMovie->_19 = false;
         }
     }
 }
@@ -238,34 +258,26 @@ void MoviePlayerSimple::stop() {
 }
 
 u32 MoviePlayerSimple::calcNeedMemoryForMovieWorks() {
-    int ret = 0;
-    s32 width;
-    s32 height;
-    s32 temp;
-    ret += 0x140000;
+    u32 needMemory = 0;
+    needMemory += 0x140000;
+
     s32 texCount = mPlayerWrapper->getUseTextureCount();
-    
-    height = MR::getFrameBufferHeight();
-    width = MR::getFrameBufferWidth();
-    temp = (texCount * width);
-    ret += ((temp * height + 31) & ~31);
-    height = MR::getFrameBufferHeight();
-    width = MR::getFrameBufferWidth();
-    ret  += ((texCount * (width * height / 4) + 31) & ~31);
-    height = MR::getFrameBufferHeight();
-    width = MR::getFrameBufferWidth();
-    ret += ((texCount * (width * height / 4) + 31) & ~31);
-    return ret + 0x1188;
+
+    needMemory += OSRoundUp32B(texCount * (MR::getFrameBufferWidth() * MR::getFrameBufferHeight()));
+    needMemory += OSRoundUp32B(texCount * (MR::getFrameBufferWidth() * MR::getFrameBufferHeight() / 4));
+    needMemory += OSRoundUp32B(texCount * (MR::getFrameBufferWidth() * MR::getFrameBufferHeight() / 4));
+
+    return needMemory + 0x1188;
 }
 
-GXColor cinemaColor = { 0, 0, 0, 0xFF };
-
 void MoviePlayerSimple::drawCinemaFrame() const {
-    MR::fillScreenSetup(cinemaColor);
+    MR::fillScreenSetup(sCinemaFrameColor);
+
     s16 width = MR::getFrameBufferWidth();
-    TBox2s box(0, 0, width, 44);
-    MR::fillScreenArea(box._0, box._8);
+    TBox2s top(0, 0, width, sCinemaFrameHeightTop);
+    MR::fillScreenArea(top._0, top._8);
+
     s16 height = MR::getFrameBufferHeight();
-    TBox2s box_1(0, 412, width, height);
-    MR::fillScreenArea(box_1._0, box_1._8);
+    TBox2s bottom(0, sCinemaFrameHeightBottom, width, height);
+    MR::fillScreenArea(bottom._0, bottom._8);
 }
