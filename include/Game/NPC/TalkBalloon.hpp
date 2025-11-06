66d9b3deeeff61d04cc7682aedf5012188f94fd0
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2025-11-06 19:11:11 +0100

    Fill out more of TalkBalloon

--- include/Game/NPC/TalkBalloon.hpp
+++ include/Game/NPC/TalkBalloon.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+#include "Game/NPC/TalkMessageCtrl.hpp"
 #include "Game/Screen/LayoutActor.hpp"
 
 class TalkBalloon : public LayoutActor {
 public:
     TalkBalloon(const char *);
+
+    virtual void open(TalkMessageCtrl *);
+    virtual void close();
+    virtual void updateBalloon();
+    virtual void updateTalking();
+    virtual void pauseOff();
+    virtual bool turnPage();
+    virtual bool hasNextPage();
+    virtual void skipMessage();
+    virtual bool isTextAppearedAll();
+
+    TalkMessageCtrl *_20;
 };
