9bde2b7558d75ff82485b1a40f515c05ce644ec1
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2025-11-07 23:17:06 +0100

    Match 62% of TalkState (#1786)
    * Loosely define TalkSupportPlayerWatcher
    
    * Fill out more of TalkBalloon
    
    * Declare TalkFunction::getMessageInfo
    
    * Declare all TalkState classes
    
    * Match 71% of TalkState
    
    * Fix typo in mSecondBalloon
    
    * Fix missing "m" prefix to some members of TalkStateHolder
    
    * Set TalkStateNormal::prep to non matching
    
    * Fix TalkStateCompose member's style
    
    * Forward declarations
    
    * Rename TalkState::init pArg2 to pBalloon
    
    * Remove redundant explicit cast in TalkState::balloonOff
    
    * Rename local variables to camelCase
    
    * Remove non match of TalkStateNormal::prep
    
    * Remove now-unecessary #include of LayoutActor in TalkState

--- include/Game/NPC/TalkBalloon.hpp
+++ include/Game/NPC/TalkBalloon.hpp
@@ -2,7 +2,21 @@
 
 #include "Game/Screen/LayoutActor.hpp"
 
+class TalkMessageCtrl;
+
 class TalkBalloon : public LayoutActor {
 public:
     TalkBalloon(const char *);
+
+    virtual void open(TalkMessageCtrl *);
+    virtual void close();
+    virtual void updateBalloon();
+    virtual void updateTalking();
+    virtual void pauseOff();
+    virtual bool turnPage();
+    virtual bool hasNextPage();
+    virtual void skipMessage();
+    virtual bool isTextAppearedAll();
+
+    TalkMessageCtrl *_20;
 };
--- src/Game/NPC/TalkState.cpp
+++ src/Game/NPC/TalkState.cpp
@@ -1,16 +1,27 @@
 #include "Game/NPC/TalkState.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/NPC/TalkBalloon.hpp"
 #include "Game/NPC/TalkMessageCtrl.hpp"
+#include "Game/NPC/TalkMessageInfo.hpp"
+#include "Game/NPC/TalkSupportPlayerWatcher.hpp"
+#include "Game/Screen/IconAButton.hpp"
+#include "Game/Util/CameraUtil.hpp"
+#include "Game/Util/DemoUtil.hpp"
+#include "Game/Util/GamePadUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
+#include "Game/Util/SoundUtil.hpp"
+#include "revolution/types.h"
 
 TalkState::TalkState() :
     _04(nullptr),
-    _08(nullptr)
+    mBalloon(nullptr)
 {
 
 }
 
-void TalkState::init(TalkMessageCtrl *pArg1, TalkBalloon *pArg2) {
+void TalkState::init(TalkMessageCtrl *pArg1, TalkBalloon *pBalloon) {
     _04 = pArg1;
-    _08 = pArg2;
+    mBalloon = pBalloon;
     mMessageID = _04->getMessageID();
 }
 
@@ -19,7 +30,7 @@ u32 TalkState::getPageCount() const {
 }
 
 void TalkState::balloonOff() const {
-    MR::hideScreen((LayoutActor *)_08);
+    MR::hideScreen(mBalloon);
 }
 
 bool TalkState::isLostMessage(const TalkMessageCtrl *pArg1) const {
@@ -55,6 +66,17 @@ bool TalkStateShort::talk(const TalkMessageCtrl *) {
     return false;
 }
 
+void TalkStateShort::open() {
+    mBalloon->open(_04);
+    LiveActor* actor = _04->mHostActor;
+    TalkMessageInfo* info = TalkFunction::getMessageInfo(_04);
+    MR::startTalkSound(info->_6, actor);
+}
+
+void TalkStateShort::clos() {
+    mBalloon->close();
+}
+
 
 TalkStateEvent::TalkStateEvent() {
     _18 = nullptr;
@@ -65,6 +87,65 @@ TalkStateEvent::TalkStateEvent() {
 
 bool TalkStateEvent::prep(const TalkMessageCtrl *pArg1) {
     return !TalkState::isLostMessage(pArg1);
+}
+
+bool TalkStateEvent::test() {
+    return _18->isEnableTalkPlayerStateEvent();
+}
+
+void TalkStateEvent::open() {
+    TalkMessageInfo* info = TalkFunction::getMessageInfo(_04);
+    MR::startTalkSound(info->_6, _04->mHostActor);
+
+    _1C = 1;
+    mPageCount = 0;
+    _10 = 0;
+
+    if (!MR::isTimeKeepDemoActive() || !info->isCameraNormal()) {
+        _04->startCamera(-1);
+    }
+    
+    mBalloon->open(_04);
+}
+
+void TalkStateEvent::clos() {
+    mBalloon->close();
+}
+
+bool TalkStateEvent::talk(const TalkMessageCtrl *pArg1) {
+    if (MR::testCorePadButtonA(0)) {
+        if (_10++ > 10) {
+            for (int i = 0; i < 2; i++) {
+                mBalloon->skipMessage();
+            }
+        }
+    } else {
+        _10 = 0;
+    }
+
+    if (!mBalloon->isTextAppearedAll()) {
+        return false;
+    }
+
+    bool unknownBool = false;
+
+    if (_1C != nullptr) {
+        _1C = MR::testCorePadButtonA(0);
+    } else if (MR::testCorePadButtonA(0) && !MR::testCorePadTriggerA(0)) {
+        _1C = true;
+        unknownBool = true;
+    }
+
+    if (TalkFunction::isSelectTalk(_04) && !mBalloon->hasNextPage()) {
+        return true;
+    }
+
+    if (unknownBool) {
+        mPageCount++;
+        return !mBalloon->turnPage();
+    }
+    
+    return false;
 }
 
 bool TalkStateEvent::term(const TalkMessageCtrl *pArg1) {
@@ -78,4 +159,127 @@ bool TalkStateEvent::term(const TalkMessageCtrl *pArg1) {
 
 u32 TalkStateEvent::getPageCount() const {
     return mPageCount;
+}
+
+bool TalkStateNormal::test() {
+    if (!_18->isEnableTalkPlayerStateNormal() || !MR::testCorePadTriggerA(0)) {
+        return false;
+    }
+    
+    return TalkStateEvent::test();
+}
+
+void TalkStateNormal::clos() {
+    _24->term();
+    mBalloon->close();
+}
+
+bool TalkStateNormal::term(const TalkMessageCtrl *pArg1) {
+    if (pArg1 != nullptr && pArg1 != _04) {
+        return true;
+    }
+
+    if (pArg1 != nullptr && TalkFunction::isShortTalk(pArg1)) {
+        return true;
+    }
+
+    return TalkStateEvent::term(pArg1);
+}
+
+// Stuck at 99% because assembly string labels don't match, even though the code *should* be correct.
+bool TalkStateNormal::prep(const TalkMessageCtrl *pArg1) {
+    if (TalkStateNormal::isLostMessage(pArg1)) {
+        _24->term();
+        return false;
+    }
+
+    TalkStateNormal::updateButton();
+
+    if (!_18->isEnableTalkPlayerStateNormal()) {
+        _24->term();
+    } else if (!MR::testCorePadTriggerA(0)) {
+        TalkMessageInfo *info = TalkFunction::getMessageInfo(_04);
+        if (!_24->isOpen()) {
+            if (info->isBalloonSign()) {
+                _24->openWithRead();
+            } else {
+                _24->openWithTalk();
+            }
+            MR::startSystemSE("SE_SM_TALK_BUTTON_APPEAR", -1, -1);
+        }
+    } else {
+        _24->term();
+        MR::startSystemSE("SE_SY_TALK_START", -1, -1);
+        MR::startCSSound("CS_CLICK_OPEN", nullptr, 0);
+    }
+
+    return true;
+}
+
+
+void TalkStateCompose::init(TalkMessageCtrl *pArg1, TalkBalloon *pArg2) {
+    TalkState::init(pArg1, pArg2);
+    mSecondBalloon->open(pArg1);
+}
+
+bool TalkStateCompose::test() {
+    if (_04->isNearPlayer(_04->mTalkDistance)) {
+        return TalkStateNormal::test();
+    }
+
+    return false;
+}
+
+void TalkStateCompose::open() {
+    TalkStateEvent::open();
+    mSecondBalloon->close();
+}
+
+bool TalkStateCompose::prep(const TalkMessageCtrl *pArg1) {
+    bool unknownBool = true;
+
+    if (TalkState::isLostMessage(pArg1)) {
+        unknownBool = false;
+    } else if (_04->isNearPlayer(_04->mTalkDistance)) {
+        unknownBool = TalkStateNormal::prep(pArg1);
+    } else {
+        _24->term();
+    }
+
+    if (!unknownBool) {
+        _24->term();
+        mSecondBalloon->close();
+    }
+
+    return unknownBool;
+}
+
+void TalkStateHolder::update() {
+    _00->update();
+}
+
+void TalkStateHolder::pauseOff() {
+    MR::requestMovementOn(_04);
+}
+
+u32 TalkStateHolder::getState(const TalkMessageCtrl *pArg1) {
+    TalkMessageInfo *info = TalkFunction::getMessageInfo(pArg1);
+
+    if (info->isNormalTalk()) {
+        return mTalkNormal;
+    }
+
+    if (info->isShortTalk()) {
+        return mTalkShort;
+    }
+
+    if (info->isEventTalk()) {
+        return mTalkEvent;
+    }
+
+    if (info->isComposeTalk()) {
+        return mTalkCompose;
+    }
+
+    return mTalkUnknown;
 }
