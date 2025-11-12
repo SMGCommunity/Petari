d478ddc1d754a7ca604fa45c4d7428ec10c97cf0
ThatNintendoNerd <samtheman1026@gmail.com>
2025-11-12 14:05:07 -0800

    Remove duplicate utility function declarations from `TalkUtil`

--- include/Game/Util/TalkUtil.hpp
+++ include/Game/Util/TalkUtil.hpp
@@ -11,65 +11,6 @@ class TalkMessageCtrl;
 class TalkMessageFuncBase;
 
 namespace MR {
-
-    void registerBranchFunc(TalkMessageCtrl *, const TalkMessageFuncBase &);
-    void registerEventFunc(TalkMessageCtrl *, const TalkMessageFuncBase &);
-    void registerAnimeFunc(TalkMessageCtrl *, const TalkMessageFuncBase &);
-    void registerKillFunc(TalkMessageCtrl *, const TalkMessageFuncBase &);
-    void setMessageArg(TalkMessageCtrl *, int);
-    void setMessageArg(TalkMessageCtrl *, const wchar_t *);
-
-    void resetAndForwardNode(TalkMessageCtrl *, s32);
-
-    bool tryTalkForceWithoutDemoMarioPuppetableAtEnd(TalkMessageCtrl *);
-
-    void setDistanceToTalk(TalkMessageCtrl *, f32);
-
-    void onRootNodeAutomatic(TalkMessageCtrl *);
-    void offRootNodeAutomatic(TalkMessageCtrl *);
-
-    void resetNode(TalkMessageCtrl *);
-
-    bool tryTalkForce(TalkMessageCtrl *);
-    bool tryTalkForceAtEnd(TalkMessageCtrl *);
-
-    void forwardNode(TalkMessageCtrl *);
-
-    void onStartOnlyFront(TalkMessageCtrl *);
-
-    bool isExistNextNode(const TalkMessageCtrl *);
-
-    bool tryTalkRequest(TalkMessageCtrl *);
-    bool tryTalkSelectLeft(TalkMessageCtrl *);
-    bool tryTalkSelectRight(TalkMessageCtrl *);
-
-    void setMessageBalloonFollowOffset(TalkMessageCtrl *, const TVec3f &);
-
-    TalkMessageCtrl* createTalkCtrl(LiveActor *, const JMapInfoIter &, const char *,const TVec3f &, MtxPtr);
-    TalkMessageCtrl* createTalkCtrlDirect(LiveActor *, const JMapInfoIter &, const char *,const TVec3f &, MtxPtr);
-    TalkMessageCtrl* createTalkCtrlDirectOnRootNodeAutomatic(LiveActor *, const JMapInfoIter &, const char *, const TVec3f &, MtxPtr);
-
-    inline void setMessageArgToCurrentGalaxyName(TalkMessageCtrl *pCtrl, const char *pName) {
-        MR::setMessageArg(pCtrl, MR::getGalaxyNameOnCurrentLanguage(pName));
-    }
-
-    bool isNearPlayer(const TalkMessageCtrl *, f32);
-
-    bool tryTalkNearPlayerAtEnd(TalkMessageCtrl *);
-    
-    bool tryTalkNearPlayer(TalkMessageCtrl *);
-
-    bool tryTalkForceWithoutDemo(TalkMessageCtrl *);
-
-    bool tryTalkTimeKeepDemo(TalkMessageCtrl *);
-    bool tryTalkTimeKeepDemoMarioPuppetable(TalkMessageCtrl *);
-
-    bool tryTalkForceWithoutDemoAtEnd(TalkMessageCtrl *);
-
-    bool inMessageArea(const TalkMessageCtrl *);
-
-    bool isTalkTalking(const TalkMessageCtrl *);
-
     class ActorTalkParam {
     public:
         void setNoTurnAction(const char*);
@@ -141,4 +82,8 @@ namespace MR {
     void onStartOnlyFront(TalkMessageCtrl*);
     bool isTalkStart(const TalkMessageCtrl*);
     bool isTalkEnd(const TalkMessageCtrl*);
+
+    inline void setMessageArgToCurrentGalaxyName(TalkMessageCtrl *pCtrl, const char *pName) {
+        MR::setMessageArg(pCtrl, MR::getGalaxyNameOnCurrentLanguage(pName));
+    }
 };
