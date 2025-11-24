755b9527dc9477666529b01fd0b3e03eb1da0aee
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2025-11-24 21:17:26 +0100

    Match 100% of TalkSupportPlayerWatcher (#1794)
    * Match 100% of TalkSupportPlayerWatcher
    
    * Use ->length() instead of PSVEMag()

--- include/Game/NPC/TalkSupportPlayerWatcher.hpp
+++ include/Game/NPC/TalkSupportPlayerWatcher.hpp
@@ -9,8 +9,8 @@ public:
     void update();
     bool isEnableTalkPlayerStateNormal() const;
     bool isEnableTalkPlayerStateEvent() const;
-    bool isEnableTalkPlayerStateSpeed() const;
+    bool isEnableTalkPlayerSpeed() const NO_INLINE;
 
-    u32 _04;
-    u32 _08;
+    u32 _00;  // Counts consecutive frames where Mario has a velocity < 4.0f
+    u32 _04;  // Counts consecutive frames where Mario has a velocity >= 4.0f
 };
