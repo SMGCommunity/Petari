d0f035728ca1c25d5aab78032f5ef30d42e0e265
shibbo <supershibbo12@gmail.com>
2025-11-30 00:33:15 -0500

    fix redefine error

--- src/RVL_SDK/exi/EXICommon.c
+++ src/RVL_SDK/exi/EXICommon.c
@@ -2,24 +2,20 @@
 
 const u32 __EXIFreq = 4;
 
-static inline u32	__EXISwap32(u32 val) {
-	return ((val >> 24) & 0x000000FF) | ((val >> 8) & 0x0000FF00) | ((val << 8) & 0x00FF0000) | ((val << 24) & 0xFF000000);
-}
-
 BOOL EXIWriteReg(s32 chan, u32 dev, u32 exiCmd, void* reg, s32 size) {
     BOOL ret = FALSE;
     u32 reg32;
 
     switch (size) {
-        case 1:
-            reg32 = (u32)((*((u8*)reg) & 0xFF) << 24);
-            break;
-        case 2:
-            reg32 = (u32)(((*((u16 *)reg) & 0xFF) << 24) | ((*((u16*)reg) & 0xFF00) << 8));
-            break;
-        default:
-            reg32 = __EXISwap32(*((u32*)reg));
-            break;
+    case 1:
+        reg32 = (u32)((*((u8*)reg) & 0xFF) << 24);
+        break;
+    case 2:
+        reg32 = (u32)(((*((u16*)reg) & 0xFF) << 24) | ((*((u16*)reg) & 0xFF00) << 8));
+        break;
+    default:
+        reg32 = __EXISwap32(*((u32*)reg));
+        break;
     }
 
     ret |= !EXILock(chan, dev, 0);
