9b1a50b53f1adf4f5594e98b66858dca952ee004
shibbo <supershibbo12@gmail.com>
2025-11-30 00:32:42 -0500

    match `exi2`

--- libs/RVL_SDK/include/revolution/exi.h
+++ libs/RVL_SDK/include/revolution/exi.h
@@ -35,6 +35,40 @@ void __OSEnableBarnacle(s32, u32);
 
 /* exi2 */
 
+typedef struct EXIChannelParam {
+    u32 cpr;     // 0x0
+    void* mar;   // 0x4
+    u32 length;  // 0x8
+    u32 cr;      // 0xC
+    u32 data;    // 0x10
+} EXIChannelParam;
+
+#ifdef __MWERKS__
+volatile EXIChannelParam EXI_CHAN_PARAMS[3] : 0xCD006800;
+#else
+volatile EXIChannelParam EXI_CHAN_PARAMS[3];
+#endif
+
+// CPR - Channel Parameter Register
+#define EXI_CPR_EXIINTMASK (1 << 0)
+#define EXI_CPR_EXIINT (1 << 1)
+#define EXI_CPR_TCINTMASK (1 << 2)
+#define EXI_CPR_TCINT (1 << 3)
+#define EXI_CPR_CLK (1 << 4 | 1 << 5 | 1 << 6)
+#define EXI_CPR_CS0B (1 << 7)
+#define EXI_CPR_CS1B (1 << 8)
+#define EXI_CPR_CS2B (1 << 9)
+#define EXI_CPR_EXTINTMASK (1 << 10)
+#define EXI_CPR_EXTINT (1 << 11)
+#define EXI_CPR_EXT (1 << 12)
+#define EXI_CPR_ROMDIS (1 << 13)
+
+// CR - Control Register
+#define EXI_CR_TSTART (1 << 0)
+#define EXI_CR_DMA (1 << 1)
+#define EXI_CR_RW (1 << 2 | 1 << 3)
+#define EXI_CR_TLEN (1 << 4 | 1 << 5)
+
 BOOL __EXI2Imm(void* mem, s32 size, u32 type);
 void __DBEXIInit(void);
 BOOL __DBEXIReadReg(u32 cmd, void* mem, s32 size);
@@ -80,6 +114,10 @@ static BOOL __DBWriteMailbox(u32 mail) {
 
 static BOOL __DBWrite(u32 ofs, const void* src, u32 size) {
     return __DBEXIWriteRam((((ofs + 0xD10000) * 0x40) & 0x3FFFFF00) | 0x80000000, src, size);
+}
+
+static u32 __EXISwap32(u32 val) {
+    return val >> 24 & 0x000000FF | val >> 8 & 0x0000FF00 | val << 8 & 0x00FF0000 | val << 24 & 0xFF000000;
 }
 
 #ifdef __cplusplus
