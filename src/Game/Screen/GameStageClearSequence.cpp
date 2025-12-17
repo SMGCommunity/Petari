ad449ac5064ecba3b50228dd5214ec82841044bb
ThatNintendoNerd <samtheman1026@gmail.com>
2025-12-16 22:28:06 -0800

    Cleanup `GameStageClearSequence`

--- src/Game/Screen/GameStageClearSequence.cpp
+++ src/Game/Screen/GameStageClearSequence.cpp
@@ -1,50 +1,58 @@
-#include "Game/Screen/GameStageClearSequence.hpp"
+#include "Game/LiveActor/Nerve.hpp"
 #include "Game/MapObj/PowerStar.hpp"
-#include "Game/Screen/LayoutActor.hpp"
+#include "Game/Screen/GameStageClearSequence.hpp"
 #include "Game/Screen/SimpleLayout.hpp"
-#include "Game/Util.hpp"
 #include "Game/Util/EventUtil.hpp"
-#include "Game/Util/LiveActorUtil.hpp"
+#include "Game/Util/LayoutUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
 #include "Game/Util/ScreenUtil.hpp"
 #include "Game/Util/SequenceUtil.hpp"
+#include "Game/Util/SoundUtil.hpp"
+
+namespace {
+    static const s32 sClearDemoStopFrame = 10;
+    static const s32 sLayoutStartStepPowerStar = 247;
+    static const s32 sClearDemoWipeStepPowerStar = 325;
+    static const s32 sClearDemoWipeFramePowerStar = 80;
+    static const s32 sFanfareStartStepPowerStar = 15;
+    static const f32 sLayoutOffsetYPowerStar = 80.0f;
+    static const s32 sLayoutStartStepGrandStar = 510;
+    static const s32 sClearDemoWipeStepGrandStar = 670;
+    static const s32 sFanfareStartStepGrandStar = 53;
+    static const f32 sLayoutOffsetYGrandStar = 20.0f;
+    static const s32 sClearDemoWipeStepKoopaVs3 = 840;
+    static const s32 sClearDemoWipeFrameKoopaVs3 = 60;
+};  // namespace
 
 namespace NrvGameStageClearSequence {
     NEW_NERVE(GameStageClearSequencePowerStarGetDemo, GameStageClearSequence, PowerStarGetDemo);
     NEW_NERVE(GameStageClearSequenceGrandStarGetDemo, GameStageClearSequence, GrandStarGetDemo);
 };  // namespace NrvGameStageClearSequence
 
-GameStageClearSequence::GameStageClearSequence() : LayoutActor("ステージクリアシーケンス", true) {
-    mEffectLayout = nullptr;
-    mFollowPos.x = 0.0f;
-    mFollowPos.y = 0.0f;
-}
+GameStageClearSequence::GameStageClearSequence() : LayoutActor("ステージクリアシーケンス", true), mEffectLayout(nullptr), mEffectFollowPos(0.0f, 0.0f) {}
 
 void GameStageClearSequence::init(const JMapInfoIter& rIter) {
     mEffectLayout = new SimpleEffectLayout("スター取得", "GetStar", 1, -1);
     mEffectLayout->initWithoutIter();
-    MR::setFollowPos(&mFollowPos, mEffectLayout, nullptr);
+
+    MR::setFollowPos(&mEffectFollowPos, mEffectLayout, nullptr);
     MR::setFollowTypeAdd(mEffectLayout, nullptr);
     mEffectLayout->kill();
+
     initNerve(&NrvGameStageClearSequence::GameStageClearSequencePowerStarGetDemo::sInstance);
     kill();
 }
 
 void GameStageClearSequence::startPowerStarGetDemo() {
     LayoutActor::appear();
-    TVec2f pos;
-    pos.x = 0.0f;
-    pos.y = 80.0f;
-    mFollowPos.set(pos);
+    mEffectFollowPos.set(TVec2f(0.0f, sLayoutOffsetYPowerStar));
     MR::setTextBoxGameMessageRecursive(mEffectLayout, "GetStar", "GetStar_PowerStar");
     setNerve(&NrvGameStageClearSequence::GameStageClearSequencePowerStarGetDemo::sInstance);
 }
 
 void GameStageClearSequence::startGrandStarGetDemo() {
     LayoutActor::appear();
-    TVec2f pos;
-    pos.x = 0.0f;
-    pos.y = 80.0f;
-    mFollowPos.set(pos);
+    mEffectFollowPos.set(TVec2f(0.0f, sLayoutOffsetYGrandStar));
     MR::setTextBoxGameMessageRecursive(mEffectLayout, "GetStar", "GetStar_GrandStar");
     setNerve(&NrvGameStageClearSequence::GameStageClearSequenceGrandStarGetDemo::sInstance);
 }
@@ -59,30 +67,29 @@ void GameStageClearSequence::exePowerStarGetDemo() {
     if (MR::isFirstStep(this)) {
         MR::stopStageBGM(2);
         MR::setSoundVolumeSetting(1, 20);
-        MR::stopScene(10);
+        MR::stopScene(sClearDemoStopFrame);
         MR::resetGlobalTimer();
         MR::deactivateDefaultGameLayout();
         MR::endGalaxyCometEvent();
     }
 
-    if (MR::isStep(this, 15)) {
+    if (MR::isStep(this, sFanfareStartStepPowerStar)) {
         MR::startSubBGM("BGM_CLEAR", false);
     }
 
-    if (MR::isStep(this, 247)) {
+    if (MR::isStep(this, sLayoutStartStepPowerStar)) {
         appearGetStarLayout();
     }
 
-    if (MR::isStep(this, 325)) {
-        MR::closeWipeCircle(80);
+    if (MR::isStep(this, sClearDemoWipeStepPowerStar)) {
+        MR::closeWipeCircle(sClearDemoWipeFramePowerStar);
     }
 
     bool isWipeActive = MR::isWipeActive();
-    if (MR::isGreaterStep(this, 325)) {
-        if (!isWipeActive) {
-            MR::requestChangeStageAfterStageClear();
-            kill();
-        }
+
+    if (MR::isGreaterStep(this, sClearDemoWipeStepPowerStar) && !isWipeActive) {
+        MR::requestChangeStageAfterStageClear();
+        kill();
     }
 }
 
@@ -90,13 +97,13 @@ void GameStageClearSequence::exeGrandStarGetDemo() {
     if (MR::isFirstStep(this)) {
         MR::stopStageBGM(2);
         MR::setSoundVolumeSetting(1, 20);
-        MR::stopScene(10);
+        MR::stopScene(sClearDemoStopFrame);
         MR::resetGlobalTimer();
         MR::deactivateDefaultGameLayout();
         MR::endGalaxyCometEvent();
     }
 
-    if (MR::isStep(this, 53)) {
+    if (MR::isStep(this, sFanfareStartStepGrandStar)) {
         if (PowerStar::isCurrentStageKoopaVs3()) {
             MR::startSubBGM("BGM_GRAND_STAR_GET_2", false);
         } else {
@@ -104,29 +111,29 @@ void GameStageClearSequence::exeGrandStarGetDemo() {
         }
     }
 
-    if (MR::isStep(this, 510)) {
+    if (MR::isStep(this, sLayoutStartStepGrandStar)) {
         appearGetStarLayout();
     }
 
     if (PowerStar::isCurrentStageKoopaVs3()) {
-        if (MR::isStep(this, 840)) {
-            MR::closeSystemWipeWhiteFade(60);
+        if (MR::isStep(this, sClearDemoWipeStepKoopaVs3)) {
+            MR::closeSystemWipeWhiteFade(sClearDemoWipeFrameKoopaVs3);
         }
 
-        bool wipeActive = MR::isSystemWipeActive();
+        bool isWipeActive = MR::isSystemWipeActive();
 
-        if (MR::isGreaterStep(this, 840) && !wipeActive) {
+        if (MR::isGreaterStep(this, sClearDemoWipeStepKoopaVs3) && !isWipeActive) {
             MR::requestChangeStageAfterStageClear();
             kill();
         }
     } else {
-        if (MR::isStep(this, 670)) {
+        if (MR::isStep(this, sClearDemoWipeStepGrandStar)) {
             MR::closeWipeCircle(-1);
         }
 
-        bool wipeActive = MR::isWipeActive();
+        bool isWipeActive = MR::isWipeActive();
 
-        if (MR::isGreaterStep(this, 670) && !wipeActive) {
+        if (MR::isGreaterStep(this, sClearDemoWipeStepGrandStar) && !isWipeActive) {
             MR::requestChangeStageAfterStageClear();
             kill();
         }
