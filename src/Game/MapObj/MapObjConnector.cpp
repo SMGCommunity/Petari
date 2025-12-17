bef5ab3533147cbe02cdfd8eb94af4df7e29076c
ThatNintendoNerd <samtheman1026@gmail.com>
2025-12-16 17:35:17 -0800

    Fix return types in `MapObjConnector`

--- src/Game/MapObj/MapObjConnector.cpp
+++ src/Game/MapObj/MapObjConnector.cpp
@@ -8,16 +8,18 @@ MapObjConnector::MapObjConnector(LiveActor* pActor) {
     _8.identity();
 }
 
-void MapObjConnector::attachToUnder() {
+bool MapObjConnector::attachToUnder() {
     TVec3f up_vec;
     MR::calcUpVec(&up_vec, mHost);
-    attach(up_vec);
+
+    return attach(up_vec);
 }
 
-void MapObjConnector::attachToBack() {
+bool MapObjConnector::attachToBack() {
     TVec3f front_vec;
     MR::calcFrontVec(&front_vec, mHost);
-    attach(front_vec);
+
+    return attach(front_vec);
 }
 
 void MapObjConnector::connect() {
