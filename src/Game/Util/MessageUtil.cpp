3241344a0e7727d859d6df5f9b80ac6112c12402
ThatNintendoNerd <samtheman1026@gmail.com>
2025-12-19 15:11:48 -0800

    Match 45% of `MessageUtil`

---
+++ src/Game/Util/MessageUtil.cpp
@@ -0,0 +1,94 @@
+#include "Game/Util/MessageUtil.hpp"
+#include "Game/Map/RaceManager.hpp"
+#include "Game/NPC/TalkMessageInfo.hpp"
+#include "Game/System/MessageHolder.hpp"
+#include "Game/Util/SceneUtil.hpp"
+#include <cstdio>
+
+#define MESSAGE_ID_BUFFER_SIZE 256
+
+namespace MR {
+    const wchar_t* getSystemMessageDirect(const char* pMessageId) {
+        TalkMessageInfo messageInfo = TalkMessageInfo();
+
+        MessageSystem::getSystemMessageDirect(&messageInfo, pMessageId);
+
+        return reinterpret_cast<wchar_t*>(messageInfo._0);
+    }
+
+    const wchar_t* getGameMessageDirect(const char* pMessageId) {
+        TalkMessageInfo messageInfo = TalkMessageInfo();
+
+        MessageSystem::getGameMessageDirect(&messageInfo, pMessageId);
+
+        return reinterpret_cast<wchar_t*>(messageInfo._0);
+    }
+
+    const wchar_t* getLayoutMessageDirect(const char* pMessageId) {
+        TalkMessageInfo messageInfo = TalkMessageInfo();
+
+        MessageSystem::getLayoutMessageDirect(&messageInfo, pMessageId);
+
+        return reinterpret_cast<wchar_t*>(messageInfo._0);
+    }
+
+    const wchar_t* getCurrentGalaxyNameOnCurrentLanguage() {
+        return getGalaxyNameOnCurrentLanguage(getCurrentStageName());
+    }
+
+    const wchar_t* getCurrentGalaxyNameShortOnCurrentLanguage() {
+        return getGalaxyNameShortOnCurrentLanguage(getCurrentStageName());
+    }
+
+    const wchar_t* getCurrentScenarioNameOnCurrentLanguage() {
+        s32 selectedScenarioNo = getCurrentSelectedScenarioNo();
+        s32 scenarioNo = selectedScenarioNo != -1 ? selectedScenarioNo : getCurrentScenarioNo();
+
+        return getScenarioNameOnCurrentLanguage(getCurrentStageName(), scenarioNo);
+    }
+
+    const wchar_t* getRaceNameOnCurrentLanguage(int raceId) {
+        return getGameMessageDirect(RaceManagerFunction::getRaceMessageId(raceId));
+    }
+
+    bool isExistGameMessage(const char* pMessageId) {
+        TalkMessageInfo messageInfo = TalkMessageInfo();
+
+        return MessageSystem::getGameMessageDirect(&messageInfo, pMessageId) && getStringLengthWithMessageTag(reinterpret_cast<wchar_t*>(messageInfo._0)) != nullptr;
+    }
+
+    // getMessageLine
+    // countMessageLine
+    // countMessageChar
+    // countMessageFigure
+    // getNextMessagePage
+
+    const wchar_t* getGalaxyNameOnCurrentLanguage(const char* pGalaxyName) {
+        char messageId[MESSAGE_ID_BUFFER_SIZE];
+        snprintf(messageId, sizeof(messageId), "GalaxyName_%s", pGalaxyName);
+
+        return getGameMessageDirect(messageId);
+    }
+
+    const wchar_t* getGalaxyNameShortOnCurrentLanguage(const char* pGalaxyName) {
+        char messageId[MESSAGE_ID_BUFFER_SIZE];
+        snprintf(messageId, sizeof(messageId), "GalaxyNameShort_%s", pGalaxyName);
+
+        return getGameMessageDirect(messageId);
+    }
+
+    const wchar_t* getScenarioNameOnCurrentLanguage(const char* pGalaxyName, s32 scenarioNo) {
+        char messageId[MESSAGE_ID_BUFFER_SIZE];
+        snprintf(messageId, sizeof(messageId), "ScenarioName_%s%d", pGalaxyName, scenarioNo);
+
+        return getGameMessageDirect(messageId);
+    }
+
+    void getLayoutMessageID(char* pDst, const char* pSuperMessageId, const char* pSubMessageId) {
+        snprintf(pDst, MESSAGE_ID_BUFFER_SIZE, "Layout_%s%s", pSuperMessageId, pSubMessageId);
+    }
+
+    void makeCometMessageID(char* pDst, u32 bufferSize, const char* pCometName) {
+        snprintf(pDst, bufferSize, "CometName_%s", pCometName);
+    }
+};  // namespace MR
