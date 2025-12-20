28d6085e8b425436ecb9e20fc730f2f7fed0204f
ThatNintendoNerd <samtheman1026@gmail.com>
2025-12-20 15:12:18 -0800

    Document three `CenterScreenBlur` member variables

--- include/Game/Screen/CenterScreenBlur.hpp
+++ include/Game/Screen/CenterScreenBlur.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Game/LiveActor/LiveActor.hpp"
-#include <revolution/types.h>
 
 class CenterScreenBlur : public LiveActor {
 public:
@@ -22,9 +21,9 @@ public:
     void exeKeep();
     void exeFadeOut();
 
-    s32 _8C;
-    s32 _90;
-    s32 _94;
+    s32 mTotalFrame;
+    s32 mFadeInFrame;
+    s32 mFadeOutFrame;
     f32 _98;
     u8 _9C;
     f32 _A0;
