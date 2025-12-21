3e6035a3f6e353a981cdd8b99decd763ad5b6daa
kaenas27 <kaenas27@protonmail.com>
2025-12-22 00:58:21 +0100

    Match 99% of AudFader::set(float, long)

--- src/Game/AudioLib/AudFader.cpp
+++ src/Game/AudioLib/AudFader.cpp
@@ -1,12 +1,15 @@
 #include "Game/AudioLib/AudFader.hpp"
 
-AudFader::AudFader(): mFloatVar(1.f), mFloatVar2(1.f), mFloatVar3(0.f){
+AudFader::AudFader() : mFloatVar(1.f), mFloatVar2(1.f), mFloatVar3(0.f) {}
+
+void AudFader::set(float floatVar, long longVar) {
+    if (longVar == 0) {
+        mFloatVar = floatVar;
+        mFloatVar3 = 0.f;
+    } else {
+        mFloatVar3 = (floatVar - mFloatVar) / (float)(longVar);
+    }
+    mFloatVar2 = floatVar;
 }
 
-void AudFader::set(float floatVar, long longVar){
-
-}
-
-void AudFader::update(){
-
-}
+void AudFader::update() {}
