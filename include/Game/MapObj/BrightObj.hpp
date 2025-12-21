b70d6d9851df908bf572aa8572bea6bde2912397
ThatNintendoNerd <samtheman1026@gmail.com>
2025-12-20 16:32:57 -0800

    Match 71% of `LensFlare`

--- include/Game/MapObj/BrightObj.hpp
+++ include/Game/MapObj/BrightObj.hpp
@@ -6,30 +6,35 @@ class BrightInfo {
 public:
     BrightInfo();
 
+    void write(const TVec2f&, const TVec2f&, f32);
     void endRead();
     void reset();
 
-    u32 _0;
-    u32 _4;
-    TVec2f mBrightnessCenter[3];  // 0x8
-    TVec2f mRealCenter[3];        // 0x20
-    f32 mBright[3];               // 0x38
+    /* 0x00 */ u32 _0;
+    /* 0x04 */ u32 _4;
+    /* 0x08 */ TVec2f mBrightnessCenter[3];
+    /* 0x20 */ TVec2f mRealCenter[3];
+    /* 0x38 */ f32 mBright[3];
 };
 
 class BrightDrawInfo {
 public:
     BrightDrawInfo();
 
-    TVec3f mPosition[2];  // 0x0
-    f32 mRadius[2];       // 0x18
+    void write(u16, const TVec3f&, f32);
+
+    /* 0x00 */ TVec3f mPosition[2];
+    /* 0x18 */ f32 mRadius[2];
 };
 
 class BrightCamInfo {
 public:
     BrightCamInfo();
 
-    TMtx34f mViewMtx[2];      // 0x0
-    Mtx44 mProjectionMtx[2];  // 0x60
-    TVec3f mCameraDir[2];     // 0xE0
-    TVec3f mCameraPos[2];     // 0xF8
+    // void write(u16, const TPos3f&, const JGeometry::TProjection3<JGeometry::TMatrix44<JGeometry::SMatrix44C<f32>>>&, const TVec3f&, const TVec3f&)
+
+    /* 0x00 */ TMtx34f mViewMtx[2];      // 0x0
+    /* 0x00 */ Mtx44 mProjectionMtx[2];  // 0x60
+    /* 0x00 */ TVec3f mCameraDir[2];     // 0xE0
+    /* 0x00 */ TVec3f mCameraPos[2];     // 0xF8
 };
