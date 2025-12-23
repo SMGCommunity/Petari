fae7305f036bf4cdc3b53db416cdd3f223e27be4
kaenas27 <kaenas27@protonmail.com>
2025-12-23 17:45:16 +0100

    Match 68% of AudFader::update()

--- src/Game/AudioLib/AudFader.cpp
+++ src/Game/AudioLib/AudFader.cpp
@@ -12,4 +12,21 @@ void AudFader::set(float floatVar, long longVar) {
     mFloatVar2 = floatVar;
 }
 
-void AudFader::update() {}
+void AudFader::update() {
+    if (mFloatVar3 == 0.0f) {
+        return;
+    }
+
+    mFloatVar += mFloatVar3;
+    //mFloatVar3 += mFloatVar;
+    if (mFloatVar3 > 0.0f) {
+        // mFloatVar += mFloatVar3;
+        // mFloatVar = mFloatVar2;
+        if (mFloatVar3 <= mFloatVar2) {
+            mFloatVar = mFloatVar2;
+            mFloatVar3 = 0.0f;
+            //mFloatVar3 = mFloatVar;
+        }
+    }
+    // NOTE: put else here and match instructions from 0x20 to 0x3C  
+}
