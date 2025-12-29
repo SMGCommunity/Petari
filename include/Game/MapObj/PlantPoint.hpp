3866060ae3a9dd7a566e863eebfa4fcbe8ab10e6
MrL314 <11429905+MrL314@users.noreply.github.com>
2025-12-29 16:20:13 -0500

    Fix some axis naming issues with `Plant`

--- include/Game/MapObj/PlantPoint.hpp
+++ include/Game/MapObj/PlantPoint.hpp
@@ -9,8 +9,8 @@ public:
     virtual void setAxisAndPos(const TVec3f&, const TVec3f&, const TVec3f&, const TVec3f&);
 
     /* 0x04 */ TVec3f mPosition;
-    /* 0x10 */ TVec3f mFront;
+    /* 0x10 */ TVec3f mSide;
     /* 0x1C */ TVec3f mUp;
-    /* 0x28 */ TVec3f mSide;
+    /* 0x28 */ TVec3f mFront;
     /* 0x34 */ f32 mThickness;
 };
