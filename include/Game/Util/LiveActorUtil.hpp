302dbd6055bed5d273fc5d7435dfa45a2e69a7af
ThePlayerRolo <thesolis78243@gmail.com>
2026-01-10 17:39:33 -0500

    PlanetMap + QuestionBoxGallery (#1844)
    * PlanetMap + QuestionBoxGalleryObj
    
    * ckeanup
    
    * Cleanup v2
    
    * last fix for this pr :pray:

--- include/Game/Util/LiveActorUtil.hpp
+++ include/Game/Util/LiveActorUtil.hpp
@@ -339,7 +339,7 @@ namespace MR {
 
     bool changeShowModelFlagSyncNearClipping(LiveActor*, f32);
 
-    u32 createIndirectPlanetModel(LiveActor*, MtxPtr);
+    PartsModel* createIndirectPlanetModel(LiveActor*, MtxPtr);
     bool tryCreateMirrorActor(LiveActor*, const char*);
 
     CollisionParts* createCollisionPartsFromLiveActor(LiveActor*, const char*, HitSensor*, CollisionScaleType);
@@ -394,6 +394,7 @@ namespace MR {
 
     ResTIMG* getTexFromArc(const char*, const LiveActor*);
     PartsModel* createBloomModel(LiveActor*, MtxPtr);
+    PartsModel* createWaterModel(LiveActor*, MtxPtr);
 
     TexMtxCtrl* initDLMakerTexMtx(LiveActor*, const char*);
 
@@ -414,6 +415,8 @@ namespace MR {
     TVec3f* getBindedFixReactionVector(const LiveActor*);
 
     CollisionParts* tryCreateCollisionSunshade(LiveActor*, HitSensor*);
+
+    CollisionParts* tryCreateCollisionWaterSurface(LiveActor*, HitSensor*);
 
     const char* getPlayingBckName(const LiveActor*);
 };  // namespace MR
