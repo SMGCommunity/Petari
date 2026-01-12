c754d790ed7c7567a398fbc6b75f0160cd3e8b79
alexsr1 <alexrudd235@gmail.com>
2026-01-12 22:22:56 +0100

    More MarioSwim decompilation

--- include/Game/Player/MarioActor.hpp
+++ include/Game/Player/MarioActor.hpp
@@ -233,10 +233,10 @@ public:
 
     void entryWallWalkMode(const TVec3f&, const TVec3f&);
 
-    const HitSensor& getCarrySensor() const;
+    const HitSensor* getCarrySensor() const;
 
     const MarioConst& getConst() const { return *mConst; }
-
+    
     inline u32 getHealth() const { return mHealth; }
 
     inline const Mario::MovementStates& getMovementStates() const { return mMario->mMovementStates; }
@@ -628,7 +628,10 @@ public:
     u32 _F24;
     u16 _F28;
     // padding
-    TVec3f _F2C;
+    union {
+        u32 _F2C;
+        TVec3f _F2CVec;
+    };
     u8 _F38;
     // padding
     union {
