18173e0b7b8d8b6fa09bbb49249e149fd18c885a
Super Hackio <superhaxio@gmail.com>
2026-01-13 14:45:40 -0800

    DynamicCollisionObj all functions matched
    Only the data segments remain unmatched

--- include/Game/MapObj/DynamicCollisionObj.hpp
+++ include/Game/MapObj/DynamicCollisionObj.hpp
@@ -3,6 +3,7 @@
 #include "Game/LiveActor/LiveActor.hpp"
 
 class CollisionParts;
+struct KCLFile;
 
 class DynamicCollisionObj : public LiveActor {
 public:
@@ -23,16 +24,16 @@ public:
     void updateCollisionHeader();
     void createCollision();
 
-    void* _8C;
-    CollisionParts* mParts;  // 0x90
-    u16 _94;
-    u16 mNrTriangles;    // 0x96
-    TVec3f* mPositions;  // 0x98
-    TVec3f* _9C;
-    TriangleIndexing* mIndices;  // 0xA0
-    u32 _A4;
-    u32 _A8;
-    u32 _AC;
-    u8 _B0[0x6C];
-    u32 _11C;
+    KCLFile* mKCLFile;           //  0x8C
+    CollisionParts* mParts;      //  0x90
+    u16 _94;                     //  0x94 : Always set to 2 by AreaPolygon::init
+    u16 mPositionNum;            //  0x96
+    TVec3f* mPositions;          //  0x98
+    TVec3f* _9C;                 //  0x9C
+    TriangleIndexing* mIndices;  //  0xA0
+    u32 _A4;                     //  0xA4
+    u32 _A8;                     //  0xA8
+    u32 _AC;                     //  0xAC
+    u8 _B0[0x6C];                //  0xB0 : Seems completely unused
+    u32 _11C;                    // 0x11C : Only used by FollowCollisionArea
 };
