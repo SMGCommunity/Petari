be375eb8dee3f2e4fa14ee4cd941e3ead6dd8486
shibbo <supershibbo12@gmail.com>
2026-01-13 20:28:25 -0500

    begin documenting `DinoPackun` headers

---
+++ include/Game/Boss/DinoPackunSequencer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Game/System/NerveExecutor.hpp"
+
+class DinoPackun;
+class HitSensor;
+
+class DinoPackunSequencer : public NerveExecutor {
+public:
+    DinoPackunSequencer(const char*, DinoPackun*);
+
+    virtual ~DinoPackunSequencer();
+    virtual void start();
+    virtual void init();
+    virtual bool isUseEggShell() const;
+    virtual u32 getVsCount() const;
+    virtual void update();
+    virtual void attackSensor(HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveMsgPush(HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveMsgPlayerAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveMsgEnemyAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+    virtual void attackSensorTail(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttackTail(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsgTail(u32, HitSensor*, HitSensor*);
+
+    DinoPackun* mParent;  // 0x8
+    u32 _C;
+};
