b20f0565ed3a7de508298805b2643b89c681978e
Gonzalo-kebab <koen.bommol@gmail.com>
2026-01-21 20:25:47 +0100

    remove newlines in StarPieceFollowGroup

--- include/Game/MapObj/StarPieceFollowGroup.hpp
+++ include/Game/MapObj/StarPieceFollowGroup.hpp
@@ -24,12 +24,10 @@ public:
     TVec3f* _90;
     /* 0x94 */ s32 mNumPiecesTotal;
     /* 0x98 */ s32 mNumPiecesUse;
-
     /* 0x9C */ f32 mRadius;
     // appears to go unused
     u32 _A0;
     /* 0xA4 */ f32 mAngle;
-
     /* 0xA8 */ s32 mTimeLimit;
     /* 0xAC */ TVec3f mLocalXAxis;
     /* 0xB8 */ TVec3f mLocalZAxis;
