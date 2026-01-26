e6c8874ffe97e60a173d921be449731598093474
barym <116033642+barym@users.noreply.github.com>
2026-01-26 02:55:19 +0100

    Match and link `JASResArcLoader`

--- configure.py
+++ configure.py
@@ -3002,7 +3002,7 @@ config.libs = [
             Object(NonMatching, "JSystem/JAudio2/JASDvdThread.cpp"),
             Object(NonMatching, "JSystem/JAudio2/JASCallback.cpp"),
             Object(NonMatching, "JSystem/JAudio2/JASHeapCtrl.cpp"),
-            Object(NonMatching, "JSystem/JAudio2/JASResArcLoader.cpp"),
+            Object(Matching, "JSystem/JAudio2/JASResArcLoader.cpp"),
             Object(NonMatching, "JSystem/JAudio2/JASProbe.cpp"),
             Object(NonMatching, "JSystem/JAudio2/JASReport.cpp"),
             Object(NonMatching, "JSystem/JAudio2/JASCmdStack.cpp"),
