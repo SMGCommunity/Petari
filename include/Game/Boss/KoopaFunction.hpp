f9d33d253a4961b2959dd6ea047eaf63050d96ce
SushiInYourFace <joel_olmsted@icloud.com>
2026-01-27 16:03:40 -0500

    as much progress on jumpToPlanet before needing PlanetShadow

--- include/Game/Boss/KoopaFunction.hpp
+++ include/Game/Boss/KoopaFunction.hpp
@@ -11,10 +11,13 @@ namespace KoopaFunction {
     void initKoopaCamera(Koopa*, const char*);
     void endKoopaCamera(Koopa*, const char*, bool, s32);
     void setKoopaPos(Koopa*, const char*);
+    void startKoopaCamera(Koopa*, const char*);
     void startKoopaTargetCamera(Koopa*, const char*);
+    void startFaceCtrl(Koopa*);
     bool isKoopaVs1(const Koopa*);
     bool isKoopaVs2(const Koopa*);
     bool isKoopaLv3(const Koopa*);
     bool isKoopaVs3(const Koopa*);
+    void* getKoopaPlanetShadow(Koopa*);
     LiveActor* getKoopaPowerUpSwitch(Koopa*);
 };  // namespace KoopaFunction
