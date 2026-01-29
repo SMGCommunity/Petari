1d1e9a010ab8043a1e3a777aacbd2a7fcfab40af
Amrit Bhogal <ambhogal01@gmail.com>
2026-01-29 16:26:44 +0000

    marioanimator 98% fuzzy match

--- include/Game/Player/MarioActor.hpp
+++ include/Game/Player/MarioActor.hpp
@@ -5,6 +5,7 @@
 #include "Game/Player/Mario.hpp"
 
 class FootPrint;
+class J3DModelData;
 class JAIAudible;
 class MarioNullBck;
 class XjointTransform;
@@ -238,6 +239,12 @@ public:
     void entryWallWalkMode(const TVec3f&, const TVec3f&);
 
     const HitSensor* getCarrySensor() const;
+    HitSensor* getLookTargetSensor() const;
+    f32 getFaceLookHeight(const char*) const;
+    void updateSpecialModeAnimation();
+    J3DModelData* getModelData() const;
+    void rushDropThrowMemoSensor();
+    void offTakingFlag();
 
     const MarioConst& getConst() const { return *mConst; }
 
@@ -388,12 +395,12 @@ public:
     u32 _41C;
     u32 _420;
     u32 _424;
-    u32 _428[4];
+    u32 _428[4]; // 0x428 - 0x434
     u8 _438[0x30];
     union {
         struct {
             u32 _468;
-            u32 _46C;
+            HitSensor* _46C;
             u32 _470;
         };
         TVec3f _468Vec;
