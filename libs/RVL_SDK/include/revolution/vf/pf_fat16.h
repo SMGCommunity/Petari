5585580b1c469f664983def6e0a636c69859335c
shibbo <supershibbo12@gmail.com>
2026-01-31 00:52:30 -0500

    `pf_fat16` at 61%

--- libs/RVL_SDK/include/revolution/vf/pf_fat16.h
+++ libs/RVL_SDK/include/revolution/vf/pf_fat16.h
@@ -9,4 +9,6 @@ s32 VFiPFFAT16_ReadFATEntryPage(PF_VOLUME* p_vol, u32 cluster, u32* p_value, PF_
 
 s32 VFiPFFAT16_WriteFATEntry(PF_VOLUME* p_vol, u32 cluster, u32 value);
 
+s32 VFiPFFAT16_WriteFATEntryPage(PF_VOLUME* p_vol, u32 cluster, u32 value, PF_CACHE_PAGE** pp_page);
+
 #endif  // PF_FAT16_H
