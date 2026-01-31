e1f23fe9c730668d3f8751a359947559d696bc95
shibbo <supershibbo12@gmail.com>
2026-01-31 16:20:34 -0500

    start on `pf_entry`

--- libs/RVL_SDK/include/revolution/vf/pf_clib.h
+++ libs/RVL_SDK/include/revolution/vf/pf_clib.h
@@ -7,4 +7,6 @@ void* VFipf_memset(void* dst, long c, unsigned long length);
 
 void* VFipf_memcpy(void* dst, void* src, u32 length);
 
+s8* VFipf_strcpy(s8* dst, const s8* src);
+
 #endif  // PF_CLIB_H
