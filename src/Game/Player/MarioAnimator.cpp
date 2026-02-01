59106591090db58b8c95efa7ca858004f34ae642
Amrit Bhogal <ambhogal01@gmail.com>
2026-02-01 15:31:53 +0000

    use INIT_NERVE(...) instead

--- src/Game/Player/MarioAnimator.cpp
+++ src/Game/Player/MarioAnimator.cpp
@@ -1372,14 +1372,14 @@ f32 XanimePlayer::tellAnimationFrame() const {
 }
 
 namespace NrvMarioActor {
-    MarioActorNrvWait MarioActorNrvWait::sInstance;
-    MarioActorNrvGameOver MarioActorNrvGameOver::sInstance;
-    MarioActorNrvGameOverAbyss MarioActorNrvGameOverAbyss::sInstance;
-    MarioActorNrvGameOverAbyss2 MarioActorNrvGameOverAbyss2::sInstance;
-    MarioActorNrvGameOverFire MarioActorNrvGameOverFire::sInstance;
-    MarioActorNrvGameOverBlackHole MarioActorNrvGameOverBlackHole::sInstance;
-    MarioActorNrvGameOverNonStop MarioActorNrvGameOverNonStop::sInstance;
-    MarioActorNrvGameOverSink MarioActorNrvGameOverSink::sInstance;
-    MarioActorNrvTimeWait MarioActorNrvTimeWait::sInstance;
-    MarioActorNrvNoRush MarioActorNrvNoRush::sInstance;
-}
+    INIT_NERVE(MarioActorNrvWait);
+    INIT_NERVE(MarioActorNrvGameOver);
+    INIT_NERVE(MarioActorNrvGameOverAbyss);
+    INIT_NERVE(MarioActorNrvGameOverAbyss2);
+    INIT_NERVE(MarioActorNrvGameOverFire);
+    INIT_NERVE(MarioActorNrvGameOverBlackHole);
+    INIT_NERVE(MarioActorNrvGameOverNonStop);
+    INIT_NERVE(MarioActorNrvGameOverSink);
+    INIT_NERVE(MarioActorNrvTimeWait);
+    INIT_NERVE(MarioActorNrvNoRush);
+};  // namespace NrvMarioActor
