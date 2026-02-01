ddc3506f388eb18d12215819d08ec6cf74e1c29d
Gonzalo-kebab <koen.bommol@gmail.com>
2026-02-01 11:13:00 +0100

    revert changes because of regressions

--- libs/JSystem/include/JSystem/JMath/JMATrigonometric.hpp
+++ libs/JSystem/include/JSystem/JMath/JMATrigonometric.hpp
@@ -39,11 +39,11 @@ namespace JMath {
 
         inline f32 sinLapRad(f32 v) {
             if (v < 0.0f) {
-                v *= -(LEN / TWO_PI);
-                return -table[(u16)v & LEN - 1].a1;
+                f32 tmp = v * (-LEN / TWO_PI);
+                return -table[(u16)tmp & LEN - 1].a1;
             } else {
-                v *= (LEN / TWO_PI);
-                return table[(u16)v & LEN - 1].a1;
+                f32 tmp = v * (LEN / TWO_PI);
+                return table[(u16)tmp & LEN - 1].a1;
             }
         }
 
