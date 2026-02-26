5106c460257d9b4f4c07ab39469593c897edfe79
shibbo <supershibbo12@gmail.com>
2026-02-26 09:44:56 -0500

    `NETVersion` and 91% of `nettime`

---
+++ src/RVL_SDK/net/NETVersion.c
@@ -0,0 +1,5 @@
+char* NETRexPPCVersionPrintableString = "<< REX-PPC 2.1.255.1 (RevoEX-2.1plus) REL 080313085908 >>";
+
+char* NETGetRexPPCVersionPrintable() {
+    return NETRexPPCVersionPrintableString;
+}
