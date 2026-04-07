d9329134bb1a8a65356fb8ba937be103b38dae2b
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2026-04-07 22:37:51 +0200

    Match 97% of `Mogucchi` (#1899)
    * add individual TRotation3::setDir inlines
    
    * Mogucchi at 49%
    
    * Mogucchi 55%
    
    * Mogucchi 69%
    
    * Mogucchi 84%
    
    * Mogucchi 93%
    
    * Mogucchi data match
    
    * Mogucchi 97%
    
    * Match Mogucchi::initAfterPlacement
    
    * Fix float mismatch in Mogucchi::updateReferenceMtx()
    
    * Name Mogucchi _9C
    
    * Name Mogucchi _D0
    
    * More Mogucchi member documentation
    
    * Mogucchi NamoObjFactory regression fix
    
    * Remove redundant include in `MogucchiHill.cpp`
    
    * Simplify isDown declaration in `Mogucchi::receuceAttackBySpinSensor`

--- include/Game/Enemy/Mogucchi.hpp
+++ include/Game/Enemy/Mogucchi.hpp
@@ -1,14 +1,53 @@
 #pragma once
 
-#include "Game/NameObj/NameObj.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
 
+class MogucchiHill;
+class ModelObj;
 
 class Mogucchi : public LiveActor {
 public:
     Mogucchi(const char*);
     virtual ~Mogucchi();
 
-private:
-    u8 mPad[(0xF4) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter& rIter);
+    virtual void initAfterPlacement();
+    virtual void makeActorAppeared();
+    virtual void kill();
+    virtual void control();
+    virtual void attackSensor(HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveMsgPlayerAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+
+    void exeStroll();
+    void endStroll();
+    void exeAppearDown();
+    void exeDive();
+    void exeScatter();
+    void exeDie();
+    void exeDown();
+    void initSensor();
+    void createMogucchiHill();
+    void createHole();
+    void updatePosition();
+    void updateReferenceMtx();
+    bool checkHipDrop() const;
+    void updateStrollSpeed();
+    void reflectStarPointer2P();
+    void makeEulerRotation();
+    void calcAttackDir(TVec3f*, const TVec3f&, const TVec3f&) const;
+    bool receiveAttackBySpinSensor(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    bool receiveAttackByBodySensor(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    void calcScatterVec(const TVec3f&, const TVec3f&);
+
+    /* 0x8C */ MogucchiHill* mHill;
+    /* 0x90 */ ModelObj* mHole;
+    /* 0x94 */ TPos3f mNewHolePos;
+    /* 0xC4 */ TVec3f mRailGravity;
+    /* 0xD0 */ bool mGrounded;
+    /* 0xD4 */ f32 mScatterPropulsionSpeed;
+    /* 0xD8 */ TVec3f mScatterNormal;
+    f32 _E4;
+    /* 0xE8 */ f32 mStrollSpeed;
+    /* 0xEC */ f32 mMaxStrollSpeed;
+    /* 0xF0 */ bool mIsStoppedByP2;
 };
---
+++ src/Game/Enemy/MogucchiHill.cpp
@@ -0,0 +1,4 @@
+#include "Game/Enemy/MogucchiHill.hpp"
+
+MogucchiHill::MogucchiHill(LiveActor* pArg1, s32, const char* pName) : LiveActor(pName) {
+}
