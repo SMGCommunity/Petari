0dacf5b5614a380d416284ac23264168002a3076
shibbo <supershibbo12@gmail.com>
2026-05-17 21:07:54 -0400

    lay basic foundation for `Teresa`

--- src/Game/Enemy/Teresa.cpp
+++ src/Game/Enemy/Teresa.cpp
@@ -1,5 +1,29 @@
 #include "Game/Enemy/Teresa.hpp"
 
-Teresa::Teresa(const char* pName) : LiveActor(pName) {}
-Teresa::~Teresa() {}
+namespace NrvTeresa {
+    static Color8 sPointLightColor(255, 255, 255, 255);
+    NEW_NERVE(TeresaNrvAppearFromWall, Teresa, AppearFromWall);
+    NEW_NERVE(TeresaNrvAppearFromGround, Teresa, AppearFromGround);
+    NEW_NERVE(TeresaNrvWait, Teresa, Wait);
+    NEW_NERVE(TeresaNrvWalk, Teresa, Walk);
+    NEW_NERVE(TeresaNrvRailWalk, Teresa, RailWalk);
+    NEW_NERVE(TeresaNrvRailTurn, Teresa, RailTurn);
+    NEW_NERVE(TeresaNrvLoveFind, Teresa, LoveFind);
+    NEW_NERVE(TeresaNrvLoveChase, Teresa, LoveChase);
+    NEW_NERVE(TeresaNrvLoveHit, Teresa, LoveHit);
+    NEW_NERVE(TeresaNrvLoveEnd, Teresa, LoveEnd);
+    NEW_NERVE(TeresaNrvChase, Teresa, Chase);
+    NEW_NERVE(TeresaNrvShay, Teresa, Shay);
+    NEW_NERVE(TeresaNrvAggressive, Teresa, Aggressive);
+    NEW_NERVE(TeresaNrvAttackSuccess, Teresa, AttackSuccess);
+    NEW_NERVE(TeresaNrvDrift, Teresa, Drift);
+    NEW_NERVE(TeresaNrvHideWall, Teresa, HideWall);
+    NEW_NERVE(TeresaNrvHideWater, Teresa, HideWater);
+    NEW_NERVE(TeresaNrvAscension, Teresa, Ascension);
+    NEW_NERVE(TeresaNrvStop, Teresa, Stop);
+};  // namespace NrvTeresa
 
+Teresa::Teresa(const char* pName) : LiveActor(pName) {
+}
+Teresa::~Teresa() {
+}
