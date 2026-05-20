b73d4f27e64a9d11cf53c0ecf4f04108252d5af6
MrL314 <11429905+MrL314@users.noreply.github.com>
2026-05-20 12:05:28 -0400

    "Match" `StarPointerBlur`
    Likely fakematch in `findDrawPointRange`

--- libs/JSystem/include/JSystem/JGeometry/TVec.hpp
+++ libs/JSystem/include/JSystem/JGeometry/TVec.hpp
@@ -48,7 +48,7 @@ namespace JGeometry {
             y += other.y;
         }
 
-        inline TVec2& addInline(const TVec2< T >& other) {
+        inline TVec2& addInline(const TVec2< T >& other) const {
             TVec2 ret(*this);
             ret.add(other);
             return ret;
@@ -98,6 +98,11 @@ namespace JGeometry {
             y = y - rOther.y;
         }
 
+        void sub(const TVec2< T >& rA, const TVec2< T >& rB) {
+            x = rA.x - rB.x;
+            y = rA.y - rB.y;
+        }
+
         inline TVec2& subInline(const TVec2< T >& other) const {
             TVec2 ret = *this;
             ret.sub(other);
@@ -139,14 +144,26 @@ namespace JGeometry {
         }
 
         /* Operators */
-        TVec2< T >& operator+(const TVec2< T >& rOther) const;
+        TVec2< T > operator+(const TVec2< T >& rOther) const {
+            TVec2 ret(*this);
+            ret.x += rOther.x;
+            ret.y += rOther.y;
+            return ret;
+        }
+
         TVec2< T > operator-(const TVec2< T >& rOther) const {
             TVec2 ret(*this);
             ret.x -= rOther.x;
             ret.y -= rOther.y;
             return ret;
         }
-        TVec2< T >& operator*(f32 scale) const;
+
+        TVec2< T > operator*(f32 scale) const {
+            TVec2 ret(*this);
+            ret.x *= scale;
+            ret.y *= scale;
+            return ret;
+        }
 
         inline void operator-=(const TVec2< T >& rOther) {
             x = x - rOther.x;
