30a2d0ac7a7e391622c8fd0db036c024cd608f6e
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-05-21 08:52:57 +0000

    Kiraira (#1917)
    * Kiraira
    
    * make changes

--- include/Game/Enemy/Kiraira.hpp
+++ include/Game/Enemy/Kiraira.hpp
@@ -1,14 +1,46 @@
 #pragma once
 
-#include "Game/NameObj/NameObj.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
 
+class KirairaChain;
+class MsgSharedGroup;
 
 class Kiraira : public LiveActor {
 public:
     Kiraira(const char*);
-    virtual ~Kiraira();
+
+    virtual void init(const JMapInfoIter& rIter);
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
+    void openEyes();
+    void closeEyes();
+    void drift();
+    void driftOnRail();
+    void explode();
+
+    bool isDown() const;
+
+    void exeWait();
+    void exeFaceToMario();
+    void exeFaceToMarioAndStare();
+    void exeExplode();
+    void exeBeExploded();
+    void exeDead();
+    void exeRecoverSign();
+    void exeRecover();
 
 private:
-    u8 mPad[(0xB8) - sizeof(LiveActor)];
+    /* 0x8C */ f32 mRailCoordSpeed;
+    /* 0x90 */ TVec3f mRailVec;
+    /* 0x9C */ TVec3f mFront;
+    /* 0xA8 */ f32 _A8;
+    /* 0xAC */ bool mIsRail;
+    /* 0xAD */ bool mIsForceDetonated;
+    /* 0xAE */ bool mEyesOpen;
+    /* 0xB0 */ MsgSharedGroup* mSharedGroup;
+    /* 0xB4 */ KirairaChain* mChain;
 };
