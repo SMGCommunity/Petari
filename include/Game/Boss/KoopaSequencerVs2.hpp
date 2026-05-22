afbea02e0b130243de2796a99339a1866eba4bb4
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-05-22 15:20:14 +0200

    Declared virtuals

--- include/Game/Boss/KoopaSequencerVs2.hpp
+++ include/Game/Boss/KoopaSequencerVs2.hpp
@@ -6,6 +6,13 @@ class KoopaSequencerVs2 : public KoopaSequencer {
 public:
     KoopaSequencerVs2();
 
+    virtual ~KoopaSequencerVs2();
+    virtual void init(Koopa*, const JMapInfoIter&);
+    virtual bool attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
 private:
     /* 0x24 */ u32 _24;
     /* 0x28 */ u32 _28;
--- src/Game/Boss/KoopaSequencerVs2.cpp
+++ src/Game/Boss/KoopaSequencerVs2.cpp
@@ -1,1 +1,22 @@
 #include "Game/Boss/KoopaSequencerVs2.hpp"
+
+KoopaSequencerVs2::KoopaSequencerVs2() : KoopaSequencer("クッパ戦（Ｖｓ２進行役）", 2) {
+}
+
+KoopaSequencerVs2::~KoopaSequencerVs2() {
+}
+
+void KoopaSequencerVs2::init(Koopa* pKoopa, const JMapInfoIter& rIter) {
+}
+
+bool KoopaSequencerVs2::attackSensor(HitSensor*, HitSensor*) {
+}
+
+bool KoopaSequencerVs2::receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*) {
+}
+
+bool KoopaSequencerVs2::receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*) {
+}
+
+bool KoopaSequencerVs2::receiveOtherMsg(u32, HitSensor*, HitSensor*) {
+}
