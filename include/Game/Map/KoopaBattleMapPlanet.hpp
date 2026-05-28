f18aaa306b2fdc764179facd84318fd14f1f32eb
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-05-28 14:50:38 +0200

    Update KoopaBattleMapPlanet.hpp

--- include/Game/Map/KoopaBattleMapPlanet.hpp
+++ include/Game/Map/KoopaBattleMapPlanet.hpp
@@ -9,7 +9,7 @@ public:
     void startAnimPowerStarAppear();
 
 private:
-    /* 0x8C */ char* _8C;
+    /* 0x8C */ const char* _8C;
     /* 0x90 */ TMtx34f _90;
     /* 0xC0 */ f32 _C0;
     /* 0xC4 */ f32 _C4;
