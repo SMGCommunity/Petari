4a874c4c6164a10259365b122357f0089f0fb44c
ThePlayerRolo <thesolis78243@gmail.com>
2026-06-01 12:24:55 -0400

    yoink NWC24iConvIdToStr from OGWS

--- src/RVL_SDK/nwc24/NWC24Utils.c
+++ src/RVL_SDK/nwc24/NWC24Utils.c
@@ -19,3 +19,16 @@ void NWC24Date_Init(NWC24Date* date) {
     date->sec = 0;
     date->BYTE_0x7 = 0;
 }
+
+//NOTE: Ported from OGWS
+void NWC24iConvIdToStr(u64 addr, char* pBuffer) {
+    u64 temp = addr;
+    int i;
+
+    for (i = NWC24i_WII_ID_LEN - 1; i >= 0; i--) {
+        pBuffer[i] = '0' + temp % 10;
+        temp /= 10;
+    }
+
+    pBuffer[NWC24i_WII_ID_LEN] = '\0';
+}
