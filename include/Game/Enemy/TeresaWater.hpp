38ee0da9a300f167bf3962523db9bae516a22fdd
BlocksumoGuys <148206420+BlocksumoGuys@users.noreply.github.com>
2026-06-01 13:18:40 +0100

    Update TeresaWater.hpp

--- include/Game/Enemy/TeresaWater.hpp
+++ include/Game/Enemy/TeresaWater.hpp
@@ -1,14 +1,32 @@
 #pragma once
 
-#include "Game/NameObj/NameObj.hpp"
+#include "Game/Enemy/AnimScaleController.hpp"
+#include "Game/Enemy/WalkerStateBindStarPointer.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
-
 
 class TeresaWater : public LiveActor {
 public:
     TeresaWater(const char*);
     virtual ~TeresaWater();
+    virtual void init(const JMapInfoIter&);
+    virtual void kill();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveMsgPlayerAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+
+    void exeMove();
+    void exeHit();
+    void exeStun();
+    bool tryDPDSwoon();
+    void exeDPDSwoon();
+    void endDPDSwoon();
 
 private:
-    u8 mPad[(0xBC) - sizeof(LiveActor)];
+    AnimScaleController* mScaleController;
+    WalkerStateBindStarPointer* mBindStarPointer;
+    TVec3f _90;
+    TVec3f _A0;
+    TVec3f _AC;
+    TVec3f* _B8;
 };
