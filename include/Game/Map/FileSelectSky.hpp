597f80c52fd60a00d26e6d4a3773c7c5bb73bb1a
ThatNintendoNerd <samtheman1026@gmail.com>
2026-06-02 02:16:29 -0700

    Improve matching in `FileSelectSky`

--- include/Game/Map/FileSelectSky.hpp
+++ include/Game/Map/FileSelectSky.hpp
@@ -1,22 +1,29 @@
 #pragma once
 
-#include "Game/LiveActor/HitSensor.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
+#include <JSystem/JGeometry/TMatrix.hpp>
+
+class ProjmapEffectMtxSetter;
 
 class FileSelectSky : public LiveActor {
 public:
-    FileSelectSky(const char*);
+    /// @brief Creates a new `FileSelectSky`.
+    /// @param pName A pointer to the null-terminated name of the object.
+    FileSelectSky(const char* pName);
 
+    /// @brief Destroys the `FileSelectSky`.
     virtual ~FileSelectSky();
-    virtual void init(const JMapInfoIter&);
+
+    virtual void init(const JMapInfoIter& rIter);
     virtual void calcAnim();
     virtual void calcAndSetBaseMtx();
-    virtual bool receiveOtherMsg(u32, HitSensor* pSender, HitSensor* pReceiver);
+    virtual bool receiveOtherMsg(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
 
     void exeWait();
 
-    f32 _8C; 
-    f32 _90;
-    TPos3f _94;
-    ProjmapEffectMtxSetter* mProjmapEffectMtxSetter; // _C4  
+private:
+    /* 0x8C */ f32 mAngleX;
+    /* 0x90 */ f32 mAngleY;
+    /* 0x94 */ TPos3f mBaseMtx;
+    /* 0xC4 */ ProjmapEffectMtxSetter* mProjmapEffectMtxSetter;
 };
