24cb03bc5469a4249801cba0b517135b245bb3c3
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-06-03 01:11:07 +0200

    KoopaDemoFallToPlanetVs3Lv2

---
+++ include/Game/Boss/KoopaDemoFallToPlanetVs3Lv2.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Game/Boss/Koopa.hpp"
+#include "Game/LiveActor/ActorStateBase.hpp"
+
+class KoopaDemoFallToPlanetVs3Lv2 : public ActorStateBase< Koopa > {
+public:
+    KoopaDemoFallToPlanetVs3Lv2(Koopa*);
+
+    virtual ~KoopaDemoFallToPlanetVs3Lv2();
+    virtual void init();
+    virtual void appear();
+    virtual void kill();
+
+    void exeWaitFall();
+    void exeDemoFall();
+    void exeFallToLand();
+    void exeLand();
+    void exeWaitPlayer();
+};
