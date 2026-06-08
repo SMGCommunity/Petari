97fc1ecdca526b3fd94ea28bac421b2cd5e3d053
ThatNintendoNerd <samtheman1026@gmail.com>
2026-06-08 14:59:20 -0700

    Match 23% of `DodoryuUtil`

--- src/Game/Boss/DodoryuUtil.cpp
+++ src/Game/Boss/DodoryuUtil.cpp
@@ -17,4 +17,59 @@ namespace DodoryuUtil {
 
         return true;
     }
+
+    // turnToward
+
+    void accelerate(Dodoryu* pDodoryu, const TVec3f& rVec, f32 param3, f32 param4, f32 param5) {
+        TVec3f auStack_40;
+        TVec3f auStack_34 = TVec3f(rVec);
+        auStack_40.scaleAdd(auStack_34, pDodoryu->mVelocity, -auStack_34.dot(pDodoryu->mVelocity));
+        pDodoryu->mVelocity.sub(auStack_40);
+        auStack_40.scale(param4);
+        pDodoryu->mVelocity.add(auStack_40);
+        TVec3f auStack_58 = TVec3f(auStack_34);
+        auStack_58.scale(param3);
+        pDodoryu->mVelocity.add(auStack_58);
+
+        if (pDodoryu->mVelocity.length() > param5) {
+            pDodoryu->mVelocity.setLength(param5);
+        }
+
+        TVec3f yDir;
+        pDodoryu->mBaseMtx.getYDir(yDir);
+        pDodoryu->mVelocity.scaleAdd(yDir, pDodoryu->mVelocity, -yDir.dot(pDodoryu->mVelocity));
+    }
+
+    void addVelocity(Dodoryu* pDodoryu, bool isSnap) {
+        TVec3f trans;
+        TPos3f mtx = pDodoryu->mBaseMtx;
+
+        mtx.getTrans(trans);
+        trans.add(pDodoryu->mVelocity);
+        mtx.setTrans(trans);
+        pDodoryu->setMtx(mtx);
+
+        if (isSnap) {
+            pDodoryu->snapToGround();
+        }
+    }
+
+    void rotateVelocityByWall(Dodoryu* pDodoryu) {
+        TVec3f auStack_38;
+        TVec3f auStack_2c;
+        pDodoryu->mBaseMtx.getYDir(auStack_2c);
+
+        f32 speed = pDodoryu->mVelocity.length();
+
+        auStack_38.scaleAdd(pDodoryu->_134, auStack_38, -pDodoryu->_134.dot(auStack_38));
+
+        if (MR::isNearZero(auStack_38)) {
+            auStack_38.cross(auStack_2c, pDodoryu->_134);
+        }
+
+        auStack_38.setLength(speed);
+        pDodoryu->mVelocity.set(auStack_38);
+    }
+
+    // calcEscapeDir
 };  // namespace DodoryuUtil
