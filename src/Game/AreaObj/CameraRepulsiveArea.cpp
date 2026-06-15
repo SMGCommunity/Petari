e8561e75bd4a961b0228b62ce38b7c7308a912ad
ThatNintendoNerd <samtheman1026@gmail.com>
2026-06-15 14:35:13 -0700

    Cleanup `CameraRepulsiveArea`

--- src/Game/AreaObj/CameraRepulsiveArea.cpp
+++ src/Game/AreaObj/CameraRepulsiveArea.cpp
@@ -1,6 +1,11 @@
 #include "Game/AreaObj/CameraRepulsiveArea.hpp"
 #include "Game/Util/AreaObjUtil.hpp"
 
+namespace {
+    // static const s32 sPow = _;
+    // static const s32 sCylPower = _;
+};  // namespace
+
 CameraRepulsiveArea::~CameraRepulsiveArea() {
 }
 
@@ -11,11 +16,7 @@ CameraRepulsiveCylinder::~CameraRepulsiveCylinder() {
 }
 
 TVec3f CameraRepulsiveSphere::getRepulsion(const TVec3f& rRep) {
-    TVec3f ret;
-    ret.x = 0.0f;
-    ret.y = 0.0f;
-    ret.z = 0.0f;
-    return ret;
+    return TVec3f(0.0f, 0.0f, 0.0f);
 }
 
 TVec3f CameraRepulsiveCylinder::getRepulsion(const TVec3f& rRep) {
@@ -26,29 +27,24 @@ TVec3f CameraRepulsiveCylinder::getRepulsion(const TVec3f& rRep) {
 
     TVec3f ret(rRep);
     ret -= position;
-    f32 dot = ret.dot(upVec);
+    f32 dot = upVec.dot(ret);
     TVec3f _14;
     TVec3f _8(upVec);
     _8.scale(dot);
     ret -= _8;
 
-    PSVECCrossProduct(&ret, &upVec, &_14);
-    f32 mag = PSVECMag(&_14);
-    f32 radius = MR::getCylinderRadius(this);
+    _14.cross(ret, upVec);
 
-    f32 scale = (2.0f * mag) / radius;
+    f32 base = (_14.length() * 2.0f) / MR::getCylinderRadius(this);
 
-    // denom = scale^3
-    f32 denom = scale;
+    // denom = base^3
+    f32 denom = base;
 
     for (int i = 0; i < 2; i++) {
-        denom *= scale;
+        denom *= base;
     }
 
     ret.scale(1.0f / denom);
-    return ret;
-}
 
-const char* CameraRepulsiveArea::getManagerName() const {
-    return "CameraRepulsiveArea";
+    return ret;
 }
