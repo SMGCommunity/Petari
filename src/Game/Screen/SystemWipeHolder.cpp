57b12169949a3958daf8c44e06a543e807096c88
Gonzalo-kebab <koen.bommol@gmail.com>
2026-06-16 13:25:26 +0200

    fix requested changes

--- src/Game/Screen/SystemWipeHolder.cpp
+++ src/Game/Screen/SystemWipeHolder.cpp
@@ -3,16 +3,16 @@
 #include "Game/Screen/WipeHolderBase.hpp"
 #include "Game/Screen/WipeRing.hpp"
 #include "Game/Util.hpp"
-#include "JSystem/J2DGraph/J2DPicture.hpp"
-#include "JSystem/JUtility/JUTVideo.hpp"
+#include <JSystem/J2DGraph/J2DPicture.hpp>
+#include <JSystem/JUtility/JUTVideo.hpp>
 
 SystemWipeHolder::SystemWipeHolder() : WipeHolderBase(4, "システムワイプ保持"), _1C(false) {
 }
 
 void SystemWipeHolder::init(const JMapInfoIter& rIter) {
-    addWipeLayout(new WipeFade("フェードワイプ", Color8(0x00, 0x00, 0x00, 0xFF)));
+    addWipeLayout(new WipeFade("フェードワイプ", Color8(0, 0, 0, 255)));
     addWipeLayout(new WipeRing(false, "円ワイプ"));
-    addWipeLayout(new WipeFade("白フェードワイプ", Color8(0xFF, 0xFF, 0xFF, 0xFF)));
+    addWipeLayout(new WipeFade("白フェードワイプ", Color8(255, 255, 255, 255)));
 }
 
 bool SystemWipeHolder::isCurrentAlive() const {
@@ -56,20 +56,20 @@ void SystemWipeHolder::startGameScreenCapture() {
 }
 
 void SystemWipeHolder::drawGameScreenCapture() const {
-    if (_1C) {
-        J2DOrthoGraphSimple graph;
-        graph.setPort();
-
-        JUTTexture texture(MR::getScreenResTIMG(), 0);
-
-        J2DPicture picture(&texture);
-        f32 width = MR::getScreenWidth();
-        // inlined MR::getscreenHeight
-        f32 height = (s32)JUTVideo::getManager()->getEfbHeight();
-        ;
-
-        picture.draw(0.0f, 0.0f, width, height, false, false, false);
+    if (!_1C) {
+        return;
     }
+    J2DOrthoGraphSimple graph;
+    graph.setPort();
+
+    JUTTexture texture(MR::getScreenResTIMG(), 0);
+
+    J2DPicture picture(&texture);
+    f32 width = MR::getScreenWidth();
+    // inlined MR::getscreenHeight
+    f32 height = (s32)JUTVideo::getManager()->getEfbHeight();
+
+    picture.draw(0.0f, 0.0f, width, height, false, false, false);
 }
 
 void SystemWipeHolder::updateWipe(const char* pWipeName) {
