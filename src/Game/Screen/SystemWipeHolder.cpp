000202a7d3d27ee61b10042670670437fa16e369
Gonzalo-kebab <koen.bommol@gmail.com>
2026-06-16 12:20:18 +0200

    99% of SystemWipeHolder

---
+++ src/Game/Screen/SystemWipeHolder.cpp
@@ -0,0 +1,95 @@
+#include "Game/Screen/SystemWipeHolder.hpp"
+#include "Game/Camera/CameraContext.hpp"
+#include "Game/Screen/WipeFade.hpp"
+#include "Game/Screen/WipeHolderBase.hpp"
+#include "Game/Screen/WipeRing.hpp"
+#include "Game/Util/Color.hpp"
+#include "Game/Util/DrawUtil.hpp"
+#include "Game/Util/JMapInfo.hpp"
+#include "Game/Util/LayoutUtil.hpp"
+#include "Game/Util/ScreenUtil.hpp"
+#include "JSystem/J2DGraph/J2DPicture.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+#include "JSystem/JUtility/JUTTexture.hpp"
+#include "JSystem/JUtility/JUTVideo.hpp"
+#include "revolution/types.h"
+
+SystemWipeHolder::SystemWipeHolder() : WipeHolderBase(4, "システムワイプ保持"), _1C(false) {
+}
+
+void SystemWipeHolder::init(const JMapInfoIter& rIter) {
+    addWipeLayout(new WipeFade("フェードワイプ", Color8(0x00, 0x00, 0x00, 0xFF)));
+    addWipeLayout(new WipeRing(false, "円ワイプ"));
+    addWipeLayout(new WipeFade("白フェードワイプ", Color8(0xFF, 0xFF, 0xFF, 0xFF)));
+}
+
+bool SystemWipeHolder::isCurrentAlive() const {
+    return getCurrent() != nullptr && !MR::isDead(getCurrent());
+}
+
+void SystemWipeHolder::movement() {
+    if (_1C) {
+        if (!isCurrentAlive() || !getCurrent()->isWipeOut()) {
+            _1C = false;
+            MR::endToCaptureScreen("SystemWipe");
+        }
+    }
+
+    if (isCurrentAlive()) {
+        getCurrent()->movement();
+    }
+}
+
+void SystemWipeHolder::calcAnim() {
+    if (isCurrentAlive()) {
+        getCurrent()->calcAnim();
+    }
+}
+
+void SystemWipeHolder::draw() const {
+    MR::captureScreenIfAllow("SystemWipe");
+    drawGameScreenCapture();
+    if (isCurrentAlive()) {
+        getCurrent()->draw();
+    }
+}
+
+void SystemWipeHolder::setWipeRingCenter(const TVec3f& rCenter) {
+    static_cast< WipeRing* >(findWipe("円ワイプ"))->setCenterPos(rCenter);
+}
+
+void SystemWipeHolder::startGameScreenCapture() {
+    _1C = true;
+    MR::startToCaptureScreen("SystemWipe");
+}
+
+void SystemWipeHolder::drawGameScreenCapture() const {
+    if (_1C) {
+        J2DOrthoGraphSimple graph;
+        graph.setPort();
+
+        JUTTexture texture(MR::getScreenResTIMG(), 0);
+
+        J2DPicture picture(&texture);
+        f32 width = MR::getScreenWidth();
+        // inlined MR::getscreenHeight
+        f32 height = (s32)JUTVideo::getManager()->getEfbHeight();
+        ;
+
+        picture.draw(0.0f, 0.0f, width, height, false, false, false);
+    }
+}
+
+void SystemWipeHolder::updateWipe(const char* pWipeName) {
+    _1C = false;
+    MR::endToCaptureScreen("SystemWipe");
+    WipeHolderBase::updateWipe(pWipeName);
+}
+
+namespace MR {
+    SystemWipeHolder* createSystemWipeHolder() {
+        SystemWipeHolder* newHolder = new SystemWipeHolder();
+        newHolder->initWithoutIter();
+        return newHolder;
+    }
+};  // namespace MR
