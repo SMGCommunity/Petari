32c9d1983d17a2003f8bcbbcbc98eb15e1b1ddd8
shibbo <supershibbo12@gmail.com>
2026-06-20 16:08:51 -0400

    some `WPadAcceleration`

--- include/Game/System/WPadAcceleration.hpp
+++ include/Game/System/WPadAcceleration.hpp
@@ -18,6 +18,19 @@ public:
     void updateAccAverage();
     void updateIsStable();
 
-    // TODO document members
-    u8 _0[0x650];
+    WPad* mPad;  // 0x00
+    u32 _4;
+    u32 _8;
+    u32 _C;
+    TVec3f _10;
+    f32 _1C;
+    bool _20;
+    TVec3f mHistory[128];  // 0x24
+    s32 _624;
+    u32 _628;
+    TVec3f _62C;
+    TVec3f _638;
+    u32 _644;
+    s32 _648;
+    s32 _64C;
 };
