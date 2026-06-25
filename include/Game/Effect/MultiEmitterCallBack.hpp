44f4b8318c0ff545dcf67ff54db604d70abbc4ac
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-06-25 20:13:42 +0200

    Match most of MultiEmitterCallback

---
+++ include/Game/Effect/MultiEmitterCallBack.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "Game/Util/Color.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+#include <JSystem/JParticle/JPAEmitterCallBack.hpp>
+
+#define FORCE_FOLLOW_ON 0x1
+#define FORCE_FOLLOW_OFF 0x2
+#define FORCE_SCALE_ON 0x10
+#define RESET_FOLLOW_CURRENT 0x40
+#define BASE_SCALE 0x100
+
+class MultiEmitter;
+
+class MultiEmitterCallBackBase : public JPAEmitterCallBack {
+public:
+    MultiEmitterCallBackBase() {};
+
+    virtual ~MultiEmitterCallBackBase() {};
+    virtual void init(JPABaseEmitter*) {};
+};
+
+class MultiEmitterCallBack : public MultiEmitterCallBackBase {
+public:
+    struct FlagSRT {
+        bool scale;
+        bool rotation;
+        bool position;
+    };
+
+    MultiEmitterCallBack(const MultiEmitter*, const TVec3f&);
+    MultiEmitterCallBack(const MultiEmitter*, const TVec3f*, const TVec3f*, const TVec3f*, const TVec3f&);
+    MultiEmitterCallBack(const MultiEmitter*, MtxPtr, const TVec3f&);
+    MultiEmitterCallBack(const MultiEmitter*, MtxPtr, const TVec3f*, const TVec3f&);
+
+    void execute(JPABaseEmitter*);
+    void setHostSRT(const TVec3f*, const TVec3f*, const TVec3f*);
+    void setHostMtx(MtxPtr);
+    void setBaseScale(f32);
+    void forceFollowOn();
+    void forceFollowOff();
+    void forceScaleOn();
+    void resetFollowCurrent();
+    void init(JPABaseEmitter*);
+    void setSRTFromHostMtx(JPABaseEmitter*, MtxPtr, const FlagSRT&, bool);
+    void setSRTFromHostSRT(JPABaseEmitter*, const FlagSRT&, bool);
+    void setScaleFromHostScale(JPABaseEmitter*, const TVec3f&, bool, bool);
+    bool effectLight(JPABaseEmitter*) NO_INLINE;
+    void followSRT(JPABaseEmitter*, bool);
+    void setColor(JPABaseEmitter*);
+    void isFollowSRT(FlagSRT*, bool) const;
+
+    inline bool isFlagOn(u16 flag) const {
+        u16 mask = mFlags & flag;
+        return mask == flag;
+    }
+
+    inline bool isFlagOff(u16 flag) const {
+        u16 mask = mFlags & flag;
+        return mask != flag;
+    }
+
+    inline void turnFlagOn(u16 flag) {
+        mFlags |= flag;
+    }
+
+    inline void turnFlagOff(u16 flag) {
+        mFlags &= ~flag;
+    }
+
+    /* 0x04 */ const MultiEmitter* mEmitter;
+    /* 0x08 */ const TVec3f* mScale;
+    /* 0x0C */ const TVec3f* mRotation;
+    /* 0x10 */ const TVec3f* mPosition;
+    /* 0x14 */ MtxPtr mMtx;
+    /* 0x18 */ TVec3f rotation8;
+    /* 0x24 */ f32 mBaseScale;
+    /* 0x28 */ Color8 mPrmColor;
+    /* 0x2C */ Color8 mEnvColor;
+    /* 0x30 */ u16 mFlags;
+};
---
+++ src/Game/Effect/MultiEmitterCallBack.cpp
@@ -0,0 +1,232 @@
+#include "Game/Effect/MultiEmitterCallBack.hpp"
+#include "Game/Effect/EffectSystemUtil.hpp"
+#include "Game/Effect/MultiEmitter.hpp"
+#include "Game/Util/MathUtil.hpp"
+#include "Game/Util/MtxUtil.hpp"
+#include "JSystem/JParticle/JPAEmitter.hpp"
+
+namespace {
+    Color8 getSyntheticColor(const Color8& color1, const Color8& color2) {
+        u8 r = color1.r * color2.r / 0xFF;
+        u8 g = color1.g * color2.g / 0xFF;
+        u8 b = color1.b * color2.b / 0xFF;
+        
+        return Color8(r, g, b, -1);
+    }
+}
+
+MultiEmitterCallBack::MultiEmitterCallBack(const MultiEmitter* pEmitter, const TVec3f& rVec)
+    : MultiEmitterCallBackBase(), mEmitter(pEmitter), mScale(), mRotation(), mPosition(), mMtx(), rotation8(rVec), mBaseScale(1.0f), mPrmColor(-1, -1, -1, -1), mEnvColor(-1, -1, -1, -1), mFlags() {
+}
+
+MultiEmitterCallBack::MultiEmitterCallBack(const MultiEmitter* pEmitter, const TVec3f* pScale, const TVec3f* pRotation, const TVec3f* pPosition,
+                                           const TVec3f& rVec)
+    : MultiEmitterCallBackBase(), mEmitter(pEmitter), mScale(pScale), mRotation(pRotation), mPosition(pPosition), mMtx(), rotation8(rVec), mBaseScale(1.0f), mPrmColor(-1, -1, -1, -1), mEnvColor(-1, -1, -1, -1),
+      mFlags() {
+}
+
+MultiEmitterCallBack::MultiEmitterCallBack(const MultiEmitter* pEmitter, MtxPtr pMtx, const TVec3f& rVec)
+    : MultiEmitterCallBackBase(), mEmitter(pEmitter), mScale(), mRotation(), mPosition(), mMtx(pMtx), rotation8(rVec), mBaseScale(1.0f), mPrmColor(-1, -1, -1, -1), mEnvColor(-1, -1, -1, -1), mFlags() {
+}
+
+MultiEmitterCallBack::MultiEmitterCallBack(const MultiEmitter* pEmitter, MtxPtr pMtx, const TVec3f* pPosition, const TVec3f& rVec)
+    : MultiEmitterCallBackBase(), mEmitter(pEmitter), mScale(), mRotation(), mPosition(pPosition), mMtx(pMtx), rotation8(rVec), mBaseScale(1.0f), mPrmColor(-1, -1, -1, -1), mEnvColor(-1, -1, -1, -1),
+      mFlags() {
+}
+
+void MultiEmitterCallBack::execute(JPABaseEmitter* pEmitter) {
+    followSRT(pEmitter, false);
+    effectLight(pEmitter);
+    setColor(pEmitter);
+}
+
+void MultiEmitterCallBack::setHostSRT(const TVec3f* pScale, const TVec3f* pRotation, const TVec3f* pPosition) {
+    mScale = pScale;
+    mRotation = pRotation;
+    mPosition = pPosition;
+    mMtx = nullptr;
+}
+
+void MultiEmitterCallBack::setHostMtx(MtxPtr pMtx) {
+    mMtx = pMtx;
+    mScale = nullptr;
+    mRotation = nullptr;
+    mPosition = nullptr;
+}
+
+void MultiEmitterCallBack::setBaseScale(f32 scale) {
+    if (isFlagOff(BASE_SCALE)) {
+        turnFlagOn(BASE_SCALE);
+    }
+
+    mBaseScale = scale;
+}
+
+void MultiEmitterCallBack::forceFollowOn() {
+    turnFlagOn(FORCE_FOLLOW_ON);
+}
+
+void MultiEmitterCallBack::forceFollowOff() {
+    turnFlagOn(FORCE_FOLLOW_OFF);
+}
+
+void MultiEmitterCallBack::forceScaleOn() {
+    turnFlagOn(FORCE_SCALE_ON);
+}
+
+void MultiEmitterCallBack::resetFollowCurrent() {
+    turnFlagOff(RESET_FOLLOW_CURRENT);
+}
+
+void MultiEmitterCallBack::init(JPABaseEmitter* pEmitter) {
+    followSRT(pEmitter, true);
+}
+
+void MultiEmitterCallBack::setSRTFromHostMtx(JPABaseEmitter* pEmitter, MtxPtr pMtx, const FlagSRT& rFlag, bool b2) {
+    TPos3f mtx = pMtx;
+    TRot3f mtx94;
+    TVec3f vecD0;
+    TVec3f vecDC;
+    JPASetRMtxSTVecfromMtx(mtx, mtx94, &vecDC, &vecD0);
+
+    if (rFlag.scale) {
+        TVec3f vecE8;
+        mtx94.mult33(rotation8, vecE8);
+
+        if (rFlag.position) {
+            vecE8.mul(vecDC);
+        }
+
+        vecD0.add(vecE8);
+        pEmitter->mGlobalTrs.set(vecD0);
+    }
+
+    if (rFlag.rotation) {
+        if (MR::Effect::isEffect2D(mEmitter)) {
+            TRot3f mtxC4;
+            mtxC4.identity();
+            mtxC4.setEulerZ(PI);
+            mtx94.concat(mtxC4);
+        }
+
+        JPASetRMtxfromMtx(mtx94, pEmitter->mGlobalRot);
+    }
+
+    setScaleFromHostScale(pEmitter, vecDC, rFlag.position, b2);
+}
+
+void MultiEmitterCallBack::setSRTFromHostSRT(JPABaseEmitter* pEmitter, const FlagSRT& rFlag, bool b2) {
+    if (rFlag.scale) {
+        TVec3f vecE0;
+        if (rFlag.rotation) {
+            TRot3f mtxD4;
+            mtxD4.identity();
+
+            TVec3f vecEC(*mRotation);
+            vecEC.scale(PI_180);
+            mtxD4.makeMatrixFromRotAxesInline(vecEC.x, vecEC.y, vecEC.z);
+            mtxD4.mult33(rotation8, vecE0);
+        } else {
+            vecE0.set(rotation8);
+        }
+
+        if (rFlag.position && mPosition != nullptr) {
+            vecE0.mul(*mPosition);
+        }
+
+        vecE0.add(*mScale);
+
+        pEmitter->mGlobalTrs.set(vecE0);
+    }
+
+    if (rFlag.rotation) {
+        MR::makeMtxRotate(pEmitter->mGlobalRot, mRotation->x, mRotation->y, mRotation->z);
+    }
+
+    setScaleFromHostScale(pEmitter, TVec3f(1.0f, 1.0f, 1.0f), rFlag.position, b2);
+}
+
+void MultiEmitterCallBack::setScaleFromHostScale(JPABaseEmitter* pEmitter, const TVec3f& rVec, bool b1, bool b2) {
+    TVec3f vec28(rVec);
+
+    if (b1) {
+        if (mPosition != nullptr) {
+            vec28.set(*mPosition);
+        }
+
+        if (isFlagOn(BASE_SCALE)) {
+            vec28.scale(mBaseScale);
+        }
+        pEmitter->setGlobalScale(vec28);
+    } else if (b2 && isFlagOn(BASE_SCALE)) {
+        vec28.setAll< f32 >(mBaseScale);
+        pEmitter->setGlobalScale(vec28);
+    }
+}
+
+bool MultiEmitterCallBack::effectLight(JPABaseEmitter*) {
+    return MR::isNearZero(mEmitter->_2C);
+}
+
+void MultiEmitterCallBack::followSRT(JPABaseEmitter* pEmitter, bool b2) {
+    FlagSRT flag;
+    isFollowSRT(&flag, b2);
+
+    bool b = (flag.scale || flag.rotation || flag.position) ? true : false;
+    if (b || (isFlagOn(BASE_SCALE))) {
+        if (mMtx != nullptr) {
+            setSRTFromHostMtx(pEmitter, mMtx, flag, b2);
+        } else {
+            setSRTFromHostSRT(pEmitter, flag, b2);
+        }
+    }
+}
+
+void MultiEmitterCallBack::setColor(JPABaseEmitter* pEmitter) {
+    // FIXME: something goes wrong with GXColor/Color8
+    if (MR::isNearZero(mEmitter->_2C)) {
+        pEmitter->setGlobalPrmColor(mPrmColor.r, mPrmColor.g, mPrmColor.b);
+        pEmitter->setGlobalEnvColor(mEnvColor.r, mEnvColor.g, mEnvColor.b);
+
+        return;
+    } 
+
+    GXColor colorC = pEmitter->mGlobalPrmClr;
+    GXColor color10 = pEmitter->mGlobalEnvClr;
+
+    GXColor col = getSyntheticColor(colorC, color10);
+
+    pEmitter->setGlobalPrmColor(col.r, col.g, col.g);
+
+    col = getSyntheticColor(colorC, mEnvColor);
+    pEmitter->setGlobalEnvColor(col.r, col.g, col.b);
+
+}
+
+void MultiEmitterCallBack::isFollowSRT(FlagSRT* rFlag, bool b2) const {
+    // FIXME: missing class at _28 in MultiEmitter
+
+    if (mMtx == nullptr && mScale == nullptr) {
+        rFlag->scale = false;
+    } else if (rFlag->scale == false) {
+        rFlag->scale = !((mFlags & 0x1) - 0x1);
+    }
+
+    if (mMtx == nullptr && mRotation == nullptr) {
+        rFlag->rotation = false;
+    } else if (rFlag->rotation == false) {
+        rFlag->rotation = !((mFlags & 0x4) - 0x4);
+    }
+
+    if (mMtx == nullptr && mPosition == nullptr) {
+        rFlag->position = false;
+
+        return;
+    }
+
+    if (rFlag->position) {
+        return;
+    }
+
+    rFlag->position = !((mFlags & 0x10) - 0x10);
+}
