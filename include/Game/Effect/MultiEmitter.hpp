83f3fa683506c09d063fad103121b15ddd456948
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-06-26 18:04:07 +0200

    Match 73% `MultiEmitter`
    Missing some bck stuff mostly and some tough `for_each` and `for_each_array` to figure out.

--- include/Game/Effect/MultiEmitter.hpp
+++ include/Game/Effect/MultiEmitter.hpp
@@ -1,60 +1,114 @@
 #pragma once
 
+#include "Game/Util/Array.hpp"
 #include "JSystem/JGeometry/TVec.hpp"
-#include <revolution.h>
 
-class EffectSystem;
 class MultiEmitterCallBackBase;
-class JPABaseEmitter;
-class JPAParticleCallBack;
+class EffectSystem;
 class ParticleEmitter;
+class XanimePlayer;
+class SingleEmitter;
+class JPAParticleCallBack;
+class MultiEmitterCallBack;
+class MultiEmitterParticleCallBack;
 
 class MultiEmitter {
 public:
-    MultiEmitter(const char*);
-    MultiEmitter(const char*, MtxPtr, const TVec3f*, const TVec3f&);
-    MultiEmitter(const char*, MtxPtr, const TVec3f&);
     MultiEmitter(const char*, const TVec3f*, const TVec3f*, const TVec3f*, const TVec3f&);
+    MultiEmitter(const char*, MtxPtr, const TVec3f&);
+    MultiEmitter(const char*, MtxPtr, const TVec3f*, const TVec3f&);
+    MultiEmitter(const char*);
 
     void createEmitter();
+    void createEmitterWithCallBack(MultiEmitterCallBackBase*);
 
-    void forceDelete(EffectSystem*);
-    void forceDeleteEmitter();
     void deleteEmitter();
-    void create(EffectSystem*);
+    void forceDeleteEmitter();
+    void forceDelete(EffectSystem*);
+    void deleteForeverEmitter();
+    void playCalcAndDeleteForeverEmitter();
 
-    void setHostMtx(MtxPtr);
+    bool isValid() const;
+    bool isExistOneTimeEmitter() const;
+
     void setHostSRT(const TVec3f*, const TVec3f*, const TVec3f*);
-    void setGlobalSRTMatrix(const MtxPtr, s32);
-
+    void setHostMtx(MtxPtr);
+    void setOffset(const TVec3f&);
     void setBaseScale(f32);
     void setName(const char*);
     bool isEqualName(u16) const;
-    void setLocalScale(const TVec3f&, s32);
-    void setGlobalScale(const TVec3f&, s32);
-    void setGlobalScale(f32, s32);
-    void setGlobalRotationDegree(const TVec3f&, s32);
-    void setRate(f32, s32);
-    void setDrawOrder(s32);
 
-    void setGlobalPrmColor(u8, u8, u8, s32);
-    void setGlobalEnvColor(u8, u8, u8, s32);
+    ParticleEmitter* getParticleEmitter(int) const;
+    void create(EffectSystem*);
+    void scanParticleEmitter(EffectSystem*);
 
-    void setGlobalTranslation(const TVec3f&, s32);
-
-    void createEmitterWithCallBack(MultiEmitterCallBackBase*);
-
-    void setParticleCallBackPtr(JPAParticleCallBack*, s32);
-    ParticleEmitter* getParticleEmitter(s32) const;
     void forceFollowOn();
     void forceFollowOff();
     void forceScaleOn();
 
-    bool isValid() const;
-
+    void initSyncBck(XanimePlayer*, const char*, s32, f32);
+    void onDeleteSyncBck(bool, f32);
+    void addSyncBck(const XanimePlayer*, const char*);
+    void setContinueBckEnd(bool);
     void onCreateSyncClipping();
     void onForceDeleteSyncClipping();
+    void stopEmitterOnClipped();
+    void playEmitterOffClipped();
 
-    /* 0x0 */ u32 _0;
-    /* 0x4 */ u8 _4[0x38-0x4];
+    void setDrawOrder(s32);
+
+    void addChildEmitter(MultiEmitter*);
+
+    void setGlobalRotationDegree(const TVec3f&, s32);
+
+    void allocateEmitter(const char*);
+    SingleEmitter* getValidEmitter(s32, bool);
+    void createOneTimeEmitter();
+    void createForeverEmitter();
+
+    // In MultiEmitterAccess
+    void setGlobalRotation(const TVec3s&, s32);
+    void setGlobalTranslation(const TVec3f&, s32);
+    void setGlobalScale(const TVec3f&, s32);
+    void setGlobalSRTMatrix(const MtxPtr, s32);
+    void setGlobalPrmColor(u8, u8, u8, s32);
+    void setGlobalEnvColor(u8, u8, u8, s32);
+    void setLocalScale(const TVec3f&, s32);
+    void setDirectionalSpeed(f32, s32);
+    void setRate(f32, s32);
+    void stopCalcEmitter(s32);
+    void playCalcEmitter(s32);
+    void stopDrawParticle(s32);
+    void playDrawParticle(s32);
+    void setParticleCallBackPtr(JPAParticleCallBack*, s32);
+    void pauseOff(s32);
+
+    inline bool isFlagOn(u16 flag) const {
+        u16 mask = mFlags & flag;
+        return mask == flag;
+    }
+
+    inline bool isFlagOff(u16 flag) const {
+        u16 mask = mFlags & flag;
+        return mask != flag;
+    }
+
+    inline void turnFlagOn(u16 flag) {
+        mFlags |= flag;
+    }
+
+    inline void turnFlagOff(u16 flag) {
+        mFlags &= ~flag;
+    }
+
+    /* 0x00 */ MR::AssignableArray< SingleEmitter > mEmitters;
+    /* 0x08 */ MR::Vector< MR::FixedArray< MultiEmitter*, 4 > > mChildren;
+    /* 0x1C */ MultiEmitterCallBack* mCallBack;
+    /* 0x20 */ MultiEmitterParticleCallBack* mParticleCallBack;
+    /* 0x24 */ void* _24;
+    /* 0x28 */ void* _28;
+    /* 0x2C */ f32 _2C;
+    /* 0x30 */ f32 _30;
+    /* 0x34 */ u16 mHash;
+    /* 0x36 */ u8 mFlags;
 };
