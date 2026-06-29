581779113cfd43adabb8ef0c4e7d7c9e41ad77ec
aba4gamer <ambernalalvarez@outlook.com>
2026-06-29 23:03:27 +0200

    I guess it's done now

--- include/Game/Enemy/Unizo.hpp
+++ include/Game/Enemy/Unizo.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "Game/LiveActor/LiveActor.hpp"
-#include "JSystem/JGeometry/TMatrix.hpp"
+#include "Game/Util/JMapInfo.hpp"
+#include <JSystem/JGeometry/TMatrix.hpp>
 
 class AnimScaleController;
 class WalkerStateBindStarPointer;
@@ -9,13 +10,14 @@ class WalkerStateBindStarPointer;
 class Unizo : public LiveActor {
 public:
     Unizo(const char*);
+    virtual void init(const JMapInfoIter&);
     virtual void initAfterPlacement();
     virtual void control();
     virtual void appear();
     virtual void kill();
     virtual void attackSensor(HitSensor*, HitSensor*);
-    virtual MtxPtr getBaseMtx() const;
     virtual ~Unizo();
+    virtual MtxPtr getBaseMtx() const;
 
     void initType(const JMapInfoIter&);
     void updateRotate();
@@ -43,17 +45,17 @@ public:
     void endPointing();
 
 private:
-    s32 _8C; // Okay I have no idea
-    s32 _90;
-    u32 mPad[3];
-    TPos3f _A0;
-    TMtx34f _D0;
-    u32 _100;
-    s32 _104; // Not sure
-    u32 mPad2[3];
-    LiveActor* _114; // It's a LiveActor, idk which one
-    AnimScaleController* _118;
-    WalkerStateBindStarPointer* _11C;
-    s32 _120;
-    u32 mPad3[4];   // Total: 0x134
+    /* 0x08C */ s32 _8C; // Okay I have no idea
+    /* 0x090 */ s32 _90;
+    /* 0x094 */ u32 mPad[3];
+    /* 0x0A0 */ TPos3f _A0;
+    /* 0x0D0 */ TMtx34f _D0;
+    /* 0x100 */ u32 _100;
+    /* 0x104 */ s32 _104; // Not sure
+    /* 0x108 */ u32 mPad2[3];
+    /* 0x114 */ LiveActor* _114; // It's a LiveActor, idk which one
+    /* 0x118 */ AnimScaleController* _118;
+    /* 0x11C */ WalkerStateBindStarPointer* _11C;
+    /* 0x120 */ s32 _120;
+    /* 0x124 */ u32 mPad3[4];   // Total: 0x134
 };
