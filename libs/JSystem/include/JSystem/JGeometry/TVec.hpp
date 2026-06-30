614353a609c7ce718fe87f9f713253550e270ca9
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-06-30 04:14:29 +0200

    Update TVec.hpp

--- libs/JSystem/include/JSystem/JGeometry/TVec.hpp
+++ libs/JSystem/include/JSystem/JGeometry/TVec.hpp
@@ -623,6 +623,12 @@ namespace JGeometry {
             JGeometry::setTVec3f(&rSrc.x, &x);
         }
 
+        static inline TVec3 makeZeroVec() {
+            TVec3 v;
+            v.set(0.0f, 0.0f, 0.0f);
+            return v;
+        }
+
 #ifdef __MWERKS__
         // Point gravity doesn't match if we use setPS
         inline void setPS2(const TVec3< f32 >& rSrc) {
