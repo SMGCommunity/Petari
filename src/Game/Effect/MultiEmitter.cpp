f2c2a52431c929128d3d3ca13c251faeb1d0ac95
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-07-01 20:48:02 +0200

    Multi emitter and co (#1943)
    * Match `BvaPlayer`
    
    * Update FollowCollisionArea.hpp
    
    * Update splits.txt
    
    * Match `GalaxyMapIcon`
    
    * Matched most of `PaneEffectKeeper`
    
    In `clear()` there is a `__ptmf_scall` call. No clue how to deal with this, so just kind of left it as is.
    
    * Swapped registers
    
    * Replaced magic number
    
    * Update JPAMath.hpp
    
    * Match most of MultiEmitterCallback
    
    * Fixed member name
    
    * Match most of `MultiEmitterAccess`
    
    * Match 73% `MultiEmitter`
    
    Missing some bck stuff mostly and some tough `for_each` and `for_each_array` to figure out.
    
    * Match `PaneEffectKeeper`
    
    * Created enum
    
    * oopsie
    
    * Set items back to matching
    
    * Renamed members
    
    * Proper inclusion of external libraries
    
    * Renamed member
    
    * Update MultiEmitter.cpp
    
    * Renamed constructor arguments
    
    * Update MultiEmitter.cpp
    
    * Update PaneEffectKeeper.cpp
    
    * Renamed arguments
    
    * Fixed some changes
    
    * Fixed typo
    
    * Use decimal instead of hexadecimal
    
    * Update MultiEmitterCallBack.cpp

---
+++ src/Game/Effect/MultiEmitter.cpp
@@ -0,0 +1,261 @@
+#include "Game/Effect/MultiEmitter.hpp"
+#include "Game/Effect/EffectSystem.hpp"
+#include "Game/Effect/MultiEmitterCallBack.hpp"
+#include "Game/Effect/MultiEmitterParticleCallBack.hpp"
+#include "Game/Effect/SingleEmitter.hpp"
+#include "Game/Util/HashUtil.hpp"
+#include <JSystem/JParticle/JPAEmitter.hpp>
+#include <algorithm>
+
+MultiEmitter::MultiEmitter(const char* pName, const TVec3f* pScale, const TVec3f* pRotation, const TVec3f* pTranslation, const TVec3f& rVec)
+    : mEmitters(), mCallBack(), mParticleCallBack(), _24(), _28(), _2C(), _30(), mHash(), mFlags() {
+    mCallBack = new MultiEmitterCallBack(this, pScale, pRotation, pTranslation, rVec);
+    mParticleCallBack = new MultiEmitterParticleCallBack();
+
+    allocateEmitter(pName);
+}
+
+MultiEmitter::MultiEmitter(const char* pName, MtxPtr pMtx, const TVec3f& rVec)
+    : mEmitters(), mCallBack(), mParticleCallBack(), _24(), _28(), _2C(), _30(), mHash(), mFlags() {
+    mCallBack = new MultiEmitterCallBack(this, pMtx, rVec);
+    mParticleCallBack = new MultiEmitterParticleCallBack();
+
+    allocateEmitter(pName);
+}
+
+MultiEmitter::MultiEmitter(const char* pName, MtxPtr pMtx, const TVec3f* pTranslation, const TVec3f& rVec)
+    : mEmitters(), mCallBack(), mParticleCallBack(), _24(), _28(), _2C(), _30(), mHash(), mFlags() {
+    mCallBack = new MultiEmitterCallBack(this, pMtx, pTranslation, rVec);
+    mParticleCallBack = new MultiEmitterParticleCallBack();
+
+    allocateEmitter(pName);
+}
+
+MultiEmitter::MultiEmitter(const char* pName) : mEmitters(), mCallBack(), mParticleCallBack(), _24(), _28(), _2C(), _30(), mHash(), mFlags() {
+    mCallBack = new MultiEmitterCallBack(this, TVec3f(0.0f, 0.0f, 0.0f));
+    mParticleCallBack = new MultiEmitterParticleCallBack();
+
+    allocateEmitter(pName);
+}
+
+void MultiEmitter::createEmitter() {
+    create(MR::getEffectSystem());
+}
+
+void MultiEmitter::createEmitterWithCallBack(MultiEmitterCallBackBase* pCallBackBase) {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        MR::getEffectSystem()->createSingleEmitter(pEmitter, pCallBackBase, nullptr);
+    }
+}
+
+void MultiEmitter::deleteEmitter() {
+    for_each_array(mEmitters.begin(), mEmitters.end(), std::mem_func(&SingleEmitter::deleteEmitter));
+    for_each(mChildren.begin(), mChildren.end(), std::mem_func(&MultiEmitter::deleteEmitter));
+}
+
+void MultiEmitter::forceDeleteEmitter() {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        MR::getEffectSystem()->forceDeleteSingleEmitter(pEmitter);
+    }
+
+    for_each(mChildren.begin(), mChildren.end(), std::mem_func(&MultiEmitter::forceDeleteEmitter));
+}
+
+void MultiEmitter::forceDelete(EffectSystem* pSystem) {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        pSystem->forceDeleteSingleEmitter(pEmitter);
+    }
+}
+
+void MultiEmitter::deleteForeverEmitter() {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isValid() && !pEmitter->isOneTime()) {
+            pEmitter->deleteEmitter();
+        }
+    }
+
+    for_each(mChildren.begin(), mChildren.end(), std::mem_func(&MultiEmitter::deleteForeverEmitter));
+}
+
+void MultiEmitter::playCalcAndDeleteForeverEmitter() {
+    playCalcEmitter(-1);
+    deleteForeverEmitter();
+}
+
+bool MultiEmitter::isValid() const {
+    for (const SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isValid()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool MultiEmitter::isExistOneTimeEmitter() const {
+    for (const SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isOneTime()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void MultiEmitter::setHostSRT(const TVec3f* pScale, const TVec3f* pRotation, const TVec3f* pTranslation) {
+    mCallBack->setHostSRT(pScale, pRotation, pTranslation);
+}
+
+void MultiEmitter::setHostMtx(MtxPtr pMtx) {
+    mCallBack->setHostMtx(pMtx);
+}
+
+void MultiEmitter::setOffset(const TVec3f& rOffset) {
+    mCallBack->_18.set(rOffset);
+}
+
+void MultiEmitter::setBaseScale(f32 scale) {
+    mCallBack->setBaseScale(scale);
+}
+
+void MultiEmitter::setName(const char* pName) {
+    mHash = MR::getHashCode(pName);
+}
+
+bool MultiEmitter::isEqualName(u16 hash) const {
+    return mHash == hash;
+}
+
+ParticleEmitter* MultiEmitter::getParticleEmitter(int idx) const {
+    const SingleEmitter* pEmitter = &mEmitters[idx];
+    if (pEmitter->isValid()) {
+        return pEmitter->mEmitter;
+    }
+
+    return nullptr;
+}
+
+void MultiEmitter::create(EffectSystem* pSystem) {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        pSystem->createSingleEmitter(pEmitter, mCallBack, mParticleCallBack);
+    }
+
+    mCallBack->resetFollowCurrent();
+
+    for (MultiEmitter** pEmitter = mChildren.begin(); pEmitter != mChildren.end(); pEmitter++) {
+        (*pEmitter)->create(pSystem);
+    }
+}
+
+/* void MultiEmitter::scanParticleEmitter(EffectSystem* pSystem) {
+} */
+
+void MultiEmitter::forceFollowOn() {
+    mCallBack->forceFollowOn();
+}
+
+void MultiEmitter::forceFollowOff() {
+    mCallBack->forceFollowOff();
+}
+
+void MultiEmitter::forceScaleOn() {
+    mCallBack->forceScaleOn();
+}
+
+/*
+void MultiEmitter::initSyncBck(XanimePlayer* pPlayer, const char* pName, s32 i1, f32 f2) {
+}
+
+void MultiEmitter::onDeleteSyncBck(bool, f32) {
+}
+
+void MultiEmitter::addSyncBck(const XanimePlayer*, const char*) {
+}
+
+void MultiEmitter::setContinueBckEnd(bool) {
+}
+ */
+void MultiEmitter::onCreateSyncClipping() {
+    turnFlagOn(JPAEmtrStts_FirstEmit);
+}
+
+void MultiEmitter::onForceDeleteSyncClipping() {
+    turnFlagOn(JPAEmtrStts_Immortal);
+}
+
+void MultiEmitter::stopEmitterOnClipped() {
+    if (isFlagOn(JPAEmtrStts_Immortal)) {
+        forceDeleteEmitter();
+    } else if (isFlagOn(JPAEmtrStts_RateStepEmit)) {
+        deleteEmitter();
+    } else if (_24 != nullptr) {
+        stopCalcEmitter(-1);
+        stopDrawParticle(-1);
+    }
+
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isValid() && !pEmitter->isOneTime()) {
+            pEmitter->mEmitter->mEmitter->setStatus(JPAEmtrStts_StopCalc);
+            pEmitter->mEmitter->mEmitter->setStatus(JPAEmtrStts_StopDraw);
+        }
+    }
+}
+
+void MultiEmitter::playEmitterOffClipped() {
+    if (isFlagOn(JPAEmtrStts_FirstEmit)) {
+        create(MR::getEffectSystem());
+    } else if (_24 != nullptr) {
+        playCalcEmitter(-1);
+        playDrawParticle(-1);
+    }
+
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isValid() && !pEmitter->isOneTime()) {
+            pEmitter->mEmitter->mEmitter->clearStatus(JPAEmtrStts_StopCalc);
+            pEmitter->mEmitter->mEmitter->clearStatus(JPAEmtrStts_StopDraw);
+        }
+    }
+}
+/*
+void MultiEmitter::setDrawOrder(s32 idx) {
+}
+ */
+void MultiEmitter::addChildEmitter(MultiEmitter* pChild) {
+    mChildren.push_back(pChild);
+}
+
+void MultiEmitter::setGlobalRotationDegree(const TVec3f& rRotation, s32 idx) {
+    setGlobalRotation(TVec3s(rRotation.x * DEGREE_TO_S16, rRotation.y * DEGREE_TO_S16, rRotation.z * DEGREE_TO_S16), idx);
+}
+
+/* void MultiEmitter::allocateEmitter(const char* pName) {
+} */
+
+SingleEmitter* MultiEmitter::getValidEmitter(s32 idx, bool) {
+    SingleEmitter* pEmitter = &mEmitters[idx];
+    if (pEmitter->isValid()) {
+        return pEmitter;
+    }
+
+    return nullptr;
+}
+
+void MultiEmitter::createOneTimeEmitter() {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (pEmitter->isOneTime()) {
+            MR::getEffectSystem()->createSingleEmitter(pEmitter, mCallBack, nullptr);
+        }
+    }
+}
+
+void MultiEmitter::createForeverEmitter() {
+    for (SingleEmitter* pEmitter = mEmitters.begin(); pEmitter != mEmitters.end(); pEmitter++) {
+        if (!pEmitter->isOneTime()) {
+            MR::getEffectSystem()->createSingleEmitter(pEmitter, mCallBack, nullptr);
+        }
+    }
+}
+
+void SingleEmitter::setGroupID(u8 groupId) {
+    mGroupId = groupId;
+}
