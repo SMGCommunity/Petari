38d70a6f2ebdd5c4b02400516d2ae0ced217c2a3
shibbo <supershibbo12@gmail.com>
2026-07-11 19:28:41 -0400

    `JKRAramBlock` and `JKRAramHeap`

---
+++ libs/JSystem/include/JSystem/JKernel/JKRAramBlock.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "JSystem/JSupport/JSUList.hpp"
+
+class JKRAramHeap;
+
+class JKRAramBlock {
+public:
+    JKRAramBlock(u32, u32, u32, u8, bool);
+    virtual ~JKRAramBlock();
+
+    JKRAramBlock* allocHead(u32, u8, JKRAramHeap*);
+    JKRAramBlock* allocTail(u32, u8, JKRAramHeap*);
+
+    u32 getAddress() const {
+        return mAddress;
+    }
+
+    u32 getSize() const {
+        return mSize;
+    }
+
+    u32 getFreeSize() const {
+        return mFreeSize;
+    }
+
+    bool isTempMemory() const {
+        return mIsTempMemory;
+    }
+
+    void newGroupID(u8 groupId) {
+        mGroupId = groupId;
+    }
+
+public:
+    /* 0x00 */  // vtable
+    /* 0x04 */ JSULink< JKRAramBlock > mBlockLink;
+    /* 0x14 */ u32 mAddress;
+    /* 0x18 */ u32 mSize;
+    /* 0x1C */ u32 mFreeSize;
+    /* 0x20 */ u8 mGroupId;
+    /* 0x21 */ bool mIsTempMemory;
+    /* 0x22 */ u8 padding[2];
+};
