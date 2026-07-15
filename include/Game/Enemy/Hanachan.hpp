705c59719c57f475c083ebc3ea5c9336b091ad6f
shibbo <supershibbo12@gmail.com>
2026-07-14 20:13:45 -0400

    map out `Hanachan`

--- include/Game/Enemy/Hanachan.hpp
+++ include/Game/Enemy/Hanachan.hpp
@@ -2,11 +2,93 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
+class Hanachan;
+class AnimScaleController;
+
+class HanachanParts : public LiveActor {
+public:
+    HanachanParts(Hanachan*, const char*, const char*);
+
+    virtual ~HanachanParts();
+    virtual void init(const JMapInfoIter&);
+    virtual void kill();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPush(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+
+    TVec3f* getCommonGravity() const;
+    void exeWalk();
+    void exeTrample();
+    void exeBecomeAngry();
+    void exeAngryPursue();
+    void exeAngryEnd();
+    void exeWallHitEnd();
+    void exeOverturn();
+    void exeOverturnBound();
+    void exeOverturnWait();
+    void exeOverturnHipDropped();
+    void exeRecover();
+    void exeHipDropped();
+    void exeBlow();
+    void exeStarPointerBind();
+    bool isHeadHitWall();
+    void changeHeadAngry();
+    void changeHeadCalmDown();
+
+    Hanachan* mParent;  // 0x8C
+    TVec3f _90;
+    TVec3f _9C;
+    s32 _A8;
+    TVec3f _AC;
+    f32 _B8;
+    u32 _BC;
+    s32 _C0;
+    u8 _C4;
+};
+
 class Hanachan : public LiveActor {
 public:
     Hanachan(const char*);
-    virtual ~Hanachan();
 
-private:
-    u8 mPad[(0xC0) - sizeof(LiveActor)];
+    virtual ~Hanachan();
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void kill();
+    virtual void startClipped();
+    virtual void endClipped();
+    virtual void control();
+
+    void exeWalk();
+    void exeTrample();
+    void exeBecomeAngry();
+    void exeAngryPursue();
+    void exeAngryEnd();
+    void exeWallHitEnd();
+    void exeOverturn();
+    void exeOverturnWait();
+    void exeOverturnBound();
+    void exeRecover();
+    void exeHipDropped();
+    void exeBlow();
+    void exeStarPointerBind();
+    void exeStarPointerBindEnd();
+    void exeStarPointerBindOverturn();
+    s32 calcNearestInfectionId();
+    bool isOwnSensor(HitSensor*);
+    void setNerveBlow(const TVec3f&);
+    void applyPlayerHipDropReaction();
+    void setNerveAllParts(const Nerve*);
+    void setDelayAllPartsAtId(s32, s32, s32);
+    bool isStarPointerPointing();
+    void moveHeadAlongRail(f32);
+    void moveHeadToPlayer(f32, f32);
+    void moveBodyAlongHead();
+
+    HanachanParts* mBodyParts[5];  // 0x8C
+    TVec3f _A0;
+    TVec3f _AC;
+    u8 _B8;
+    AnimScaleController* mScaleCtrl;  // 0xBC
 };
--- src/Game/Enemy/Hanachan.cpp
+++ src/Game/Enemy/Hanachan.cpp
@@ -1,4 +1,40 @@
 #include "Game/Enemy/Hanachan.hpp"
+#include "Game/LiveActor/Nerve.hpp"
+
+namespace NrvHanachan {
+    NEW_NERVE(HanachanPartsNrvWalk, HanachanParts, Walk);
+    NEW_NERVE(HanachanPartsNrvBecomeAngry, HanachanParts, BecomeAngry);
+    NEW_NERVE(HanachanPartsNrvAngryPursue, HanachanParts, AngryPursue);
+    NEW_NERVE(HanachanPartsNrvAngryEnd, HanachanParts, AngryEnd);
+    NEW_NERVE(HanachanPartsNrvWallHitEnd, HanachanParts, WallHitEnd);
+    NEW_NERVE(HanachanPartsNrvTrample, HanachanParts, Trample);
+    NEW_NERVE(HanachanPartsNrvOverturn, HanachanParts, Overturn);
+    NEW_NERVE(HanachanPartsNrvOverturnBound, HanachanParts, OverturnBound);
+    NEW_NERVE(HanachanPartsNrvOverturnWait, HanachanParts, OverturnWait);
+    NEW_NERVE(HanachanPartsNrvRecover, HanachanParts, Recover);
+    NEW_NERVE(HanachanPartsNrvHipDropped, HanachanParts, HipDropped);
+    NEW_NERVE(HanachanPartsNrvOverturnHipDropped, HanachanParts, HipDropped);
+    NEW_NERVE(HanachanPartsNrvBlow, HanachanParts, Blow);
+    NEW_NERVE(HanachanPartsNrvStarPointerBind, HanachanParts, StarPointerBind);
+
+    NEW_NERVE(HanachanNrvHanachanWalk, Hanachan, Walk);
+    NEW_NERVE(HanachanNrvHanachanBecomeAngry, Hanachan, BecomeAngry);
+    NEW_NERVE(HanachanNrvHanachanAngryPursue, Hanachan, AngryPursue);
+    NEW_NERVE(HanachanNrvHanachanAngryEnd, Hanachan, AngryEnd);
+    NEW_NERVE(HanachanNrvHanachanWallHitEnd, Hanachan, WallHitEnd);
+    NEW_NERVE(HanachanNrvHanachanTrample, Hanachan, Trample);
+    NEW_NERVE(HanachanNrvHanachanOverturn, Hanachan, Overturn);
+    NEW_NERVE(HanachanNrvHanachanOverturnBound, Hanachan, OverturnBound);
+    NEW_NERVE(HanachanNrvHanachanOverturnWait, Hanachan, OverturnWait);
+    NEW_NERVE(HanachanNrvHanachanRecover, Hanachan, Recover);
+    NEW_NERVE(HanachanNrvHanachanHipDropped, Hanachan, HipDropped);
+    NEW_NERVE(HanachanNrvHanachanOverturnHipDropped, Hanachan, HipDropped);
+    NEW_NERVE(HanachanNrvHanachanBlow, Hanachan, Blow);
+    NEW_NERVE(HanachanNrvHanachanStarPointerBind, Hanachan, StarPointerBind);
+    NEW_NERVE(HanachanNrvHanachanStarPointerBindEnd, Hanachan, WallHitEnd);
+    NEW_NERVE(HanachanNrvHanachanStarPointerBindOverturn, Hanachan, StarPointerBind);
+    NEW_NERVE(HanachanNrvHanachanStarPointerBindEndOverturn, Hanachan, WallHitEnd);
+};  // namespace NrvHanachan
 
 Hanachan::Hanachan(const char* pName) : LiveActor(pName) {
 }
