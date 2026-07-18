5394026b072f4d2b18a3138ce01b3bfc9f346829
ThatNintendoNerd <samtheman1026@gmail.com>
2026-07-17 17:07:45 -0700

    Reorganize VI

---
+++ libs/RVL_SDK/include/revolution/vi/vi3in1.h
@@ -0,0 +1,49 @@
+#ifndef VI3IN1_H
+#define VI3IN1_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <revolution/vi/vi3in1types.h>
+
+typedef enum _VIGamma {
+    VI_GM_0_1 = 1,
+    VI_GM_0_2,
+    VI_GM_0_3,
+    VI_GM_0_4,
+    VI_GM_0_5,
+    VI_GM_0_6,
+    VI_GM_0_7,
+    VI_GM_0_8,
+    VI_GM_0_9,
+    VI_GM_1_0,
+    VI_GM_1_1,
+    VI_GM_1_2,
+    VI_GM_1_3,
+    VI_GM_1_4,
+    VI_GM_1_5,
+    VI_GM_1_6,
+    VI_GM_1_7,
+    VI_GM_1_8,
+    VI_GM_1_9,
+    VI_GM_2_0,
+    VI_GM_2_1,
+    VI_GM_2_2,
+    VI_GM_2_3,
+    VI_GM_2_4,
+    VI_GM_2_5,
+    VI_GM_2_6,
+    VI_GM_2_7,
+    VI_GM_2_8,
+    VI_GM_2_9,
+    VI_GM_3_0
+} VIGamma;
+
+void VISetTrapFilter(VIBool);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VI3IN1_H
--- libs/RVL_SDK/include/revolution/vi/vifuncs.h
+++ libs/RVL_SDK/include/revolution/vi/vifuncs.h
@@ -3,19 +3,33 @@
 
 #include <revolution/types.h>
 #include <revolution/vi/vitypes.h>
+#include <revolution/gx/GXStruct.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef enum _VITimeToDIM { VI_DM_DEFAULT = 0, VI_DM_10M, VI_DM_15M } VITimeToDIM;
+
 void VIInit(void);
-
-u32 VIGetTVFormat(void);
-
+void VIFlush();
+void VIWaitForRetrace();
+void VIConfigure(const GXRenderModeObj*);
+void VIConfigurePan(u16, u16, u16, u16);
+void VISetNextFrameBuffer(void*);
+void* VIGetNextFrameBuffer(void);
+void* VIGetCurrentFrameBuffer(void);
 VIRetraceCallback VISetPreRetraceCallback(VIRetraceCallback);
 VIRetraceCallback VISetPostRetraceCallback(VIRetraceCallback);
-
-void* VIGetCurrentFrameBuffer(void);
+void VISetBlack(BOOL);
+u32 VIGetRetraceCount(void);
+u32 VIGetCurrentLine(void);
+u32 VIGetTvFormat(void);
+u32 VIGetScanMode(void);
+u32 VIGetDTVStatus(void);
+BOOL VIEnableDimming(BOOL);
+u32 VIGetDimmingCount(void);
+BOOL VIResetDimmingCount();
 
 #ifdef __cplusplus
 }
