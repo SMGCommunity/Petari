1228d6dab22b36e9d92611749b3c48ac09954eff
Gabbo <gabry.lim06@gmail.com>
2026-07-24 17:57:13 +0200

    `MechanicKoopaMini` header

--- include/Game/Enemy/MechanicKoopaMini.hpp
+++ include/Game/Enemy/MechanicKoopaMini.hpp
@@ -2,11 +2,45 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
+class AnimScaleController;
+class WalkerStateBindStarPointer;
+
 class MechanicKoopaMini : public LiveActor {
 public:
     MechanicKoopaMini(const char*);
-    virtual ~MechanicKoopaMini();
 
-private:
-    u8 mPad[(0x98) - sizeof(LiveActor)];
+    virtual ~MechanicKoopaMini();
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void makeActorAppeared();
+    virtual void kill();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void updateHitSensor(HitSensor*);
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPush(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);    
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);    
+
+    void onAppearsSwitchOn();
+    void exeWait();
+    void exeWalkOnRail();
+    void exeTurn();
+    void exeJumpStart();
+    void exeJumpEnd();
+    void exeFind();
+    void exePursue();
+    void exePreFireAttack();
+    void exeFireAttack();
+    void exeFireAttackEnd();
+    void exeAttackHit();
+    void exeSpinHit();
+    void exeTrample();
+    void exeHipDropped();
+
+    /* 0x8C */ bool _8C;
+    /* 0x8D */ bool _8D;
+    /* 0x90 */ AnimScaleController* mScaleController;
+    /* 0x94 */ WalkerStateBindStarPointer* mBindStarPointer ;
 };
