3b271375b838a2e5115058fa97f67db050a2d934
Gabbo <gabry.lim06@gmail.com>
2026-07-29 00:34:39 +0200

    `AnimStampController` 100.0%

--- include/Game/Enemy/AnimStampController.hpp
+++ include/Game/Enemy/AnimStampController.hpp
@@ -12,8 +12,12 @@ public:
     void init(s32, f32, f32, f32, f32, f32, f32);
     void startAnim(const TVec3f&);
     void exeAnim();
+    void exeAnimEnd();
     bool isAnimEnd() const;
 
-    TVec3f _8;
-    TPos3f _14;
+    /* 0x8  */ TVec3f _8;
+    /* 0x14 */ TPos3f _14;
+    /* 0x44 */ s32 _44;
+    /* 0x50 */ TVec3f _50;  // unused
+    /* 0x5C */ TVec3f _5C;  // unused
 };
