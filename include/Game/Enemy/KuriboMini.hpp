272be031e8d2657b9f1546b6a870a77fddb085db
Gabbo <gabry.lim06@gmail.com>
2026-08-01 15:10:02 +0200

    KuriboMini header

--- include/Game/Enemy/KuriboMini.hpp
+++ include/Game/Enemy/KuriboMini.hpp
@@ -2,11 +2,62 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
+class AnimScaleController;
+class ItemGenerator;
+class WalkerStateWander;
+class WalkerStateFindPlayer;
+class WalkerStateChase;
+class WalkerStateStagger;
+class WalkerStateBindStarPointer;
+
 class KuriboMini : public LiveActor {
 public:
     KuriboMini(const char*);
-    virtual ~KuriboMini();
 
-private:
-    u8 mPad[(0xC4) - sizeof(LiveActor)];
+    virtual ~KuriboMini();
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void makeActorAppeared();
+    virtual void kill();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPush(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
+    void initState();
+    bool requestHipDropDown(HitSensor*, HitSensor*);
+    bool requestFlatDown(HitSensor*, HitSensor*);
+    bool requestPressDown(HitSensor*, HitSensor*);
+    bool requestBlowDown(HitSensor*, HitSensor*);
+    bool requestStagger(HitSensor*, HitSensor*);    
+    bool requestAttackSuccess();    
+    bool tryFind();
+    bool tryPointBind();    
+    bool tryDeadMap();    
+    void exeWander();
+    void exeFindPlayer();
+    void exeChase();
+    void exeStagger();
+    void exeAttackSuccess();    
+    void exeHipDropDown();
+    void exeFlatDown();
+    void exePressDown();
+    void exeBlowDown();
+    void calcPassiveMovement();
+    bool isEnableAttack() const;
+    bool isEnableKick() const;
+    bool isDown() const;
+
+    /* 0x8C */ AnimScaleController* mScaleController;    
+    /* 0x90 */ ItemGenerator* mItemGenerator;
+    /* 0x94 */ WalkerStateWander* mStateWander;
+    /* 0x98 */ WalkerStateFindPlayer* mStateFindPlayer;
+    /* 0x9C */ WalkerStateChase* mStateChase;
+    /* 0xA0 */ WalkerStateStagger* mStateStagger; 
+    /* 0xA4 */ WalkerStateBindStarPointer* mStateBindStarPointer;    
+    /* 0xA8 */ TQuat4f _A8;
+    /* 0xB8 */ TVec3f _B8;
 };
