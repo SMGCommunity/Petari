d20623c8a61e121a82a4fa37693d5751ae436bf9
ThatNintendoNerd <samtheman1026@gmail.com>
2026-08-01 01:52:21 -0700

    Cleanup `SandCapsulePressGround`

--- include/Game/MapObj/SandCapsulePressGround.hpp
+++ include/Game/MapObj/SandCapsulePressGround.hpp
@@ -1,26 +1,24 @@
 #pragma once
 
 #include "Game/MapObj/MapObjActor.hpp"
-#include "Game/MapObj/MapObjActorInitInfo.hpp"
-#include <JSystem/JGeometry.hpp>
+#include <JSystem/JGeometry/TMatrix.hpp>
 
 class SandCapsulePressGround : public MapObjActor {
 public:
     SandCapsulePressGround(const char*);
 
-    virtual ~SandCapsulePressGround();
     virtual void init(const JMapInfoIter&);
+
     virtual void initCaseUseSwitchA(const MapObjActorInitInfo&) {
-        return;
     }
-    virtual void initCaseNoUseSwitchA(const MapObjActorInitInfo&) {
-        return;
-    }
+
     virtual void initCaseUseSwitchB(const MapObjActorInitInfo&) {
-        return;
     }
+
+    virtual void initCaseNoUseSwitchA(const MapObjActorInitInfo&) {
+    }
+
     virtual void initCaseNoUseSwitchB(const MapObjActorInitInfo&) {
-        return;
     }
 
     void exeWait();
@@ -32,7 +30,7 @@ public:
     void calcMoveVelocity() NO_INLINE;
     bool tryStop();
 
-    TVec3f _C4;
-    TVec3f _D0;
-    TPos3f _DC;
+    /* 0xC4 */ TVec3f _C4;
+    /* 0xD0 */ TVec3f _D0;
+    /* 0xDC */ TPos3f _DC;
 };
