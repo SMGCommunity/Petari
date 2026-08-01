c19a40998b6cdcdb4d67181341b723b9bbd61c1a
ThatNintendoNerd <samtheman1026@gmail.com>
2026-07-31 23:33:22 -0700

    Match 98% of `SandUpDownEffectObj`

--- include/Game/MapObj/SandUpDownEffectObj.hpp
+++ include/Game/MapObj/SandUpDownEffectObj.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
 #include "Game/LiveActor/LiveActor.hpp"
+#include <JSystem/JGeometry/TMatrix.hpp>
+
+class TriangleFilterFunc;
 
 class SandUpDownEffectObj : public LiveActor {
 public:
-    SandUpDownEffectObj(const char*);
-    virtual ~SandUpDownEffectObj();
+    /// @brief Creates a new `SandUpDownEffectObj`.
+    /// @param pName A pointer to the null-terminated name of the object.
+    SandUpDownEffectObj(const char* pName);
+
+    virtual void init(const JMapInfoIter& rIter);
+
+    void startMoveUp();
+    void startMoveDown();
+    bool checkSandHit();
+    bool isUfoTop() const;
+
+    void exeWait();
+    void exeFallingRock();
+    void exeFallingRockAndSmoke();
+    void exeBurySmokePrepare();
+    void exeBurySmoke();
+    void exeBuryEnd();
 
 private:
-    u8 mPad[(0xD4) - sizeof(LiveActor)];
+    /* 0x8C */ f32 _8C;
+    /* 0x90 */ f32 _90;
+    /* 0x94 */ TVec3f _94;
+    /* 0xA0 */ TPos3f _A0;
+    /* 0xD0 */ TriangleFilterFunc* _D0;
 };
