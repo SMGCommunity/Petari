937f21ed671ec0fcf8f9ce0a9b32bdd33f863771
ThatNintendoNerd <samtheman1026@gmail.com>
2026-08-05 12:02:17 -0700

    Cleanup `HoneyBee`

--- include/Game/NPC/HoneyBee.hpp
+++ include/Game/NPC/HoneyBee.hpp
@@ -2,18 +2,31 @@
 
 #include "Game/NPC/NPCActor.hpp"
 
-class NameObjArchiveListCollector;
-
 class HoneyBee : public NPCActor {
 public:
-    HoneyBee(const char*);
+    enum InitAction {
+        /* 0x00 */ InitAction_Wait,
+        /* 0x01 */ InitAction_JumpLecture,
+        /* 0x02 */ InitAction_Fly,
+        /* 0x03 */ InitAction_Sleep,
+        /* 0x04 */ InitAction_Stick,
+        /* 0x05 */ InitAction_Gatekeeper,
+        /* 0x06 */ InitAction_DropLecture,
+        /* 0x07 */ InitAction_Flustered,
+        /* 0x08 */ InitAction_FlyLectureA,
+        /* 0x09 */ InitAction_FlyLectureB,
+    };
 
-    virtual ~HoneyBee();
+    /// @brief Creates a new `HoneyBee`.
+    /// @param pName A pointer to the null-terminated name of the object.
+    HoneyBee(const char* pName);
+
     virtual void init(const JMapInfoIter&);
     virtual void control();
     virtual void calcAndSetBaseMtx();
 
     static void makeArchiveList(NameObjArchiveListCollector*, const JMapInfoIter&);
+
     void exeWait();
     void exeReaction();
     void exeFly();
@@ -22,9 +35,9 @@ public:
     void exeFlyLectureA();
     void exeFlyLectureB();
 
-    s32 _15C;
-    TMtx34f _160;
-    TVec3f _190;
-    s32 _19C;
-    f32 _1A0;
+    /* 0x15C */ s32 _15C;
+    /* 0x160 */ TMtx34f _160;
+    /* 0x190 */ TVec3f _190;
+    /* 0x19C */ s32 mArg0;
+    /* 0x1A0 */ f32 _1A0;
 };
--- src/Game/NPC/HoneyBee.cpp
+++ src/Game/NPC/HoneyBee.cpp
@@ -15,6 +15,13 @@
 #include "Game/Util/SoundUtil.hpp"
 #include "Game/Util/TalkUtil.hpp"
 
+namespace {
+    static const s32 sFlyLectureWaitStep = 120;
+    static const s32 sHipLectureWaitStep = 120;
+    static const f32 sFlyMoveSpeed = 5.0f;
+    // static const f32 sMessageOffsetY = _;
+};  // namespace
+
 namespace NrvHoneyBee {
     NEW_NERVE(HoneyBeeNrvWait, HoneyBee, Wait);
     NEW_NERVE(HoneyBeeNrvReaction, HoneyBee, Reaction);
@@ -25,34 +32,29 @@ namespace NrvHoneyBee {
     NEW_NERVE(HoneyBeeNrvFlyLectureB, HoneyBee, FlyLectureB);
 };  // namespace NrvHoneyBee
 
-HoneyBee::HoneyBee(const char* pName) : NPCActor(pName) {
-    _15C = -1;
-    _19C = 0;
-    _1A0 = 0.0f;
+HoneyBee::HoneyBee(const char* pName) : NPCActor(pName), _15C(-1), mArg0(), _1A0() {
     _160.identity();
     _190.zero();
 }
 
-HoneyBee::~HoneyBee() {
-}
-
 void HoneyBee::makeArchiveList(NameObjArchiveListCollector* pArchive, const JMapInfoIter& rIter) {
-    const char* name;
-    MR::getObjectName(&name, rIter);
-    NPCActorItem npcItem = name;
-    s32 arg = -1;
-    MR::getJMapInfoArg0NoInit(rIter, &arg);
-    if (MR::getNPCItemData(&npcItem, arg)) {
+    const char* objName;
+    MR::getObjectName(&objName, rIter);
+    NPCActorItem npcItem = objName;
+
+    s32 arg0 = -1;
+    MR::getJMapInfoArg0NoInit(rIter, &arg0);
+
+    if (MR::getNPCItemData(&npcItem, arg0)) {
         NPCActor::addArchive(pArchive, npcItem);
     }
 }
 
 void HoneyBee::init(const JMapInfoIter& rIter) {
     f32 radius;
-    s32 arg;
     NPCActorCaps caps = "HoneyBee";
     caps.setDefault();
-    caps.mBinder = 0;
+    caps.mBinder = false;
     caps._6C = "Center";
     caps.mSensorJoint = "Center";
     caps.mSensorSize = 70.0f;
@@ -61,83 +63,74 @@ void HoneyBee::init(const JMapInfoIter& rIter) {
     caps.mSensorOffset.z = 0.0f;
     caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvWait::sInstance;
     caps._70 = "Center";
-    arg = 0;
-    MR::getJMapInfoArg0NoInit(rIter, &arg);
-    s32 switchTemp = _19C = arg;
+
+    s32 arg0 = 0;
+    MR::getJMapInfoArg0NoInit(rIter, &arg0);
+    mArg0 = arg0;
+
     setDefaultsParam();
     _128 = 0;
     _12C = 450.0f;
-    switch (switchTemp) {
-    case 0:
+
+    switch (mArg0) {
+    case InitAction_Wait:
         _128 = 1;
         break;
-
-    case 1:
+    case InitAction_JumpLecture:
         caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvJumpLecture::sInstance;
         break;
-
-    case 2:
+    case InitAction_Fly:
         caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvFly::sInstance;
         break;
-
-    case 3: {
+    case InitAction_Sleep:
         mParam.setNoTurnAction("SleepWait");
-    } break;
-
-    case 4:
+        break;
+    case InitAction_Stick:
         mParam.setNoTurnAction("StickWait");
         break;
-
-    case 5:
+    case InitAction_Gatekeeper:
         mParam.setSingleAction("GatekeeperWait");
         _128 = 1;
         break;
-
-    case 6:
+    case InitAction_DropLecture:
         caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvDropLecture::sInstance;
         break;
-
-    case 7:
+    case InitAction_Flustered:
         mParam.setSingleAction("Flustered");
         _128 = 1;
         break;
-
-    case 8:
+    case InitAction_FlyLectureA:
         caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvFlyLectureA::sInstance;
         caps.mTalkMtx = _160;
         caps.mMessageOffset.zero();
         break;
-
-    case 9:
+    case InitAction_FlyLectureB:
         caps.mWaitNerve = &NrvHoneyBee::HoneyBeeNrvFlyLectureB::sInstance;
         caps.mTalkMtx = _160;
         caps.mMessageOffset.zero();
         break;
-
     default:
         break;
     }
 
     NPCActor::initialize(rIter, caps);
     NPCActorItem item = ("HoneyBee");
-    switch (_19C) {
-    case 8:
-    case 9:
+
+    switch (mArg0) {
+    case InitAction_FlyLectureA:
+    case InitAction_FlyLectureB:
         radius = 0.0f;
         _160.set(getBaseMtx());
         MR::extractMtxTrans(_160, &_190);
         MR::calcModelBoundingRadius(&radius, this);
-        TVec3f vec;
-        vec.x = 0.0f;
-        vec.y = 0.0f;
-        vec.z = 0.0f;
-        MR::setShadowDropPositionAtJoint(this, nullptr, "Center", vec);
+        MR::setShadowDropPositionAtJoint(this, nullptr, "Center", TVec3f(0.0f, 0.0f, 0.0f));
         MR::onCalcShadow(this, nullptr);
         MR::setClippingTypeSphere(this, radius, &_190);
         mLodCtrl->invalidate();
         break;
     }
-    MR::getNPCItemData(&item, _19C);
+
+    MR::getNPCItemData(&item, mArg0);
     NPCActor::equipment(item, false);
     MR::initFur(this);
     MR::initFur(mLodCtrl->_10);
@@ -171,9 +164,9 @@ void HoneyBee::control() {
     f32 a = _160[1][3] - 80.0f;
     _160.set(MR::getJointMtx(this, "Center"));
     MR::extractMtxTrans(_160, &_190);
-    _160[1][3] = 80.0f + (((90.0f * a) + _160[1][3]) / 10.0f);
+    _160[1][3] = 80.0f + (((9.0f * a) + _160[1][3]) / 10.0f);
 
-    if (_19C != 3 && _19C != 4) {
+    if (mArg0 != InitAction_Sleep && mArg0 != InitAction_Stick) {
         _1A0 = MR::calcFloatOffset(this, _1A0, 120.0f);
     }
 }
@@ -194,17 +187,19 @@ void HoneyBee::exeFly() {
     }
 
     MR::startLevelSound(this, "SE_SM_LV_HONEYBEE_FLY");
-    if (mMsgCtrl) {
+
+    if (mMsgCtrl != nullptr) {
         MR::tryTalkNearPlayer(mMsgCtrl);
     }
 
-    MR::moveCoordAndFollowTrans(this, 5.0f);
+    MR::moveCoordAndFollowTrans(this, ::sFlyMoveSpeed);
+
     if (_15C != MR::getCurrentRailPointNo(this)) {
-        s32 arg = 0;
-        s32 railPoint = MR::getCurrentRailPointNo(this);
-        _15C = railPoint;
-        MR::getRailPointArg0WithInit(this, railPoint, &arg);
-        if (!arg) {
+        s32 arg0 = 0;
+        _15C = MR::getCurrentRailPointNo(this);
+        MR::getRailPointArg0WithInit(this, _15C, &arg0);
+
+        if (arg0 == 0) {
             if (!MR::isBckPlaying(this, "WalkWait")) {
                 MR::startBck(this, "Wait", nullptr);
             }
@@ -226,7 +221,7 @@ void HoneyBee::exeDropLecture() {
             NPCActor::turnToPlayer();
         }
 
-        if (MR::isGreaterStep(this, 120)) {
+        if (MR::isGreaterStep(this, ::sHipLectureWaitStep)) {
             MR::startBck(this, "HipDropWait", nullptr);
         }
     } else if (MR::isBckLooped(this)) {
@@ -244,7 +239,7 @@ void HoneyBee::exeJumpLecture() {
             NPCActor::turnToPlayer();
         }
 
-        if (MR::isGreaterStep(this, 120)) {
+        if (MR::isGreaterStep(this, ::sFlyLectureWaitStep)) {
             MR::startBck(this, "FlyLectureWait", nullptr);
         }
     } else if (MR::isBckLooped(this)) {
@@ -257,6 +252,7 @@ void HoneyBee::exeFlyLectureA() {
         MR::onCalcShadow(this, nullptr);
         MR::startBck(this, "FlyLectureA", nullptr);
     }
+
     MR::startLevelSound(this, "SE_SM_LV_HONEYBEE_FLY_SOFT");
     MR::tryTalkNearPlayer(mMsgCtrl);
 }
@@ -266,6 +262,7 @@ void HoneyBee::exeFlyLectureB() {
         MR::onCalcShadow(this, nullptr);
         MR::startBck(this, "FlyLectureB", nullptr);
     }
+
     MR::startLevelSound(this, "SE_SM_LV_HONEYBEE_FLY_SOFT");
     MR::tryTalkNearPlayer(mMsgCtrl);
 }
