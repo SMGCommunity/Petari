0703582858f7ff09cf71e1e6125f5a35ff74a5ca
ThatNintendoNerd <samtheman1026@gmail.com>
2026-08-05 12:35:23 -0700

    Cleanup `SignBoard`

--- include/Game/NPC/SignBoard.hpp
+++ include/Game/NPC/SignBoard.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "Game/LiveActor/HitSensor.hpp"
 #include "Game/NPC/NPCActor.hpp"
 
 class FollowCollisionArea;
 
 class SignBoard : public NPCActor {
 public:
-    SignBoard(const char*);
+    /// @brief Creates a new `SignBoard`.
+    /// @param pName A pointer to the null-terminated name of the object.
+    SignBoard(const char* pName);
 
-    virtual ~SignBoard();
     virtual void init(const JMapInfoIter&);
     virtual void control();
     virtual void attackSensor(HitSensor*, HitSensor*);
 
     void exeWait();
 
-    u32 _15C;
+    /* 0x15C */ u32 _15C;
 };
