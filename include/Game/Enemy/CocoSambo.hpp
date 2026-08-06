8171bdc1298f717f6d7af37f1beda74758ed4348
shibbo <supershibbo12@gmail.com>
2026-08-06 11:23:12 -0400

    start on `CocoSambo`

--- include/Game/Enemy/CocoSambo.hpp
+++ include/Game/Enemy/CocoSambo.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
-#include "Game/LiveActor/LiveActor.hpp"
+#include "Game/LiveActor/PartsModel.hpp"
+#include <JSystem/JGeometry.hpp>
+
+class CocoSamboHead : public PartsModel {
+public:
+    CocoSamboHead(LiveActor*);
+
+    virtual ~CocoSamboHead();
+    virtual void init(const JMapInfoIter&);
+    virtual void kill();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+
+    void updateFrontVecToPlayer(const TVec3f&);
+    void exeHeadConnectedBody();
+    void exeFall();
+    void exeFallLand();
+    void exeSwoon();
+    void exeSwoonEnd();
+    void exeBlow();
+
+    TVec3f mFrontVec;  // _9C
+};
 
 class CocoSambo : public LiveActor {
 public:
