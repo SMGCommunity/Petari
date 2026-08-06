ffc20c3b351c9d656e6cbe791ad615ba5bb23e4c
MrL314 <11429905+MrL314@users.noreply.github.com>
2026-08-05 23:31:35 -0400

    Match `CameraObjParallel`

--- include/Game/Camera/CameraObjParallel.hpp
+++ include/Game/Camera/CameraObjParallel.hpp
@@ -5,13 +5,18 @@
 class CameraObjParallel : public Camera {
 public:
     CameraObjParallel(const char* pName = "オブジェ並行カメラ");
-    virtual ~CameraObjParallel();
 
     virtual void reset();
     virtual CameraTargetObj* calc();
     virtual CamTranslatorBase* createTranslator();
 
-    f32 mAngleA;  // 0x4C
-    f32 mAngleB;  // 0x50
-    f32 mDist;    // 0x54
+    void setParam(f32 dist, f32 angleX, f32 angleY) {
+        mDist = dist;
+        mAngleX = angleX;
+        mAngleY = angleY;
+    }
+
+    /* 0x4C */ f32 mAngleX;
+    /* 0x50 */ f32 mAngleY;
+    /* 0x54 */ f32 mDist;
 };
