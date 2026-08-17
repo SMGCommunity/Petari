d1301a6028b21875e0f48d451d7f3a8c58489a50
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2026-08-17 12:41:39 +0200

    `Kabokuri` header and nerves

--- include/Game/Enemy/Kabokuri.hpp
+++ include/Game/Enemy/Kabokuri.hpp
@@ -7,6 +7,36 @@ public:
     Kabokuri(const char*);
     virtual ~Kabokuri();
 
+    virtual void init(const JMapInfoIter&);
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+
+    void initSensor();
+    void updatePose();
+    void addVelocityBase();
+    void addVelocityToRailPoint(f32);
+    void requestTrampled();
+    void requestStarPieceHitted();
+    void requestHipDropped();
+    void requestHitAttacked(HitSensor*, HitSensor*);
+    void tryPointBind();
+    void exeWait();
+    void exeWalk();
+    void exeDropFire();
+    void exeAttackSuccess();
+    void exeTrampled();
+    void exeStarPieceHitted();
+    void exeHipDropped();
+    void exeHitAttacked();
+    void exeBreak();
+    void exeBindStarPointer();
+    bool isEnableAttack() const;
+    bool isEnablePointBind() const;
+    bool isEnableTrampled() const;
+    bool isEnablePush() const;
+
 private:
     u8 mPad[(0xC0) - sizeof(LiveActor)];
 };
--- src/Game/Enemy/Kabokuri.cpp
+++ src/Game/Enemy/Kabokuri.cpp
@@ -1,4 +1,18 @@
 #include "Game/Enemy/Kabokuri.hpp"
+#include "Game/LiveActor/Nerve.hpp"
+
+namespace NrvKabokuri {
+    NEW_NERVE(KabokuriNrvWait, Kabokuri, Wait);
+    NEW_NERVE(KabokuriNrvWalk, Kabokuri, Walk);
+    NEW_NERVE(KabokuriNrvDropFire, Kabokuri, DropFire);
+    NEW_NERVE(KabokuriNrvAttacksuccess, Kabokuri, AttackSuccess);
+    NEW_NERVE(KabokuriNrvTrampled, Kabokuri, Trampled);
+    NEW_NERVE(KabokuriNrvStarPieceHitted, Kabokuri, StarPieceHitted);
+    NEW_NERVE(KabokuriNrvBindStarPointer, Kabokuri, BindStarPointer);
+    NEW_NERVE(KabokuriNrvHitAttacked, Kabokuri, HitAttacked);
+    NEW_NERVE(KabokuriNrvHipDropped, Kabokuri, HipDropped);
+    NEW_NERVE(KabokuriNrvBreak, Kabokuri, Break);
+};  // namespace NrvKabokuri
 
 Kabokuri::Kabokuri(const char* pName) : LiveActor(pName) {
 }
