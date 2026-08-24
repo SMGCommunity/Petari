b98297f492714826ffba64037dca89682da7b9f2
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2026-08-24 22:57:32 +0200

    `JumpGuarder` (#1985)
    * `JumpGuarder` 30%
    
    * Implicit `JumpEmitter` dtor
    
    * `JumpGuarder` consts
    
    * `JumpGuarder` 44%
    
    * `JumpGuarder` 70%
    
    * `JumpGuarder` 81%
    
    * `JumpGuarder` 96%
    
    * Match `JumpEmitter::updateRotate()`
    
    * `JumpGuarder` 99%
    
    * use `ATYPE` enum, fix fakematch
    
    * Match `JumpEmitter`
    
    * `JumpEmitter` cleanup
    
    * `BegomanBaby` ctor and member name fixes
    
    * use `MR::abs`
    
    * `gekko_pe_copy` -> `TMtx34::set`
    
    * Move docs to header

--- include/Game/Enemy/JumpGuarder.hpp
+++ include/Game/Enemy/JumpGuarder.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
+#include "Game/Enemy/BegomanBaby.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
-#include <JSystem/JGeometry/TMatrix.hpp>
 
 class ActorCameraInfo;
 class PartsModel;
 class MultiEventCamera;
+class JumpGuarderBaby;
 
 class JumpEmitter : public LiveActor {
 public:
     JumpEmitter(const char*);
 
-    virtual ~JumpEmitter();
     virtual void kill();
 
     void initEventCamera(const JMapInfoIter&);
@@ -20,24 +20,59 @@ public:
     void endEventCamera();
     void updateRotate();
 
-    MtxPtr _8C;
-    TMtx34f _90;
-    PartsModel* mHeadModel;  // 0xC0
-    TVec3f _C4;
-    u8 _D0;
-    ActorCameraInfo* mCameraInfo;        // 0xD4
-    MultiEventCamera* mMultEventCamera;  // 0xD8
+    /* 0x8C */ MtxPtr mBodyJointMtx;
+    /* 0x90 */ TMtx34f _90;
+    /* 0xC0 */ PartsModel* mHeadModel;
+    /* 0xC4 */ TVec3f _C4;
+    /* 0xD0 */ bool mIsEventCamera;
+    /* 0xD4 */ ActorCameraInfo* mCameraInfo;
+    /* 0xD8 */ MultiEventCamera* mMultEventCamera;
 };
 
 namespace MR {
     bool enableGroupAttack(LiveActor*, f32, f32);
 };  // namespace MR
 
-class JumpGuarder : public NameObj {
+class JumpGuarder : public JumpEmitter {
 public:
     JumpGuarder(const char*);
-    virtual ~JumpGuarder();
 
-private:
-    u8 mPad[(0x104) - sizeof(NameObj)];
+    virtual void init(const JMapInfoIter&);
+    virtual void control();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
+    /// @brief Filters out their own babies.
+    bool isHit(const LiveActor*) const;
+
+    bool enableAttack();
+    void exeHide();
+    void exeUp();
+    void exeWait();
+    void exeDown();
+    void exeHopStart();
+    void exeHopWait();
+    void exeHopJump();
+    void exeHopEnd();
+    void exePreOpen();
+    void addBaby(JumpGuarderBaby*);
+    void exeOpen();
+    void exeClose();
+    void exeInter();
+
+    /// @brief Allocated array of `mNumBabies` babies.
+    /* 0x0DC */ JumpGuarderBaby* mBabies;
+
+    /* 0x0E0 */ s32 mNumBabies;
+    /* 0x0E4 */ s32 mBumpCooldown;
+    /* 0x0E8 */ JumpGuarderBaby* mActiveBabies[4];
+    /* 0x0F8 */ s32 mNumActiveBabies;
+    /* 0x0FC */ s32 mNumCoins;
+    /* 0x100 */ f32 mBabyVelocity;
+};
+
+class JumpGuarderBaby : public BegomanBaby {
+public:
+    JumpGuarderBaby();
 };
