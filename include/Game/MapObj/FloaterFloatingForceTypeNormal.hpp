9e646384a85087ca740dcdf19bbdc8b41e517e60
ThatNintendoNerd <samtheman1026@gmail.com>
2026-08-26 15:25:15 -0700

    Match 99% of `FloaterFloatingForceTypeNormal`

--- include/Game/MapObj/FloaterFloatingForceTypeNormal.hpp
+++ include/Game/MapObj/FloaterFloatingForceTypeNormal.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
-#include "Game/LiveActor/Nerve.hpp"
 #include "Game/MapObj/FloaterFloatingForce.hpp"
 
 class FloaterFloatingForceTypeNormal : public FloaterFloatingForce {
 public:
     FloaterFloatingForceTypeNormal(LiveActor*, const char*);
 
-    virtual ~FloaterFloatingForceTypeNormal();
     virtual void init(const JMapInfoIter&);
     virtual void start();
     virtual void updateHostTrans(TVec3f*) const;
-    virtual const TVec3f& getCurrentVelocity() const;
+    virtual const TVec3f& getCurrentVelocity() const {
+        return mVelocity;
+    }
 
     void updateVelocity(bool);
     void soundMoveSE();
@@ -20,9 +20,9 @@ public:
     void exeMoveFirst();
     void exeMove();
 
-    f32 mRotateAngle;  // 0x38
-    f32 mRotateSpeed;  // 0x3C
-    f32 _40;
-    f32 _44;
-    TVec3f mVelocity;  // 0x48
+    /* 0x38 */ f32 mRotateAngle;
+    /* 0x3C */ f32 mRotateSpeed;
+    /* 0x40 */ f32 _40;
+    /* 0x44 */ f32 _44;
+    /* 0x48 */ TVec3f mVelocity;
 };
