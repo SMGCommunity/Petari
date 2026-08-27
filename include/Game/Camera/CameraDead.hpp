21f34b6f02dd310a350b54ea26bfc447fd3d8ca2
MrL314 <11429905+MrL314@users.noreply.github.com>
2026-08-27 06:34:30 -0400

    Cleanup `CameraDead`

--- include/Game/Camera/CameraDead.hpp
+++ include/Game/Camera/CameraDead.hpp
@@ -4,6 +4,11 @@
 
 class CameraDead : public Camera {
 public:
+    enum CameraType {
+        CameraType_FixedPos = 0,
+        CameraType_Interpolate = 1,
+    };
+
     CameraDead(const char* pName = "通常死亡カメラ");
 
     virtual void reset();
@@ -13,9 +18,15 @@ public:
     }
     virtual CamTranslatorBase* createTranslator();
 
+    void setParam(f32 dist, s32 deadTime, s32 cameraType) {
+        mDist = dist;
+        mDeadTime = deadTime;
+        mCameraType = cameraType;
+    }
+
     /* 0x4C */ f32 mFovy;
     /* 0x50 */ f32 mDist;
     /* 0x54 */ s32 mDeadTime;
-    /* 0x58 */ s32 _58;  // TODO: is this a state or a bool?
+    /* 0x58 */ s32 mCameraType;
     /* 0x5C */ s32 mDeadFrame;
 };
