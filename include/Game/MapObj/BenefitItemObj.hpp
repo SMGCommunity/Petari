2b1c99202bd1a605aa2a37c66321168f99f44ee9
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-08-27 15:39:07 +0200

    Update BenefitItemObj.hpp

--- include/Game/MapObj/BenefitItemObj.hpp
+++ include/Game/MapObj/BenefitItemObj.hpp
@@ -2,7 +2,7 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 #include "Game/LiveActor/Nerve.hpp"
-#include "JSystem/JGeometry/TMatrix.hpp"
+#include <JSystem/JGeometry/TMatrix.hpp>
 
 class ActorCameraInfo;
 class FlashingCtrl;
@@ -57,7 +57,7 @@ public:
     /* 0x0BA */ s16 _BA;
     /* 0x0BC */ TVec3f _BC;
     /* 0x0C8 */ TVec3f _C8;
-    /* 0x0D4 */ MtxPtr mFollowMtx;  // 0xD4
+    /* 0x0D4 */ MtxPtr mFollowMtx;
     /* 0x0D8 */ u8 _D8;
     /* 0x0D9 */ u8 _D9;
     /* 0x0DA */ u8 _DA;
@@ -78,11 +78,11 @@ public:
     /* 0x0EC */ f32 _EC;
     /* 0x0F0 */ TMtx34f _F0;
     /* 0x120 */ const char* _120;
-    /* 0x124 */ FlashingCtrl* mFlashCtrl;      // 0x124
-    /* 0x128 */ ActorCameraInfo* mCameraInfo;  // 0x128
+    /* 0x124 */ FlashingCtrl* mFlashCtrl;
+    /* 0x128 */ ActorCameraInfo* mCameraInfo;
     /* 0x12C */ u32 _12C;
     /* 0x130 */ u32 _130;
-    /* 0x134 */ LiveActor* mHitSensorActor;  // 0x134
+    /* 0x134 */ LiveActor* mHitSensorActor;
     /* 0x138 */ u8 _138;
     /* 0x139 */ u8 _139;
     /* 0x13A */ u16 _13A;
