a3fcbec9b3f0e247bfcd4437fecd67a471ae3bc4
NextOp-Next <58630743+NextOp-Next@users.noreply.github.com>
2026-08-27 17:39:30 +0200

    `BalloonFish` declarations

--- include/Game/Enemy/Balloonfish.hpp
+++ include/Game/Enemy/Balloonfish.hpp
@@ -7,6 +7,22 @@ public:
     Balloonfish(const char*);
     virtual ~Balloonfish();
 
-private:
-    u8 mPad[(0xAC) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter& rIter);
+    virtual void appear();
+    virtual void kill();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+
+    void exeWait();
+    void exeDash();
+    void exeDashEnd();
+    void exeStarPointerBind();
+
+    /* _8C */ u32 _8C;
+    /* _90 */ u32 _90;
+    /* _94 */ u32 _94;
+    /* _98 */ TQuat4f _98;
+    /* _A8 */ f32 _A8;
 };
--- src/Game/Enemy/Balloonfish.cpp
+++ src/Game/Enemy/Balloonfish.cpp
@@ -1,6 +1,30 @@
 #include "Game/Enemy/Balloonfish.hpp"
+#include "Game/LiveActor/Nerve.hpp"
+#include "Game/Util/JMapInfo.hpp"
 
-Balloonfish::Balloonfish(const char* pName) : LiveActor(pName) {
+namespace {
+    // static const ??? hWaitTime = ???;
+    // static const ??? hAttackBeginTime = ???;
+    // static const ??? hTargetPlayerVelocityMult = ???;
+    // static const ??? hWaitMaxScale = ???;
+    // static const ??? hDashTime = ???;
+    // static const ??? hDashVel = ???;
+    // static const ??? hDashScale = ???;
+    // static const ??? hDashEndTime = ???;
+    // static const ??? hRotateAngle = ???;
+}
+
+namespace NrvBalloonFish {
+    NEW_NERVE(HostTypeNrvStarPointerBind, Balloonfish, StarPointerBind);
+    NEW_NERVE(HostTypeNrvDashEnd, Balloonfish, DashEnd);
+    NEW_NERVE(HostTypeNrvDash, Balloonfish, Dash);
+    NEW_NERVE(HostTypeNrvWait, Balloonfish, Wait);
+};  // namespace NrvBalloonFish
+
+Balloonfish::Balloonfish(const char* pName) : LiveActor(pName), _8C(), _90(), _98(0.0f, 0.0f, 0.0f, 1.0f), _A8() {
 }
 Balloonfish::~Balloonfish() {
+}
+
+void Balloonfish::init(const JMapInfoIter& rIter) {
 }
