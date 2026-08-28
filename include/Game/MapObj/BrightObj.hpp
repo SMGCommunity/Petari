61d46f0f4f7f396e06ab09b5db0605c4eb63affe
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-08-28 06:27:26 +0200

    Match a lot of `BrightObj`

--- include/Game/MapObj/BrightObj.hpp
+++ include/Game/MapObj/BrightObj.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "Game/NameObj/NameObj.hpp"
+#include "Game/LiveActor/LiveActor.hpp"
 #include <JSystem/JGeometry/TMatrix.hpp>
+
+class Sun;
 
 class BrightInfo {
 public:
@@ -12,8 +14,8 @@ public:
     void endRead();
     void reset();
 
-    /* 0x00 */ u32 _0;
-    /* 0x04 */ u32 _4;
+    /* 0x00 */ u32 mIndex;
+    /* 0x04 */ u32 mCurrIndex;
     /* 0x08 */ TVec2f mBrightnessCenter[3];
     /* 0x20 */ TVec2f mRealCenter[3];
     /* 0x38 */ f32 mBright[3];
@@ -35,28 +37,79 @@ public:
     /// @brief Creates a new `BrightCamInfo`.
     BrightCamInfo();
 
-    // void write(u16, const TPos3f&, const JGeometry::TProjection3<JGeometry::TMatrix44<JGeometry::SMatrix44C<f32>>>&, const TVec3f&, const TVec3f&)
+    void write(u16, const TPos3f&, const TProj3f&, const TVec3f&, const TVec3f&);
 
-    /* 0x00 */ TMtx34f mViewMtx[2];
-    /* 0x60 */ Mtx44 mProjectionMtx[2];
+    /* 0x00 */ TPos3f mViewMtx[2];
+    /* 0x60 */ TProj3f mProjectionMtx[2];
     /* 0xE0 */ TVec3f mCameraDir[2];
     /* 0xF8 */ TVec3f mCameraPos[2];
 };
 
-class BrightObj : public NameObj {
+class BrightObjBase {
 public:
-    BrightObj(const char*);
-    virtual ~BrightObj();
+    struct CheckArg {
+        CheckArg() : mMaxBrightness(), mCurrBrightness(), mObjCenter(0.0f, 0.0f), mCenter(0.0f, 0.0f) {};
 
-private:
-    u8 mPad[(0x110) - sizeof(NameObj)];
+        /* 0x00 */ u32 mMaxBrightness;
+        /* 0x04 */ u32 mCurrBrightness;
+        /* 0x08 */ TVec2f mObjCenter;
+        /* 0x10 */ TVec2f mCenter;
+    };
+
+    BrightObjBase();
+
+    virtual ~BrightObjBase();
+    virtual void calcBrightInfo(u16, const BrightCamInfo&) = nullptr;
+    virtual f32 getBright() const;
+    virtual const TVec2f* getBrightCenter() const;
+    virtual const TVec2f* getCenter() const;
+    virtual void endRead();
+    virtual void getNowCenter(TVec2f*) const;
+
+    void checkVisibilityOfSphere(u16, const BrightCamInfo&);
+    void checkVisible(CheckArg*, const TVec3f&, const TPos3f&, const TProj3f&);
+    void setResult(const CheckArg&);
+    void drawSphere(const TVec3f&, float) const;
+
+    /* 0x04 */ BrightInfo mInfo;
+    /* 0x48 */ TVec2f mBrightCenter;
+    /* 0x50 */ TVec2f mNowCenter;
+    /* 0x58 */ f32 mBrightness;
+    /* 0x5C */ bool mIsNotVisible;
+    /* 0x60 */ BrightDrawInfo mDrawInfo;
 };
 
-class BrightSun : public NameObj {
+class BrightObj : public LiveActor, public BrightObjBase {
+public:
+    BrightObj(const char*);
+
+    virtual ~BrightObj();
+    virtual void init(const JMapInfoIter&);
+    virtual void draw() const;
+    virtual void control();
+
+    virtual void calcBrightInfo(u16, const BrightCamInfo&);
+    virtual void getNowCenter(TVec2f*) const;
+
+    void checkVisibilityOfSphere(u16 index, const BrightCamInfo&);
+    void getNowCenter(TVec2f*);
+
+    /* 0x10C */ f32 mRadius;
+};
+
+class BrightSun : public LiveActor, public BrightObjBase {
 public:
     BrightSun(const char*);
-    virtual ~BrightSun();
 
-private:
-    u8 mPad[(0x110) - sizeof(NameObj)];
+    virtual ~BrightSun();
+    virtual void init(const JMapInfoIter&);
+    virtual void draw() const;
+    virtual void control();
+
+    virtual void calcBrightInfo(u16, const BrightCamInfo&);
+    virtual void getNowCenter(TVec2f*) const;
+
+    void controlSunModel();
+
+    /* 0x10C */ Sun* mSun;
 };
--- src/Game/MapObj/BrightObj.cpp
+++ src/Game/MapObj/BrightObj.cpp
@@ -1,25 +1,82 @@
 #include "Game/MapObj/BrightObj.hpp"
-#include "Game/LiveActor/Nerve.hpp"
-#include "Game/Util.hpp"
+#include "Game/MapObj/Sun.hpp"
+#include "Game/Scene/SceneFunction.hpp"
+#include "Game/Screen/LensFlare.hpp"
+#include "Game/Util/ActorSwitchUtil.hpp"
+#include "Game/Util/CameraUtil.hpp"
+#include "Game/Util/DemoUtil.hpp"
+#include "Game/Util/DirectDraw.hpp"
+#include "Game/Util/JMapUtil.hpp"
+#include "Game/Util/LiveActorUtil.hpp"
+#include "Game/Util/MathUtil.hpp"
+#include "Game/Util/MtxUtil.hpp"
+#include "Game/Util/ObjUtil.hpp"
+#include "Game/Util/ScreenUtil.hpp"
+#include <JSystem/JUtility/JUTVideo.hpp>
+
+namespace {
+    const static f32 sSunRadius = 3000.0f;
+    const static f32 sSunModelScale = 100.0f;
+
+    bool calcScreenPosition(TVec2f* pDst, const TVec3f& rVec, const TPos3f& rPos, const TProj3f& rProj) {
+        TVec3f vec58;
+        rPos.mult(rVec, vec58);
+
+        TVec3f vec30;
+        rProj.mult(vec58, vec30);
+
+        bool ret;
+        if (1.0f < MR::abs(vec30.x) || 1.0f < MR::abs(vec30.y)) {
+            ret = false;
+        } else {
+            ret = 0.0f < vec30.z == false;
+        }
+
+        f32 width = MR::getScreenWidth();
+        f32 height = JUTGetVideoManager()->getEfbHeight();
+        TVec3f screenPos(0.5f * width + 0.5f * vec30.x * width, 0.5f * height + 0.5f * -vec30.y * height, vec30.z);
+
+        pDst->x = screenPos.x;
+        pDst->y = screenPos.y;
+
+        return ret;
+    }
+};  // namespace
 
 BrightInfo::BrightInfo() {
     reset();
 }
 
-void BrightInfo::endRead() {
-    u32 v1 = _4;
-    if (v1 != _0) {
-        _4 = v1 + 1;
+void BrightInfo::write(const TVec2f& rBrightnessCenter, const TVec2f& rRealCenter, f32 brightness) {
+    mBrightnessCenter[mIndex].set(rBrightnessCenter);
+    mRealCenter[mIndex].set(rRealCenter);
+    mBright[mIndex] = brightness;
 
-        if (v1 + 1 > 2) {
-            _4 = 0;
-        }
+    u32 val = mIndex;
+    if (++val > 2) {
+        val = 0;
+    }
+
+    if (val != mCurrIndex) {
+        mIndex = val;
+    }
+}
+
+void BrightInfo::endRead() {
+    if (mCurrIndex == mIndex) {
+        return;
+    }
+
+    mCurrIndex++;
+
+    if (mCurrIndex > 2) {
+        mCurrIndex = 0;
     }
 }
 
 void BrightInfo::reset() {
-    _0 = 0;
-    _4 = 0;
+    mIndex = 0;
+    mCurrIndex = 0;
 
     for (u32 i = 0; i < 3; i++) {
         mBrightnessCenter[i].zero();
@@ -29,44 +86,325 @@ void BrightInfo::reset() {
 }
 
 BrightDrawInfo::BrightDrawInfo() {
-    for (u32 i = 0; i < 2; i++) {
-        mPosition[i].zero();
-        mRadius[i] = 0.0f;
+    for (u32 idx = 0; idx < 2; idx++) {
+        mPosition[idx].zero();
+        mRadius[idx] = 0.0f;
     }
+}
+
+void BrightDrawInfo::write(u16 index, const TVec3f& rPosition, f32 radius) {
+    mPosition[index].set(rPosition);
+    mRadius[index] = radius;
 }
 
 BrightCamInfo::BrightCamInfo() {
-    for (u32 i = 0; i < 2; i++) {
-        mViewMtx[i].identity();
+    for (u32 idx = 0; idx < ARRAY_SIZE(mViewMtx); idx++) {
+        mViewMtx[idx].identity();
+
         /* set mtx to identity mtx */
-        Mtx44Ptr mtx = mProjectionMtx[i];
-        mtx[2][3] = 0.0f;
-        mtx[1][3] = 0.0f;
-        mtx[0][3] = 0.0f;
-        mtx[3][2] = 0.0f;
-        mtx[1][2] = 0.0f;
-        mtx[0][2] = 0.0f;
-        mtx[3][1] = 0.0f;
-        mtx[2][1] = 0.0f;
-        mtx[0][1] = 0.0f;
-        mtx[3][0] = 0.0f;
-        mtx[2][0] = 0.0f;
-        mtx[1][0] = 0.0f;
-        mtx[3][3] = 1.0f;
-        mtx[2][2] = 1.0f;
-        mtx[1][1] = 1.0f;
-        mtx[0][0] = 1.0f;
-        mCameraDir[i].set(0.0f, 1.0f, 0.0f);
-        mCameraPos[i].zero();
+        mProjectionMtx[idx][2][3] = 0.0f;
+        mProjectionMtx[idx][1][3] = 0.0f;
+        mProjectionMtx[idx][0][3] = 0.0f;
+        mProjectionMtx[idx][3][2] = 0.0f;
+        mProjectionMtx[idx][1][2] = 0.0f;
+        mProjectionMtx[idx][0][2] = 0.0f;
+        mProjectionMtx[idx][3][1] = 0.0f;
+        mProjectionMtx[idx][2][1] = 0.0f;
+        mProjectionMtx[idx][0][1] = 0.0f;
+        mProjectionMtx[idx][3][0] = 0.0f;
+        mProjectionMtx[idx][2][0] = 0.0f;
+        mProjectionMtx[idx][1][0] = 0.0f;
+        mProjectionMtx[idx][3][3] = 1.0f;
+        mProjectionMtx[idx][2][2] = 1.0f;
+        mProjectionMtx[idx][1][1] = 1.0f;
+        mProjectionMtx[idx][0][0] = 1.0f;
+
+        mCameraDir[idx].set< f32 >(0.0f, 1.0f, 0.0f);
+        mCameraPos[idx].zero();
     }
 }
 
-BrightObj::BrightObj(const char* pName) : NameObj(pName) {
+void BrightCamInfo::write(u16 index, const TPos3f& rViewMtx, const TProj3f& rProjMtx, const TVec3f& rCamDir, const TVec3f& rCamPos) {
+    mViewMtx[index].set(rViewMtx);
+    mProjectionMtx[index].set(rProjMtx);
+    mCameraDir[index].set(rCamDir);
+    mCameraPos[index].set(rCamPos);
 }
+
+BrightObjBase::BrightObjBase() : mInfo(), mBrightCenter(0.0f, 0.0f), mNowCenter(0.0f, 0.0f), mBrightness(), mIsNotVisible(true), mDrawInfo() {
+    MR::addBrightObj(this);
+}
+
+void BrightObjBase::checkVisibilityOfSphere(u16 index, const BrightCamInfo& rCamInfo) {
+    GXPokeAlphaRead(GX_READ_NONE);
+
+    TVec3f drawPos(mDrawInfo.mPosition[index]);
+    TVec3f camDir(rCamInfo.mCameraDir[index]);
+    TVec3f drawDir(rCamInfo.mCameraPos[index] - drawPos);
+
+    if (MR::isNearZero(drawDir)) {
+        mIsNotVisible = true;
+        mBrightness = 0.0f;
+
+        return;
+    }
+
+    MR::normalize(&drawDir);
+
+    TVec3f vec110;
+    vec110.cross(camDir, drawDir);
+    camDir.cross(drawDir, vec110);
+
+    TPos3f mtx;
+    mtx.setTR(vec110, camDir, drawDir, drawPos);
+
+    CheckArg args = CheckArg();
+    calcScreenPosition(&args.mCenter, drawPos, rCamInfo.mViewMtx[index], rCamInfo.mProjectionMtx[index]);
+    checkVisible(&args, drawPos, rCamInfo.mViewMtx[index], rCamInfo.mProjectionMtx[index]);
+
+    for (u32 idx = 0; idx < 8; idx++) {
+        f32 angle = idx / 4.0f * MR::pi();
+        TVec3f vec11C(MR::cos(angle), MR::sin(angle), 0.0f);
+        vec11C.scale(mDrawInfo.mRadius[index] * 0.4f);
+
+        mtx.mult(vec11C, vec11C);
+
+        checkVisible(&args, vec11C, rCamInfo.mViewMtx[index], rCamInfo.mProjectionMtx[index]);
+    }
+
+    for (u32 idx = 0; idx < 8; idx++) {
+        f32 angle = (idx + 0.5f) / 4.0f * MR::pi();
+        TVec3f vec128(MR::cos(angle), MR::sin(angle), 0.0f);
+        vec128.scale(mDrawInfo.mRadius[index] * 0.7f);
+
+        mtx.mult(vec128, vec128);
+
+        checkVisible(&args, vec128, rCamInfo.mViewMtx[index], rCamInfo.mProjectionMtx[index]);
+    }
+
+    setResult(args);
+}
+
+void BrightObjBase::checkVisible(BrightObjBase::CheckArg* pArg, const TVec3f& rVec, const TPos3f& rPos, const TProj3f& rProj) {
+    TVec2f screenPos;
+    if (calcScreenPosition(&screenPos, rVec, rPos, rProj)) {
+        TVec2f frameBufferPos;
+        MR::convertScreenPosToFrameBufferPos(&frameBufferPos, screenPos);
+
+        u32 color;
+        GXPeekARGB(frameBufferPos.x, frameBufferPos.y, &color);
+
+        if ((color & 0xFF000000) >= 0xF0000000) {
+            pArg->mCurrBrightness++;
+            pArg->mObjCenter += screenPos;
+        }
+    }
+
+    pArg->mMaxBrightness++;
+}
+
+void BrightObjBase::setResult(const BrightObjBase::CheckArg& rArg) {
+    f32 ratio = static_cast< f32 >(rArg.mCurrBrightness) / static_cast< f32 >(rArg.mMaxBrightness);
+
+    if (rArg.mCurrBrightness != 0) {
+        f32 scale = 1.0f / rArg.mCurrBrightness;
+        mBrightCenter.set(TVec2f(rArg.mObjCenter.x * scale, rArg.mObjCenter.y * scale));
+        mBrightness = ratio;
+        mNowCenter.set(rArg.mCenter);
+        mIsNotVisible = false;
+    } else {
+        mBrightness = ratio;
+        mIsNotVisible = true;
+    }
+
+    mInfo.write(mBrightCenter, rArg.mCenter, mBrightness);
+}
+
+void BrightObjBase::drawSphere(const TVec3f& rPos, float radius) const {
+    TPos3f viewMtx(MR::getCameraViewMtx());
+
+    TPos3f mtx;
+    mtx.identity33();
+    mtx.setTrans(rPos);
+    mtx.concat(viewMtx, mtx);
+
+    TDDraw::setup(0, 0, 1);
+    TDDraw::loadViewMtx(mtx);
+
+    GXSetColorUpdate(GX_FALSE);
+    GXSetAlphaUpdate(GX_TRUE);
+    GXSetDstAlpha(GX_FALSE, 0);
+    GXSetZMode(GX_TRUE, GX_ALWAYS, GX_FALSE);
+
+    TDDraw::drawSphere(TVec3f(0, 0, 0), radius * 1.1f, 0xFF000000, 0x10);
+
+    GXSetZMode(GX_TRUE, GX_LEQUAL, GX_FALSE);
+
+    TDDraw::drawSphere(TVec3f(0, 0, 0), radius, 0x00FF00FF, 0x10);
+
+    GXSetColorUpdate(GX_TRUE);
+    GXSetAlphaUpdate(GX_FALSE);
+}
+
+BrightObj::BrightObj(const char* pName) : LiveActor(pName), BrightObjBase(), mRadius(::sSunModelScale) {
+}
+
+void BrightObj::init(const JMapInfoIter& rIter) {
+    MR::initDefaultPos(this, rIter);
+
+    MR::getJMapInfoArg0NoInit(rIter, &mRadius);
+
+    MR::invalidateClipping(this);
+
+    MR::connectToScene(this, MR::MovementType_Environment, -1, -1, MR::DrawType_BrightSun);
+
+    makeActorAppeared();
+}
+
+void BrightObj::control() {
+    f32 radius = mRadius;
+    mDrawInfo.write(MR::getLensFlareDrawSyncTokenIndex(), mPosition, radius);
+}
+
+void BrightObj::draw() const {
+    if (MR::isDead(this)) {
+        return;
+    }
+
+    if (MR::isHiddenModel(this)) {
+        return;
+    }
+
+    switch (MR::isClipped(this)) {
+    case false:
+        drawSphere(mPosition, mRadius);
+    }
+}
+
+void BrightObj::checkVisibilityOfSphere(u16 index, const BrightCamInfo& rCamInfo) {
+    BrightObjBase::checkVisibilityOfSphere(index, rCamInfo);
+}
+
+void BrightObj::getNowCenter(TVec2f* pDst) {
+    calcScreenPosition(pDst, mPosition, MR::getCameraViewMtx(), MR::getCameraProjectionMtx());
+}
+
+BrightSun::BrightSun(const char* pName) : LiveActor(pName), BrightObjBase(), mSun() {
+}
+
+void BrightSun::control() {
+    TVec3f camPos = MR::getCamPos();
+
+    TPos3f mtx;
+    mtx.identity33();
+    mtx.setTrans(0.0f, 0.0f, 100000.0f);
+
+    TPos3f mtxTR;
+    MR::makeMtxTR(mtxTR, camPos.x, camPos.y, camPos.z, mRotation.x, mRotation.y, mRotation.z);
+
+    mtxTR.concat(mtx);
+    mtxTR.getTrans(mPosition);
+
+    controlSunModel();
+
+    mDrawInfo.write(MR::getLensFlareDrawSyncTokenIndex(), mPosition, ::sSunRadius);
+}
+
+void BrightSun::draw() const {
+    if (MR::isDead(this)) {
+        return;
+    }
+
+    if (MR::isHiddenModel(this)) {
+        return;
+    }
+
+    switch (MR::isClipped(this)) {
+    case false:
+        drawSphere(mPosition, ::sSunRadius);
+    }
+}
+
+void BrightSun::calcBrightInfo(u16 index, const BrightCamInfo& rCamInfo) {
+    if (MR::isDead(this)) {
+        return;
+    }
+
+    checkVisibilityOfSphere(index, rCamInfo);
+}
+
+void BrightSun::getNowCenter(TVec2f* pDst) const {
+    calcScreenPosition(pDst, mPosition, MR::getCameraViewMtx(), MR::getCameraProjectionMtx());
+}
+
+void BrightSun::controlSunModel() {
+    mSun->mPosition.set(mPosition);
+    mSun->mScale.set(::sSunModelScale);
+
+    TVec3f camDir(MR::getCamPos() - mPosition);
+    MR::normalize(&camDir);
+
+    TVec3f fwdVec(0.0f, 0.0f, 1.0f);
+    TPos3f rotateMtx;
+    rotateMtx.identity();
+    rotateMtx.setRotate(fwdVec, camDir);
+
+    TVec3f rotation;
+    if (fwdVec.dot(camDir) < -0.999f) {
+        rotation.set< f32 >(0.0f, 180.0f, 0.0f);
+    } else {
+        rotateMtx.getEulerXYZ(rotation);
+        rotation.scale(_180_PI);
+    }
+
+    mSun->mRotation.set(rotation);
+}
+
 BrightObj::~BrightObj() {
 }
 
-BrightSun::BrightSun(const char* pName) : NameObj(pName) {
-}
 BrightSun::~BrightSun() {
+}
+
+BrightObjBase::~BrightObjBase() {
+}
+
+void BrightObjBase::endRead() {
+    mInfo.endRead();
+}
+
+const TVec2f* BrightObjBase::getCenter() const {
+    return &mInfo.mRealCenter[mInfo.mCurrIndex];
+}
+
+const TVec2f* BrightObjBase::getBrightCenter() const {
+    return &mInfo.mBrightnessCenter[mInfo.mCurrIndex];
+}
+
+f32 BrightObjBase::getBright() const {
+    return mInfo.mBright[mInfo.mCurrIndex];
+}
+
+void BrightObjBase::getNowCenter(TVec2f* pDst) const {
+    pDst->set(mNowCenter);
+}
+
+void BrightSun::init(const JMapInfoIter& rIter) {
+    MR::initDefaultPos(this, rIter);
+
+    MR::invalidateClipping(this);
+
+    MR::connectToScene(this, MR::MovementType_Environment, -1, -1, MR::DrawType_BrightSun);
+
+    mSun = new Sun("太陽");
+    mSun->initWithoutIter();
+
+    MR::tryRegisterDemoCast(this, rIter);
+
+    if (MR::useStageSwitchReadAppear(this, rIter)) {
+        MR::syncStageSwitchAppear(this);
+        makeActorDead();
+    } else {
+        makeActorAppeared();
+    }
 }
