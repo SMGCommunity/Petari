88c13835b5f88e72fe4d9570284fd7de0192eae0
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-08-29 00:48:54 +0200

    Match most of `DragonHeadFlower` (#2008)
    * Match most of `DragonHeadFlower`
    
    * Small fixes
    
    * Update splits.txt
    
    * Update splits.txt
    
    * Update splits.txt
    
    * Update DragonHeadFlower.cpp

--- include/Game/MapObj/DragonHeadFlower.hpp
+++ include/Game/MapObj/DragonHeadFlower.hpp
@@ -5,8 +5,28 @@
 class DragonHeadFlower : public LiveActor {
 public:
     DragonHeadFlower(const char*);
-    virtual ~DragonHeadFlower();
 
-private:
-    u8 mPad[(0xE0) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter&);
+    virtual void control();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
+    void initParabola(const TVec3f&);
+
+    void exeWait();
+    void exeSetCenter();
+    void exeLaunchReady();
+    void exeRailMove();
+
+    /* 0x8C */ HitSensor* mPlayerSensor;
+    /* 0x90 */ f32 mParabolaProgress;
+    /* 0x94 */ TVec3f mParabolaStartPos;
+    /* 0xA0 */ TVec3f mTargetVelocity;
+    /* 0xAC */ TVec3f mCurrentGravity;
+    /* 0xB8 */ TVec3f mEndGravity;
+    /* 0xC4 */ TVec3f mParabolaDirection;
+    /* 0xD0 */ u32 mParabolaSteps;
+    /* 0xD4 */ f32 mParabolaDistance;
+    /* 0xD8 */ f32 mParabolaAccel;
+    /* 0xDC */ f32 mParabolaVel;
 };
