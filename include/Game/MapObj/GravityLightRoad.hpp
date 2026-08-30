a197a5ae44190b0e07b56e85ba9e783e6bd69611
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-08-30 17:43:56 +0200

    Match most of `GravityLightRoad`

--- include/Game/MapObj/GravityLightRoad.hpp
+++ include/Game/MapObj/GravityLightRoad.hpp
@@ -5,8 +5,23 @@
 class GravityLightRoad : public LiveActor {
 public:
     GravityLightRoad(const char*);
-    virtual ~GravityLightRoad();
+    
+    virtual void init(const JMapInfoIter&);
+    virtual void updateHitSensor(HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
 
-private:
-    u8 mPad[(0xA4) - sizeof(LiveActor)];
+    void updateBindActorMtx();
+    void endBind();
+    void startDisappear();
+    bool tryStartBind(LiveActor*);
+    bool isPlayerInCylinder() const;
+
+    void exeWait();
+    void exeDraw();
+    void exeDisappear();
+
+    /* 0x8C */ LiveActor* mHost;
+    /* 0x90 */ f32 mSpeed;
+    /* 0x94 */ f32 mRadius;
+    /* 0x98 */ TVec3f mEndPos;
 };
