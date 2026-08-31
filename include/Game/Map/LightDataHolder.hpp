0d5f7d3bda086b7126d80563bce6e3db8499968f
shibbo <supershibbo12@gmail.com>
2026-08-31 18:38:39 -0400

    match `LightDataHolder`

--- include/Game/Map/LightDataHolder.hpp
+++ include/Game/Map/LightDataHolder.hpp
@@ -3,36 +3,28 @@
 #include <JSystem/JGeometry/TVec.hpp>
 #include <revolution.h>
 
-class LightInfo {
-public:
+struct LightInfo {
     /* 0x00 */ GXColor mColor;
-    /* 0x04 */ TVec3f mPos;
+    /* 0x04 */ Vec mPos;
     /* 0x10 */ bool mIsFollowCamera;
 };
 
-class LightInfoCoin : public LightInfo {
-public:
-    LightInfoCoin() {
-    }
-
-    u8 _14;
-    u8 _15;
-    u8 _16;
-    u8 _17;
-    f32 _18;
+struct LightInfoCoin {
+    LightInfo base;
+    struct {
+        u8 _14;
+        u8 _15;
+        u8 _16;
+        u8 _17;
+        f32 _18;
+    };
 };
 
-class ActorLightInfo {
-public:
-    ActorLightInfo() {
-    }
-
-    void operator=(const ActorLightInfo&);
-
+struct ActorLightInfo {
     LightInfo mInfo0;  // 0x0
     LightInfo mInfo1;  // 0x14
     u8 mAlpha2;        // 0x28
-    GXColor mColor;   // 0x29
+    GXColor mColor;    // 0x29
 };
 
 struct AreaLightInfo {
@@ -54,6 +46,10 @@ public:
     AreaLightInfo* findAreaLight(const char*) const;
     const char* getDefaultAreaLightName() const;
     s32 getDefaultStepInterpolate() const;
+
+    inline AreaLightInfo* getLightInfo(s32 idx) const {
+        return &mLights[idx];
+    }
 
     /* 0x00 */ s32 mLightCount;
     /* 0x04 */ AreaLightInfo* mLights;
