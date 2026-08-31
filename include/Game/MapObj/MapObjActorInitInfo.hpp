753cc72bffdbb30ed105b2b393397f2094d88883
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-08-31 19:36:23 +0200

    `MapObjActorInitInfo` 99%
    There are probably a few functions that are inlined here that are not showing up in the symbols. There is a function for almost every member, so why not all members?
    
    The only other thing holding this back from being matched is the `.data` section. There are two strings (HioNode names) that appear at the start, but are used at the end.

--- include/Game/MapObj/MapObjActorInitInfo.hpp
+++ include/Game/MapObj/MapObjActorInitInfo.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "JSystem/JGeometry.hpp"
+#include <JSystem/JGeometry/TVec.hpp>
 
 class Nerve;
 
@@ -29,7 +29,7 @@ public:
     void setupRailMover();
     void setupRotator();
     void setupRailRotator();
-    void setupSEesaw1AxisRotator(const char*, f32);
+    void setupSeesaw1AxisRotator(const char*, f32);
     void setupRailPosture();
     void setupBaseMtxFollowTarget();
     void setupAffectedScale();
@@ -38,59 +38,57 @@ public:
     void setupPrepareChangeDummyTexture(const char*);
     void setupNoUseLodCtrl();
 
-    bool mSetDefaultPosition;  // 0x0
-    bool mConnectToScene;      // 0x1
-    bool mInitBinder;          // 0x2
-    bool mHasEffect;           // 0x3
-    bool mHasSensors;          // 0x4
-    bool mHasShadows;          // 0x5
-    bool mCalcGravity;         // 0x6
-    bool mProjectMapMtx;       // 0x7
-    bool mInitFur;             // 0x8
-    bool mHasRailMover;        // 0x9
-    bool mHasRotator;          // 0xA
-    bool mHasRailRotator;      // 0xB
-    u8 _C;
-    u8 _D;
-    bool mUsesRailPosture;          // 0xE
-    bool mDoesBaseMtxFollowTarget;  // 0xF
-    bool mIsAffectedByScale;        // 0x10
-    bool mUseMirrorReflection;      // 0x11
-    u8 _12;
-    u8 _13;
-    const char* mModelName;  // 0x14
-    const char* mHioNode;    // 0x18
-    u16 _1C;
-    u8 _1E;
-    u8 _1F;
-    f32 mSensorSize;       // 0x20
-    TVec3f mSensorOffset;  // 0x24
-    bool mHitSensorCB;     // 0x30
-    u8 _31;
-    u8 _32;
-    u8 _33;
-    f32 mBinderRadius;        // 0x34
-    f32 mBinderCenterY;       // 0x38
-    const char* mEffectName;  // 0x3C
-    s32 mSound;               // 0x40
-    TVec3f* mSoundPos;        // 0x44
-    u8 _48;
-    const Nerve* mNerve;  // 0x4C
-    s32 mGroupClipping;   // 0x50
-    f32 mClippingRadius;  // 0x54
-    f32 mFarClipping;     // 0x58
-    s32 _5C;
-    s32 mColorChangeArg;     // 0x60
-    s32 mTextureChangeArg;   // 0x64
-    bool mNoAppearRiddleSE;  // 0x68
-    u32 _6C;
-    f32 _70;
-    bool _74;
-    const char* mShadowName;  // 0x78
-    f32 mShadowLength;        // 0x7C
-    u32 _80;
-    const char* mDummyChangeTexture;  // 0x84
-    u32 _88;
-    bool _8C;
-    bool mNoUseLOD;  // 0x8D
+    /* 0x00 */ bool mSetDefaultPosition;
+    /* 0x01 */ bool mConnectToScene;
+    /* 0x02 */ bool mInitBinder;
+    /* 0x03 */ bool mHasEffect;
+    /* 0x04 */ bool mHasSensors;
+    /* 0x05 */ bool mHasShadows;
+    /* 0x06 */ bool mCalcGravity;
+    /* 0x07 */ bool mUseProjectMapMtx;
+    /* 0x08 */ bool mInitFur;
+    /* 0x09 */ bool mHasRailMover;
+    /* 0x0A */ bool mHasRotator;
+    /* 0x0B */ bool mHasRailRotator;
+    /* 0x0C */ bool _C;
+    /* 0x0D */ bool _D;
+    /* 0x0E */ bool mUseRailPosture;
+    /* 0x0F */ bool mUseBaseMtxFollowTarget;
+    /* 0x10 */ bool mIsAffectedByScale;
+    /* 0x11 */ bool mUseMirrorReflection;
+    /* 0x12 */ bool _12;
+    /* 0x13 */ bool _13;
+    /* 0x14 */ const char* mModelName;
+    /* 0x18 */ const char* mHioNode;
+    /* 0x1C */ u16 _1C;
+    /* 0x20 */ f32 mSensorRadius;
+    /* 0x24 */ TVec3f mSensorPosition;
+    /* 0x30 */ bool mHasHitSensorCB;
+    /* 0x31 */ u8 _31;
+    /* 0x32 */ u8 _32;
+    /* 0x33 */ u8 _33;
+    /* 0x34 */ f32 mBinderRadius;
+    /* 0x38 */ f32 mBinderCenterY;
+    /* 0x3C */ const char* mEffectName;
+    /* 0x40 */ s32 mSound;
+    /* 0x44 */ TVec3f* mSoundPos;
+    /* 0x48 */ bool _48;
+    /* 0x4C */ const Nerve* mNerve;
+    /* 0x50 */ s32 mGroupClipping;
+    /* 0x54 */ f32 mClippingRadius;
+    /* 0x58 */ f32 mFarClipping;
+    /* 0x5C */ s32 _5C;
+    /* 0x60 */ s32 mColorChangeArg;    // Obj_arg0
+    /* 0x64 */ s32 mTextureChangeArg;  // Obj_arg1
+    /* 0x68 */ bool mIsAppearRiddleSE;
+    /* 0x6C */ const char* mSeesaw1AxisRotatorName;
+    /* 0x70 */ f32 mSeesaw1AxisRotatorRadius;
+    /* 0x74 */ bool _74;
+    /* 0x78 */ const char* mShadowName;
+    /* 0x7C */ f32 mShadowLength;  // Obj_arg2
+    /* 0x80 */ const char* _80;
+    /* 0x84 */ const char* mDummyChangeTexture;
+    /* 0x88 */ u32 _88;
+    /* 0x8C */ bool _8C;
+    /* 0x8D */ bool mNoUseLOD;
 };
