3768044c8b476c4676a725d38a9a0db390226950
Fl-ppie <sebastiaan.aloisius@gmail.com>
2026-09-02 00:23:13 +0200

    Match `PunchBox` 100%

--- include/Game/MapObj/PunchBox.hpp
+++ include/Game/MapObj/PunchBox.hpp
@@ -5,8 +5,30 @@
 class PunchBox : public LiveActor {
 public:
     PunchBox(const char*);
-    virtual ~PunchBox();
 
-private:
-    u8 mPad[(0xA0) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void kill();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
+
+    void doHit(HitSensor*, HitSensor*);
+
+    void exeWait();
+    void exeHit();
+
+    /* 0x8C */ u16 _8C;
+    /* 0x8E */ u16 _8E;  // Unused?
+    /* 0x90 */ u16 _90;
+    /* 0x92 */ u16 _92;
+    /* 0x94 */ u16 _94;
+    /* 0x96 */ bool _96;
+    /* 0x97 */ bool _97;
+    /* 0x98 */ bool _98;
+    /* 0x99 */ bool mHasShadow;  // Obj_arg1
+    /* 0x9C */ s32 mCoinCount;   // Obj_arg0
 };
