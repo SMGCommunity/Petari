cec0bdfbb828c78a071438f4ef4592ff0143a91b
Gonzalo-kebab <koen.bommol@gmail.com>
2026-09-11 01:33:32 +0200

    match MarioBlown & nearly match MarioFaint (#2030)

--- include/Game/Player/MarioFaint.hpp
+++ include/Game/Player/MarioFaint.hpp
@@ -8,5 +8,16 @@ class MarioFaint : public MarioState {
 public:
     MarioFaint(MarioActor*);
 
-    u8 _11[0x17];
+    virtual bool start();
+    virtual bool close();
+    virtual bool update();
+
+    void setVec(const TVec3f&);
+
+    /* 0x12 */ u16 mTimer;
+    /* 0x14 */ u16 _14;
+    /* 0x16 */ u16 _16;
+    /* 0x18 */ TVec3f _18;
+    /* 0x24 */ bool mNoDamage;
+    /* 0x25 */ bool mTookDamage;
 };
