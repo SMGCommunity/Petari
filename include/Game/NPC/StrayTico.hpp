3c913eef1013ab400ad252104e5e21ae71e8d235
ThatNintendoNerd <samtheman1026@gmail.com>
2026-09-10 15:35:19 -0700

    Match 98% of `StrayTico`

--- include/Game/NPC/StrayTico.hpp
+++ include/Game/NPC/StrayTico.hpp
@@ -5,11 +5,11 @@
 class CollectTico;
 class PartsModel;
 
+/// @brief Silver Star
 class StrayTico : public LiveActor {
 public:
     StrayTico(const char*, CollectTico*);
 
-    virtual ~StrayTico();
     virtual void init(const JMapInfoIter&);
     virtual void appear();
     virtual void control();
@@ -19,28 +19,32 @@ public:
     virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
     virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
 
+    void initSensor();
+    void initShadow() NO_INLINE;
+    void startGlad();
     bool requestCompleteDemo(const TVec3f&, const TVec3f&, f32);
-    void exeWait();
-    void exeGlad();
-    void exeChase();
-    void exeLauncherCamera();
     void updateWarp();
     void updateChase();
-    void exeCompleteDemo();
     bool isEnableSpin() const;
     bool isRescued() const;
     bool isCompleteDemoEnd() const;
     void noticeEnd();
 
-    CollectTico* mParent;      // 0x8C
-    PartsModel* mBubbleModel;  // 0x90
-    TQuat4f _94;
-    TVec3f _A4;
-    TVec3f _B0;
-    TVec3f _BC;
-    TVec3f _C8;
-    TVec3f _D4;
-    TVec3f _E0;
-    f32 _EC;
-    f32 _F0;
+    void exeWait();
+    void exeGlad();
+    void exeChase();
+    void exeLauncherCamera();
+    void exeCompleteDemo();
+
+    /* 0x8C */ CollectTico* mHost;
+    /* 0x90 */ PartsModel* mItemBubbleModel;
+    /* 0x94 */ TQuat4f mRotateQuat;
+    /* 0xA4 */ TVec3f mFrontVec;
+    /* 0xB0 */ TVec3f _B0;
+    /* 0xBC */ TVec3f _BC;
+    /* 0xC8 */ TVec3f _C8;
+    /* 0xD4 */ TVec3f _D4;
+    /* 0xE0 */ TVec3f _E0;
+    /* 0xEC */ f32 _EC;
+    /* 0xF0 */ f32 _F0;
 };
