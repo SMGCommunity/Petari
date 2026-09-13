3d3d24f198cebef052d6f9a9a85d6649ac61202e
Pucas01 <129163556+Pucas01@users.noreply.github.com>
2026-09-13 22:10:00 +0200

    match PhantomShipBoxFloater.cpp

--- include/Game/MapObj/PhantomShipBoxFloater.hpp
+++ include/Game/MapObj/PhantomShipBoxFloater.hpp
@@ -2,11 +2,25 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
+class FloaterFloatingForceTypeSpring;
+
 class PhantomShipBoxFloater : public LiveActor {
 public:
     PhantomShipBoxFloater(const char*);
-    virtual ~PhantomShipBoxFloater();
 
-private:
-    u8 mPad[(0xA4) - sizeof(LiveActor)];
+    virtual ~PhantomShipBoxFloater();
+    virtual void init(const JMapInfoIter&);
+    virtual void control();
+
+    f32 getCurrentSinkDepth() const;
+
+    void exeWait();
+    void exeMove();
+    void exeMoveUnderWater();
+    void playRippleSe();
+
+    /* 0x8C */ FloaterFloatingForceTypeSpring* mFloatingForce;
+    /* 0x90 */ const char* mObjectName;
+    /* 0x94 */ TVec3f mFloaterOffset;
+    /* 0xA0 */ s32 mRippleSeCounter;
 };
--- src/Game/MapObj/PhantomShipBoxFloater.cpp
+++ src/Game/MapObj/PhantomShipBoxFloater.cpp
@@ -1,8 +1,129 @@
 #include "Game/MapObj/PhantomShipBoxFloater.hpp"
 #include "Game/LiveActor/Nerve.hpp"
+#include "Game/MapObj/FloaterFloatingForceTypeSpring.hpp"
 #include "Game/Util.hpp"
+#include "JSystem/JGeometry/TMatrix.hpp"
 
-PhantomShipBoxFloater::PhantomShipBoxFloater(const char* pName) : LiveActor(pName) {
+namespace {
+    const char* const cEffectName = "Ripple";
+}  // namespace
+
+namespace NrvPhantomShipBoxFloater {
+    NEW_NERVE(HostTypeWait, PhantomShipBoxFloater, Wait);
+    NEW_NERVE(HostTypeMove, PhantomShipBoxFloater, Move);
+    NEW_NERVE(HostTypeMoveUnderWater, PhantomShipBoxFloater, MoveUnderWater);
+}  // namespace NrvPhantomShipBoxFloater
+
+PhantomShipBoxFloater::PhantomShipBoxFloater(const char* pName) : LiveActor(pName), mObjectName(), mFloaterOffset(gZeroVec), mRippleSeCounter() {
 }
+
+void PhantomShipBoxFloater::init(const JMapInfoIter& rIter) {
+    MR::initDefaultPos(this, rIter);
+    mFloaterOffset.set(mPosition);
+    MR::getObjectName(&mObjectName, rIter);
+    initModelManagerWithAnm(mObjectName, nullptr, false);
+    MR::connectToSceneMapObj(this);
+    initEffectKeeper(0, nullptr, false);
+    MR::setEffectHostSRT(this, ::cEffectName, &mFloaterOffset, nullptr, nullptr);
+    initSound(4, false);
+    mRippleSeCounter = MR::getRandom(20l, 90l);
+    initHitSensor(1);
+    MR::initCollisionParts(this, mObjectName, MR::addBodyMessageSensorMapObj(this), nullptr);
+    MR::setClippingTypeSphereContainsModelBoundingBox(this, 100.0f);
+    initNerve(&NrvPhantomShipBoxFloater::HostTypeWait::sInstance);
+
+    mFloatingForce = new FloaterFloatingForceTypeSpring(this, mObjectName);
+    mFloatingForce->init(rIter);
+    mFloatingForce->start();
+    makeActorAppeared();
+}
+
+void PhantomShipBoxFloater::control() {
+    mFloatingForce->movement();
+    mFloatingForce->updateHostVelocity(&mVelocity);
+}
+
+f32 PhantomShipBoxFloater::getCurrentSinkDepth() const {
+    TVec3f zDir, yDir;
+    TPos3f depthMtx;
+    depthMtx.setInline(getBaseMtx());
+    depthMtx.getTrans(zDir);
+    depthMtx.getYDir(yDir);
+    f32 sign = MR::sign(yDir.dot(mFloaterOffset - zDir));
+    return sign * mFloaterOffset.distance(zDir);
+}
+
+void PhantomShipBoxFloater::exeWait() {
+    bool isMoving = 0.1f < mFloatingForce->getCurrentVelocity().length();
+
+    if (isMoving) {
+        setNerve(&NrvPhantomShipBoxFloater::HostTypeMove::sInstance);
+    }
+}
+
+void PhantomShipBoxFloater::exeMove() {
+    if (MR::isFirstStep(this)) {
+        MR::startSound(this, "SE_OJ_BOX_FLOATER_BOUND");
+        MR::emitEffect(this, ::cEffectName);
+    }
+
+    playRippleSe();
+
+    if (MR::isOnPlayer(this)) {
+        const char* objectName = mObjectName;
+        f32 sinkDepth;
+
+        if (MR::isEqualString("PhantomShipBoxFloaterA", objectName)) {
+            sinkDepth = 580.0f;
+        } else if (MR::isEqualString("PhantomShipBoxFloaterB", objectName)) {
+            sinkDepth = 380.0f;
+        } else if (MR::isEqualString("PhantomShipBoxFloaterC", objectName)) {
+            sinkDepth = 180.0f;
+        } else {
+            sinkDepth = 0.0f;
+        }
+
+        if (sinkDepth < getCurrentSinkDepth()) {
+            MR::deleteEffect(this, ::cEffectName);
+            setNerve(&NrvPhantomShipBoxFloater::HostTypeMoveUnderWater::sInstance);
+        }
+    } else {
+        bool val = 0.1f < mFloatingForce->getCurrentVelocity().length();
+
+        if (!val) {
+            MR::deleteEffect(this, ::cEffectName);
+            setNerve(&NrvPhantomShipBoxFloater::HostTypeWait::sInstance);
+        }
+    }
+}
+
+void PhantomShipBoxFloater::exeMoveUnderWater() {
+    if (!MR::isOnPlayer(this)) {
+        const char* objectName = mObjectName;
+        f32 sinkDepth;
+
+        if (MR::isEqualString("PhantomShipBoxFloaterA", objectName)) {
+            sinkDepth = 575.0f;
+        } else if (MR::isEqualString("PhantomShipBoxFloaterB", objectName)) {
+            sinkDepth = 375.0f;
+        } else if (MR::isEqualString("PhantomShipBoxFloaterC", objectName)) {
+            sinkDepth = 175.0f;
+        } else {
+            sinkDepth = 0.0f;
+        }
+
+        if (getCurrentSinkDepth() < sinkDepth) {
+            setNerve(&NrvPhantomShipBoxFloater::HostTypeMove::sInstance);
+        }
+    }
+}
+
+void PhantomShipBoxFloater::playRippleSe() {
+    if (--mRippleSeCounter <= 0) {
+        MR::startSound(this, "SE_OJ_BOX_FLOATER_RIPPLE");
+        mRippleSeCounter = MR::getRandom(20l, 90l);
+    }
+}
+
 PhantomShipBoxFloater::~PhantomShipBoxFloater() {
 }
