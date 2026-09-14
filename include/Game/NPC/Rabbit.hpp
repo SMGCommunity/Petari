7093f9e387ed2185214e207881fb126eb03ba773
Gabbo <74122269+Gabbo089@users.noreply.github.com>
2026-09-14 13:54:59 +0200

    Rabbit Header

--- include/Game/NPC/Rabbit.hpp
+++ include/Game/NPC/Rabbit.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Game/NPC/NPCActor.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+
+class LodCtrl;
+class TalkMessageCtrl;
 
 class Rabbit : public NPCActor {
 public:
@@ -10,9 +14,32 @@ public:
     };
 
     Rabbit(const char*);
-    virtual ~Rabbit();
-    static void makeArchiveList(NameObjArchiveListCollector*, const JMapInfoIter&);
 
-private:
-    u8 mPad[(0x184) - sizeof(NPCActor)];
+    virtual ~Rabbit();
+    virtual void init(const JMapInfoIter&);
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+
+    void calcRailPos(TVec3f*);
+    bool isNeedTurn(const TVec3f&);
+    void updateJump();
+
+    void exeAppear();
+    void exeAppearLand();
+    void exeWait();
+    void exeGoal();
+    void exeFinish();
+    void exeForwardLand();
+    void exePreJump();
+    void exeMove();
+    void exeBackwardLand();
+    void exePreJumpBack();
+    void exeNear();
+    void exeReaction();
+    void exeTalk();
+    void exeJumpV();
+    void exeJumpH();
+
+    /* 0x8C */ LodCtrl* mLodCtrl;
+    /* 0x90 */ TalkMessageCtrl* mTalkCtrl;
 };
