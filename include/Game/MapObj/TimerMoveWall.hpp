ac8776833a840ddebb9c7d3dda1c2e89cd1ce81a
Gabbo <74122269+Gabbo089@users.noreply.github.com>
2026-09-18 01:12:12 +0200

    ``TimerMoveWall`` 100%

--- include/Game/MapObj/TimerMoveWall.hpp
+++ include/Game/MapObj/TimerMoveWall.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "Game/MapObj/MapObjActor.hpp"
+#include "Game/MapObj/MapObjActorInitInfo.hpp"
 
 class TimerMoveWall : public MapObjActor {
 public:
     TimerMoveWall(const char*);
-    virtual ~TimerMoveWall();
 
-private:
-    u8 mPad[(0xCC) - sizeof(MapObjActor)];
+    virtual void init(const JMapInfoIter&);
+    virtual void initCaseUseSwitchB(const MapObjActorInitInfo&);
+
+    void startMove();
+
+    void exeWait();
+    void exeMove();
+
+    /* 0xC4 */ s32 mTimer;
+    /* 0xC8 */ f32 mMoveLenght;
 };
