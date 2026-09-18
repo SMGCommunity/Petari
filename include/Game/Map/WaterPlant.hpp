f6518d6a2b273f1f37ce35dfa3146dcce3a0ba67
Gonzalo-kebab <koen.bommol@gmail.com>
2026-09-18 09:51:29 +0200

    WaterPlant 97%

--- include/Game/Map/WaterPlant.hpp
+++ include/Game/Map/WaterPlant.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Game/LiveActor/LiveActor.hpp"
+#include "Game/Util/PlayerUtil.hpp"
 
 class JUTTexture;
 
@@ -10,13 +11,34 @@ public:
     public:
         /// @brief Creates a new `PlantData`.
         PlantData();
+
+        /* 0x00 */ TVec3f mPosition;
+        /* 0x0C */ TVec3f mRotation;
+        /* 0x18 */ TVec3f mSpinSwingDir;
+        /* 0x24 */ TVec3f mTempSpinSwingDir;
+        /* 0x30 */ f32 mSpinSwingStrength;
+        /* 0x34 */ s32 mSpinWaveSwingTimer;
+        /* 0x38 */ s32 mSpinWaveTimer;
+        /* 0x3C */ TVec3f mSpinWaveSwingVec1;
+        /* 0x48 */ TVec3f mSpinWaveSwingVec2;
+        /* 0x54 */ TVec3f mSpinWaveSwingVec3;
+        /* 0x60 */ f32 mHeight1;
+        /* 0x64 */ f32 mHeight2;
+        /* 0x68 */ f32 mHeight3;
+        /* 0x6C */u8 pad_6C;
+        /* 0x70 */ f32* mSwingStrength1;
+        /* 0x74 */ f32* mSwingStrength2;
+        /* 0x78 */ f32* mSwingStrength3;
+
+        bool newSpinWaveTimerIsGreater(s32 newTimer) {
+            return !(newTimer < mSpinWaveTimer);
+        }
     };
 
     /// @brief Creates a new `WaterPlant`.
     /// @param pName A pointer to the null-terminated name of the object.
     WaterPlant(const char* pName);
 
-    virtual ~WaterPlant();
     virtual void init(const JMapInfoIter& rIter);
     virtual void initAfterPlacement();
     virtual void movement();
@@ -26,13 +48,17 @@ public:
     void initPlantAfterPlacement();
     void drawDirect() const;
 
-    /* 0x8C */ s32 _8C;
-    /* 0x90 */ PlantData* _90;
-    /* 0x94 */ f32 _94;
-    /* 0x98 */ bool _98;
-    /* 0x9C */ f32 _9C;
+    PlantData* getData(int i) {
+        return &mPlantDataArray[i];
+    }
+
+    /* 0x8C */ s32 mPlantNum;
+    /* 0x90 */ PlantData* mPlantDataArray;
+    /* 0x94 */ f32 mAreaRadius;
+    /* 0x98 */ bool mIsSpin;
+    /* 0x9C */ f32 mHalfWidth;
     /* 0xA0 */ f32 mHeight;
-    /* 0xA4 */ s32 _A4;
+    /* 0xA4 */ s32 mType;
 };
 
 class WaterPlantDrawInit : public NameObj {
@@ -47,10 +73,10 @@ public:
     void loadTex(int);
     void updateSwingPos();
 
-    /* 0x0C */ f32 _C;
-    /* 0x10 */ f32* _10;
-    /* 0x14 */ JUTTexture* _14;
-    /* 0x18 */ JUTTexture* _18;
-    /* 0x1C */ JUTTexture* _1C;
-    /* 0x20 */ JUTTexture* _20;
+    /* 0x0C */ f32 mAngleOffset;
+    /* 0x10 */ f32* mSwingPosTable;
+    /* 0x14 */ JUTTexture* mPlantATex;
+    /* 0x18 */ JUTTexture* mPlantBTex;
+    /* 0x1C */ JUTTexture* mPlantCTex;
+    /* 0x20 */ JUTTexture* mPlantDTex;
 };
