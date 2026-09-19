ffeab6affa305f9e0e3323332c81f19f3d022e7e
Gonzalo-kebab <koen.bommol@gmail.com>
2026-09-19 17:46:07 +0200

    forgot to commit header

--- include/Game/MapObj/SeesawMoveNut.hpp
+++ include/Game/MapObj/SeesawMoveNut.hpp
@@ -5,8 +5,16 @@
 class SeesawMoveNut : public MapObjActor {
 public:
     SeesawMoveNut(const char*);
-    virtual ~SeesawMoveNut();
 
-private:
-    u8 mPad[(0xD0) - sizeof(MapObjActor)];
+    virtual void init(const JMapInfoIter&);
+
+    void exeHalfway();
+    void exeEdge();
+    void autoBackHome();
+    bool isEnableStart() const;
+    bool isAtEdge() const;
+
+    /* 0xC4 */ f32 mSpeed;
+    /* 0xC8 */ f32 mPrevAngularSpeed;
+    /* 0xCC */ s32 _CC;
 };
