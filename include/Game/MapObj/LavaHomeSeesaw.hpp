e99325f703e34cb942025232121c52854f08af95
Gabbo <74122269+Gabbo089@users.noreply.github.com>
2026-09-20 14:37:24 +0200

    ``LavaHomeSeesaw`` 100%

--- include/Game/MapObj/LavaHomeSeesaw.hpp
+++ include/Game/MapObj/LavaHomeSeesaw.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include "Game/LiveActor/LiveActor.hpp"
+#include "JSystem/JGeometry/TVec.hpp"
+
+class LavaHomeSeesawRotator;
+class ProjmapEffectMtxSetter;
 
 class LavaHomeSeesaw : public LiveActor {
 public:
     LavaHomeSeesaw(const char*);
-    virtual ~LavaHomeSeesaw();
 
-private:
-    u8 mPad[(0xA4) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter&);
+    virtual void initAfterPlacement();
+    virtual void control();
+    virtual void calcAndSetBaseMtx();
+
+    /* 0x8C */ LavaHomeSeesawRotator* mSeesawRotator;
+    /* 0x90 */ ProjmapEffectMtxSetter* mMtxSetter;
+    /* 0x94 */ TVec3f _94;
+    /* 0xA0 */ f32 mYOffset;
 };
