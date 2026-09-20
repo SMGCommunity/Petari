11550eef6bd8e0a22ae5a776349755732f466030
alexsr1 <alexrudd235@gmail.com>
2026-09-20 16:04:46 +0200

    Match `RunawayRabbit`

--- include/Game/NPC/RunawayRabbit.hpp
+++ include/Game/NPC/RunawayRabbit.hpp
@@ -11,20 +11,27 @@ class WalkerStateRunaway;
 
 class RunawayRabbit : public LiveActor {
 public:
+    enum HideLocation {
+        /* -0x1 */ HideLocation_None = -1,
+        /* 0x0 */ HideLocation_Hole,
+        /* 0x1 */ HideLocation_Leaves
+    };
+
     RunawayRabbit(const char*, RunawayRabbitCollect*);
 
-    virtual void init(const JMapInfoIter& rIter);
+    virtual void init(const JMapInfoIter&);
     virtual void initAfterPlacement();
     virtual void appear();
     virtual void control();
     virtual void calcAndSetBaseMtx();
-    virtual void attackSensor(HitSensor* pSender, HitSensor* pReceiver);
-    virtual bool receiveMsgPush(HitSensor* pSender, HitSensor* pReceiver);
-    virtual bool receiveMsgPlayerAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
-    virtual bool receiveMsgEnemyAttack(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
-    virtual bool receiveOtherMsg(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    virtual void attackSensor(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPush(HitSensor*, HitSensor*);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor*, HitSensor*);
+    virtual bool receiveOtherMsg(u32, HitSensor*, HitSensor*);
 
     void initSensor();
+    void initFootPrint();
     void updatePose();
     void updateBindActorMatrix();
     void activate();
@@ -35,11 +42,11 @@ public:
     void setNotCaughtable();
     void startJumpSound();
     void setMsgCtrl(TalkMessageCtrl*);
-    bool isCaught() const;
+    bool isCaught() const NO_INLINE;
     bool isCaughtable() const;
     bool isRunnaway() const;
     bool isChasing() const;
-    bool isEnableBlow() const;
+    bool isEnableBlow() const NO_INLINE;
     bool isValidFollow(s32) const;
 
     void exeNoActive();
@@ -59,17 +66,17 @@ public:
     /* 0x98 */ FootPrint* mFootPrint;
     /* 0x9C */ SpotMarkLight* mSpotMarkLight;
     /* 0xA0 */ TalkMessageCtrl* mMsgCtrl;
-    /* 0xA4 */ TQuat4f _A4;
-    /* 0xB4 */ TVec3f _B4;
-    /* 0xC0 */ TQuat4f _C0;
-    /* 0xD0 */ TVec3f _D0;
+    /* 0xA4 */ TQuat4f mPoseQuat;
+    /* 0xB4 */ TVec3f mFrontVec;
+    /* 0xC0 */ TQuat4f mPlayerPoseQuat;
+    /* 0xD0 */ TVec3f mPlayerBindPos;
     /* 0xDC */ u8 _DC[4];
-    /* 0xE0 */ s32 mObjArg0;
+    /* 0xE0 */ s32 mGroupId;
     /* 0xE4 */ s32 mRunawayLevel;
-    /* 0xE8 */ s32 mObjArg1;
-    /* 0xEC */ s32 _EC;
+    /* 0xE8 */ s32 mHideLocation;
+    /* 0xEC */ s32 mPressureTimer;
     /* 0xF0 */ s32 mNotCaughtableTimer;
-    /* 0xF4 */ bool _F4;
-    /* 0xF5 */ bool _F5;
-    /* 0xF8 */ f32 mObjArg3;
+    /* 0xF4 */ bool mIsActive;
+    /* 0xF5 */ bool mIsLinked;
+    /* 0xF8 */ f32 mNearSoundRadius;
 };
