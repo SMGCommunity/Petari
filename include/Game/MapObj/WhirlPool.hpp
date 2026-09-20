9c93af6cf6c7b7a62a3a335a19a39bb58e7ddf16
aba4gamer <ambernalalvarez@outlook.com>
2026-09-20 22:48:36 +0200

    Partially documented WhirlPool and added init

--- include/Game/MapObj/WhirlPool.hpp
+++ include/Game/MapObj/WhirlPool.hpp
@@ -2,11 +2,22 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
+class JUTTexture;
+
 class WhirlPool : public LiveActor {
 public:
     WhirlPool(const char*);
-    virtual ~WhirlPool();
 
-private:
-    u8 mPad[(0xD8) - sizeof(LiveActor)];
+    virtual void init(const JMapInfoIter& rITer);
+    virtual void movement();
+
+    void initPoints();
+
+    /* 0x8C */ f32 _8C;
+    /* 0x90 */ f32 _90;
+    /* 0x94 */ TVec3f _94;
+    u8 mPad[0x24];
+    /* 0xC4 */ JUTTexture* _C4;
+    /* 0xC8 */ JUTTexture* _C8;
+    /* 0xCC */ TVec3f _CC;
 };
--- src/Game/MapObj/WhirlPool.cpp
+++ src/Game/MapObj/WhirlPool.cpp
@@ -1,8 +1,31 @@
 #include "Game/MapObj/WhirlPool.hpp"
-#include "Game/LiveActor/Nerve.hpp"
+#include "Game/Map/WaterAreaHolder.hpp"
+#include "Game/Scene/SceneFunction.hpp"
 #include "Game/Util.hpp"
+#include "JSystem/JUtility/JUTTexture.hpp"
 
-WhirlPool::WhirlPool(const char* pName) : LiveActor(pName) {
+WhirlPool::WhirlPool(const char* pName) : LiveActor(pName), _8C(100.0f), _90(100.0f), _C4(), _C8(), _94(0.0f, 1.0f, 0.0f), _CC(0.0f, 0.0f, 0.0f) {
 }
-WhirlPool::~WhirlPool() {
+
+void WhirlPool::init(const JMapInfoIter& rIter) {
+    MR::connectToScene(this, MR::MovementType_MapObj, -1, -1, MR::DrawType_WhirlPoolAccelerator);
+    mPosition.set(*MR::getPlayerPos());
+    mPosition.y = 1500.0f;
+    MR::initDefaultPos(this, rIter);
+    _8C = mScale.x * 100.0f;
+    _90 = mScale.y * 100.0f;
+    MR::calcActorAxisY(&_94, this);
+    initPoints();
+    _C4 = new JUTTexture(MR::loadTexFromArc("WaterWave.arc", "Water.bti"), 0);
+    _C8 = new JUTTexture(MR::loadTexFromArc("WaterWave.arc", "WaterIndirect.bti"), 0);
+    initSound(4, false);
+    _CC.set(mPosition);
+    _CC.y -= _8C / 2.0f;
+    MR::setClippingTypeSphere(this, _8C, &_CC);
+    WaterAreaFunction::entryWhirlPool(this);
+    makeActorAppeared();
+}
+
+void WhirlPool::movement() {
+    MR::startLevelSound(this, "SE_AT_LV_WHIRL_POOL");
 }
