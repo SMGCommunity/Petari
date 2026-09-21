204ab6ac78ccb1f6fcfb77687fc4f5ff7074f77e
alexsr1 <alexrudd235@gmail.com>
2026-09-21 19:57:20 +0200

    Link RunawayRabbitCollect

--- include/Game/NPC/RunawayRabbitCollect.hpp
+++ include/Game/NPC/RunawayRabbitCollect.hpp
@@ -32,8 +32,8 @@ private:
     /* 0x98 */ RunawayTico** mTico;
     /* 0x9C */ s32 mTicoNum;
     /* 0xA0 */ u32 _A0;
-    /* 0xA4 */ u32 _A4;
+    /* 0xA4 */ s32 _A4;
     /* 0xA8 */ s32 mCompleteRabbitCount;
     /* 0xAC */ s32 mBgmState;
-    /* 0xB0 */ bool _B0[4];
+    /* 0xB0 */ bool isAppearTico[3];
 };
