285acb915c65a6539b4d8fe33614997390522809
MrL314 <11429905+MrL314@users.noreply.github.com>
2026-09-23 02:57:47 -0400

    `LavaProminenceTriple` almost...

--- src/Game/MapObj/LavaProminenceTriple.cpp
+++ src/Game/MapObj/LavaProminenceTriple.cpp
@@ -1,31 +1,51 @@
 #include "Game/MapObj/LavaProminenceTriple.hpp"
+#include "Game/LiveActor/HitSensor.hpp"
 #include "Game/LiveActor/LiveActor.hpp"
 #include "Game/LiveActor/Nerve.hpp"
 #include "Game/LiveActor/PartsModel.hpp"
 #include "Game/Util.hpp"
+#include "Game/Util/MathUtil.hpp"
+#include "Game/Util/PlayerUtil.hpp"
+
+void LavaProminenceTriple_FORCE_MATCH_SDATA2() {
+    (void)1.0f;
+    (void)0.0f;
+    (void)0.5f;
+    (void)2.0f;
+}
+
+void LavaProminenceTriple_DUMMY() {
+    TVec3f a, b;
+    a.add(b);
+}
+
+namespace {
+    static const f32 sNoSensorTable[] = {
+        0.0f, 59.765598f, 134.2f, 176.045f, 222.6f, 278.26199f, 360.0f,
+    };
+
+    static const f32 sInitRotateVel = 100.0f;
+    static const f32 sToSensorRadius = 1900.0f;
+    static const f32 sEnvironmentRadius = 2100.0f;
+};  // namespace
 
 LavaProminenceTriple::LavaProminenceTriple(const char* pName)
-    : LiveActor(pName), mBloomModel(), _C0(0, 0, 0, 1), _D0(0, 0, 0, 1), _E0(0, 0, 1), _EC(1, 0, 0) {
-    _FC = 0;
-    mArg0 = 100.0f;
-    _FD = 0;
-    _FE = 0;
-    _FF = 0;
-    mArg1 = 0;
-    _90.identity();
+    : LiveActor(pName), mBloomModel(), mBaseRot(0, 0, 0, 1), mCurrRot(0, 0, 0, 1), mSide(0, 0, 1), mFront(1, 0, 0), mIsAppearDrop1(),
+      mRotateVel(::sInitRotateVel), mIsAppearDrop2(), mIsAppearDrop3(), mIsEnvironment(), mDefaultAppearTime() {
+    mBloomMtx.identity();
 }
 
 void LavaProminenceTriple::init(const JMapInfoIter& rIter) {
     MR::initDefaultPos(this, rIter);
-    TQuat4f v7;
-    v7.setEulerDegree(mRotation.x, mRotation.y, mRotation.z);
-    v7.getZDir(_EC);
-    v7.getXDir(_E0);
-    _C0 = v7;
+    TQuat4f rot;
+    rot.setEulerDegree(mRotation.x, mRotation.y, mRotation.z);
+    rot.getZDir(mFront);
+    rot.getXDir(mSide);
+    mBaseRot = rot;
 
-    MR::getJMapInfoArg0NoInit(rIter, &mArg0);
-    MR::getJMapInfoArg1NoInit(rIter, &mArg1);
-    _104 = mArg1;
+    MR::getJMapInfoArg0NoInit(rIter, &mRotateVel);
+    MR::getJMapInfoArg1NoInit(rIter, &mDefaultAppearTime);
+    mAppearTime = mDefaultAppearTime;
 
     if (MR::useStageSwitchReadAppear(this, rIter)) {
         MR::syncStageSwitchAppear(this);
@@ -37,20 +57,20 @@ void LavaProminenceTriple::init(const JMapInfoIter& rIter) {
     if (MR::isEqualString(objName, "LavaProminenceEnvironment")) {
         initModelManagerWithAnm("LavaProminenceEnvironment", nullptr, false);
         MR::startBtk(this, "LavaProminenceEnvironment");
-        _FF = 1;
-        mBloomModel = MR::createBloomModel(this, _90);
+        mIsEnvironment = true;
+        mBloomModel = MR::createBloomModel(this, mBloomMtx);
         MR::startBtk(mBloomModel, "LavaProminenceEnvironmentBloom");
     } else {
         initModelManagerWithAnm("LavaProminenceTriple", nullptr, false);
         MR::startBtk(this, "LavaProminenceTriple");
-        _FF = 0;
-        mBloomModel = MR::createBloomModel(this, _90);
+        mIsEnvironment = false;
+        mBloomModel = MR::createBloomModel(this, mBloomMtx);
     }
 
     initSound(4, false);
     MR::connectToSceneMapObj(this);
 
-    if (!_FF) {
+    if (!mIsEnvironment) {
         initHitSensor(1);
         MR::addHitSensorCallbackMapObj(this, "attack", 1, 80.0f * mScale.y);
     }
@@ -58,10 +78,10 @@ void LavaProminenceTriple::init(const JMapInfoIter& rIter) {
     initEffectKeeper(0, nullptr, false);
     MR::setGroupClipping(this, rIter, 16);
 
-    if (_FF) {
-        MR::setClippingTypeSphere(this, 2100.0f * mScale.y);
+    if (mIsEnvironment) {
+        MR::setClippingTypeSphere(this, ::sEnvironmentRadius * mScale.y);
     } else {
-        MR::setClippingTypeSphere(this, 1900.0f * mScale.y);
+        MR::setClippingTypeSphere(this, ::sToSensorRadius * mScale.y);
     }
 
     if (MR::isValidSwitchAppear(this)) {
@@ -73,83 +93,126 @@ void LavaProminenceTriple::init(const JMapInfoIter& rIter) {
 
 void LavaProminenceTriple::appear() {
     LiveActor::appear();
-    _FC = 1;
-    _104 = mArg1;
+    mIsAppearDrop1 = true;
+    mAppearTime = mDefaultAppearTime;
     MR::emitEffect(this, "Drop1");
 
-    if (!_FF) {
-        _FD = 1;
-        _FE = 1;
+    if (!mIsEnvironment) {
+        mIsAppearDrop2 = true;
+        mIsAppearDrop3 = true;
         MR::emitEffect(this, "Drop2");
         MR::emitEffect(this, "Drop3");
     }
 }
 
-/*
 void LavaProminenceTriple::control() {
-    if (_104 <= 0) {
-        f32 rot = (mRotation.y + (mArg0 / 100.0f));
-        f32* ptr = &mRotation.y;
-        *ptr = rot;
-        MR::repeatDegree(ptr);
-        f32 v5 = RAD_TO_DEG(mRotation.y) / 2.0f;
-        _D0.scale(sin(v5), TVec3f(0, 1, 0));
-        f32 v7 = cos(v5);
-        TVec3f* pPos = &mBloomModel->mPosition;
-        _D0.w = v7;
-        pPos->set< f32 >(mPosition);
-        MR::startLevelSound(this, "SE_OJ_LV_PROMINENCE_T_MOVE");
-        updateEffectClipping();
+    if (mAppearTime > 0) {
+        mAppearTime--;
+        return;
+    }
 
-        if (MR::isValidSwitchAppear(this)) {
-            if (!MR::isOnSwitchAppear(this)) {
-                kill();
-            }
+    mRotation.y += mRotateVel / 100.0f;
+    MR::repeatDegree(&mRotation.y);
+    mCurrRot.setRotate(TVec3f(0, 1, 0), MR::toRadian(mRotation.y));
+    mBloomModel->mPosition.set(mPosition);
+    MR::startLevelSound(this, "SE_OJ_LV_PROMINENCE_T_MOVE");
+    updateEffectClipping();
+
+    if (MR::isValidSwitchAppear(this)) {
+        if (!MR::isOnSwitchAppear(this)) {
+            kill();
         }
-    } else {
-        _104--;
     }
 }
-*/
 
 void LavaProminenceTriple::calcAndSetBaseMtx() {
-    TQuat4f v10 = _C0;
-    PSQUATMultiply(&v10, &_D0, &v10);
+    TQuat4f rot = mBaseRot;
+    rot.mult(rot, mCurrRot);
     TPos3f mtx;
     mtx.zeroTrans();
-    mtx.setQT(v10, mPosition);
+    mtx.setQT(rot, mPosition);
     MR::setBaseTRMtx(this, mtx);
-    _90.set(mtx);
+    mBloomMtx.set(mtx);
 }
 
 void LavaProminenceTriple::attackSensor(HitSensor* pSender, HitSensor* pReceiver) {
     MR::sendMsgEnemyAttackFireStrong(pReceiver, pSender);
 }
 
-// LavaProminenceTriple::updateHitSensor
+void LavaProminenceTriple::updateHitSensor(HitSensor* pSensor) {
+    // issue with loading from secondary offset in table entries
+    // load order swap
+    // https://decomp.me/scratch/4nBud
+
+    TVec3f diff = *MR::getPlayerCenterPos();
+    diff -= mPosition;
+
+    if (MR::isNearZero(diff)) {
+        return;
+    }
+
+    f32 y = mSide.dot(diff);
+    f32 x = mFront.dot(diff);
+    f32 angle = MR::toDegree(MR::atan2(y, x));
+    angle -= mRotation.y;
+    MR::repeatDegree(&angle);
+
+    for (int i = 0; i < 3; i++) {
+        const f32* sensor0 = &::sNoSensorTable[0];
+        const f32* sensor1 = &::sNoSensorTable[1];
+
+        f32 half = (sensor0[i * 2 + 0] + sensor0[i * 2 + 1]) / 2.0f;
+
+        if (angle < half) {
+            int j;
+            if (i == 0) {
+                angle += 360.0f;
+                j = 2;
+            } else {
+                j = i - 1;
+            }
+
+            angle = MR::clamp(angle, sensor1[j * 2 + 0], sensor1[j * 2 + 1]);
+            break;
+        }
+
+        if (i == 2) {
+            angle = MR::clamp(angle, sensor1[i * 2 + 0], sensor1[i * 2 + 1]);
+            break;
+        }
+    }
+
+    angle += mRotation.y;
+    MR::repeatDegree(&angle);
+
+    TVec3f offset = mFront * MR::cosDegree(angle);
+    offset += mSide * MR::sinDegree(angle);
+    offset *= mScale.y * ::sToSensorRadius;
+    pSensor->mPosition.set(mPosition);
+    pSensor->mPosition.add(offset);
+}
 
 void LavaProminenceTriple::updateEffectClipping() {
-    updateEffectClippingIndividual(&_FC, "Drop1", "Head1");
+    updateEffectClippingIndividual(&mIsAppearDrop1, "Drop1", "Head1");
 
-    if (!_FF) {
-        updateEffectClippingIndividual(&_FD, "Drop2", "Head2");
-        updateEffectClippingIndividual(&_FE, "Drop3", "Head3");
+    if (!mIsEnvironment) {
+        updateEffectClippingIndividual(&mIsAppearDrop2, "Drop2", "Head2");
+        updateEffectClippingIndividual(&mIsAppearDrop3, "Drop3", "Head3");
     }
 }
 
-void LavaProminenceTriple::updateEffectClippingIndividual(bool* a1, const char* a2, const char* a3) {
+void LavaProminenceTriple::updateEffectClippingIndividual(bool* pIsAppear, const char* pEffectName, const char* pJointName) {
     TVec3f jointPos;
-    MR::copyJointPos(this, a3, &jointPos);
-    if (*a1 == 1) {
+    MR::copyJointPos(this, pJointName, &jointPos);
+    if (*pIsAppear == true) {
         if (MR::isJudgedToClipFrustum(jointPos, 1.0f)) {
-            MR::deleteEffect(this, a2);
-            *a1 = false;
+            MR::deleteEffect(this, pEffectName);
+            *pIsAppear = false;
         }
-    } else if (!MR::isJudgedToClipFrustum(jointPos, 1.0f)) {
-        MR::emitEffect(this, a2);
-        *a1 = true;
+    } else {
+        if (!MR::isJudgedToClipFrustum(jointPos, 1.0f)) {
+            MR::emitEffect(this, pEffectName);
+            *pIsAppear = true;
+        }
     }
-}
-
-LavaProminenceTriple::~LavaProminenceTriple() {
 }
