8dc3384eadf04508f9cd9c440b75004dcbd255c0
aba4gamer <ambernalalvarez@outlook.com>
2026-09-26 10:35:30 +0200

    JetTurtle small cleanup (#2085)
    * JetTurtle quick cleanup
    
    * Okay
    
    * Come on and join to the C++ casting to be the best C++
    
    * Ehem

--- include/Game/MapObj/JetTurtle.hpp
+++ include/Game/MapObj/JetTurtle.hpp
@@ -2,8 +2,8 @@
 
 #include "Game/LiveActor/LiveActor.hpp"
 
-#define JETTURTLETYPE_RED (s16)1
-#define JETTURTLETYPE_GOLD (s16)2
+#define JETTURTLETYPE_RED 1
+#define JETTURTLETYPE_GOLD 2
 
 class JetTurtle : public LiveActor {
 public:
@@ -45,23 +45,23 @@ public:
 
     inline bool isNerveWait();
 
-    f32 _8C;
-    u16 mShellType;  // 0x90 (0 = green, 1 = red, 2 = gold)
-    u16 _92;
-    HitSensor* _94;
-    HitSensor* _98;
-    TVec3f _9C;
-    TVec3f _A8;
-    TVec3f _B4;
-    TVec3f _C0;
-    TVec3f _CC;
-    u16 _D8;
-    u16 _DA;
-    u32 _DC;
-    u8 _E0;
-    u8 _E1;
-    u8 _E2;
-    u8 _E3;
+    /* 0x8C */ f32 _8C;
+    /* 0x90 */ u16 mShellType;  // (0 = green, 1 = red, 2 = gold)
+    /* 0x92 */ u16 _92;
+    /* 0x94 */ HitSensor* _94;
+    /* 0x98 */ HitSensor* _98;
+    /* 0x9C */ TVec3f _9C;
+    /* 0xA8 */ TVec3f _A8;
+    /* 0xB4 */ TVec3f _B4;
+    /* 0xC0 */ TVec3f _C0;
+    /* 0xCC */ TVec3f _CC;
+    /* 0xD8 */ u16 _D8;
+    /* 0xDA */ u16 _DA;
+    /* 0xDC */ u32 _DC;
+    /* 0xE0 */ u8 _E0;
+    /* 0xE1 */ u8 _E1;
+    /* 0xE2 */ u8 _E2;
+    /* 0xE3 */ u8 _E3;
 };
 
 class GoldenTurtle : public JetTurtle {
